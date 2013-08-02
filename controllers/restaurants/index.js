18ae45969acc915ce12e3caed802a86f7956f3ec
Lalit Kapoor <lalitkapoor@gmail.com>
2013-08-02 15:49:22 -0500

    modified schema of menu_items os that lookup can be done by restaurant_id more easily. Also returning categories/items

--- controllers/restaurants/index.js
+++ controllers/restaurants/index.js
@@ -1,5 +1,6 @@
 var
-  db = require('../../db')
+  async = require('async')
+, db = require('../../db')
 , errors = require('../../errors')
 , utils = require('../../utils')
 ;
@@ -43,6 +44,47 @@ module.exports.get = function(req, res) {
   });
 }
 
-module.exports.listItems = function(req, res) {
-  res.send(501);  //not implemented
+module.exports.menu = function(req, res) {
+  var data = {
+    categories: null
+  , items: null
+  };
+
+  var tasks = {
+    getCategories: function(callback) {
+      var query = {
+        type: 'select'
+      , table: 'menu_categories'
+      , columns: ['*']
+      , where: {id: parseInt(req.params.id)}
+      }
+      var sql = db.builder.sql(query);
+      db.query(sql.query, sql.values, function(error, response) {
+        if (error) return res.error(errors.internal.DB_FAILURE, error, callback);
+        data.categories = response;
+        return callback();
+      });
+    }
+  , getItems: function(callback) {
+      var query = {
+        type: 'select'
+      , table: 'menu_items'
+      , columns: ['*']
+      , where: {id: parseInt(req.params.id)}
+      }
+      var sql = db.builder.sql(query);
+      db.query(sql.query, sql.values, function(error, response) {
+        if (error) return res.error(errors.internal.DB_FAILURE, error, callback);
+        data.items = response;
+        return callback();
+      });
+    }
+  }
+
+  var done = function(error, results){
+    if(error) return;
+    res.send(data);
+  }
+
+  async.parallel(tasks, done);
 }

6d96ba28a4eff5302524155d0c419ed52f23a762
Geoffrey Parker <grp@gparker.co>
2013-08-03 22:49:43 -0500

    moved category into restaurants controller

--- controllers/restaurants/categories.js
+++ controllers/restaurants/categories.js
@@ -6,20 +6,24 @@ var models = require('../../models');
 module.exports.list = function(req, res) {
   //TODO: middleware to validate and sanitize query object
   var query = utils.extend({where: {}}, req.query);
-  utils.extend(query.where, {'menu_id': req.params.menuId});
+  utils.extend(query.where, {'menu_id': req.params.rid});
   models.Category.find(query, function(error, results) {
     if (error) return res.error(errors.internal.DB_FAILURE, error);
-    res.send(utils.invoke(results
+    res.send(utils.invoke(results, 'toJSON'));
   });
 }
 
 module.exports.get = function(req, res) {
-  models.Category.findOne(parseInt(req.params.id), function(error, response) {
+  models.Category.findOne(parseInt(req.params.cid), function(error, response) {
     if (error) return res.error(errors.internal.DB_FAILURE, error);
     res.send(response ? response.toJSON() : 404);
   });
 }
 
 module.exports.listItems = function(req, res) {
-  res.send(501);  //not implemented
+  var query = {where: {'category_id': req.params.cid}};
+  models.Item.find(query, function(err, results) {
+    if (error) return res.error(errors.internal.DB_FAILURE, error);
+    res.send(utils.invoke(results, 'toJSON'));
+  });
 }

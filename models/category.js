30dac75f87e4d17e280f8bb65bf723f4bbfa99fe
Geoffrey Parker <grp@gparker.co>
2013-08-04 19:51:49 -0500

    category getItems

--- models/category.js
+++ models/category.js
@@ -1,3 +1,17 @@
 var Model = require('./model');
+var utils = require('../utils');
 
-module.exports = Model.extend({}, {table: 'categories'});
+module.exports = Model.extend({
+  getItems: function(callback) {
+    var self = this;
+    callback = callback || function() {};
+    require('./item').find(
+      {where: {'category_id': self.attributes.id}},
+      function(err, items) {
+        if (err) return callback(err);
+        self.items = items;
+        callback(null, items);
+      }
+    );
+  }
+}, {table: 'categories'});

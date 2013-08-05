7b56dc09675443c3708318eb1a039c2a3e9ebc88
Geoffrey Parker <grp@gparker.co>
2013-08-04 19:18:58 -0500

    added restaurant supresource getters.  items not working yet

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,12 +1,35 @@
 var Model = require('./model');
 var utils = require('../utils');
 
-Restaurant = Model.extend({
-  validate: function() {
-    //TODO: validate using schema.  probably ought to put that part in super
-  }
-}, {
-  table: 'restaurants'
-})
+module.exports = Model.extend({
+  getCategories: function(callback) {
+    var self = this;
+    callback = callback || function() {};
+    require('./category').find(
+      {where: {'restaurant_id': this.attributes.id}},
+      function(err, results) {
+        if (err) return callback(err);
+        self.categories = results;
+        callback(null, results);
+      });
+  },
 
-module.exports = Restaurant;
+  getItems: function(callback) {
+    var self = this;
+    callback = callback || function() {};
+    var items = function(err) {
+      if (err) return callback(err);
+      var categories = utils.map(self.categories, function(cat) { return cat.toJSON().id; });
+      require('./item').find(
+        {where: {'category_id': {$in: categories}}},
+        function(err, results) {
+          if (err) return callback(err);
+          self.items = results;
+          callback(null, results);
+        }
+      );
+    }
+
+    self.categories ? items() : self.getCategories(items);
+  }
+}, {table: 'restaurants'});

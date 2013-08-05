f32ce72fad536fa560a29d0eb6e92c142f885622
Geoffrey Parker <grp@gparker.co>
2013-08-05 13:40:40 -0500

    add order manipulation routes

--- routes.js
+++ routes.js
@@ -16,6 +16,10 @@ module.exports.register = function(app) {
 
   app.get('/restaurants/:rid/categories/:cid/items', controllers.restaurants.categories.listItems);
 
+  app.get('/restaurants/:rid/orders', controllers.restaurants.orders.list);
+
+  app.post('/restaurants/:rid/orders', controllers.restaurants.orders.create);
+
   app.get('/items', controllers.items.list);
 
   app.get('/items/:id', controllers.items.get);
@@ -24,9 +28,26 @@ module.exports.register = function(app) {
 
   app.get('/orders/:id', controllers.orders.get);
 
+  app.put('/orders/:id', controllers.orders.update);
+
+  app.del('/orders/:id', controllers.orders.cancel);
+
+  app.get('/orders/:id/status-history', controllers.orders.listStatus); // latest is on order
+
+  app.post('/orders/:id/status-history', controllers.orders.changeStatus);
+
+  app.all('/orders/:id/status-history', function(req, res, next) {
+    res.set('Allow', 'GET, POST');
+    res.send(405);
+  });
+
   app.get('/orders/:oid/items', controllers.orders.orderItems.list);
 
+  app.post('/orders/:oid/items', controllers.orders.orderItems.add);
+
   app.get('/orders/:oid/items/:iid', controllers.orders.orderItems.get);
+
+  app.del('/orders/:oid/items/:iid', controllers.orders.orderItems.remove);
 
   app.get('/*', function(req, res) {
     file.serve(req, res);

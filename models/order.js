d5ee3f6a34dfdc21f0ae2b3aaa07f9d62c1413b2
Geoffrey Parker <grp@gparker.co>
2013-08-06 17:12:09 -0500

    added latest status join to orders find

--- models/order.js
+++ models/order.js
@@ -16,6 +16,42 @@ module.exports = Model.extend({
 
   find: function(query, callback) {
     // TODO: alter query to add latest status
+    query = query || {};
+    query.columns = query.columns || ['*'];
+
+    query.columns.push('latest.status');
+
+    query.joins = query.joins || {};
+
+    query.joins.latest = {
+      type: 'left'
+    , columns: ['order_id', 'status']
+    , on: {
+        'orderId': '$orders.id$'
+      }
+    , target: {
+        type: 'select'
+      , columns: ['order_id', 'status']
+      , table: 'order_statuses'
+      , alias: 'statuses'
+      , joins: {
+          recent: {
+            type: 'inner'
+          , on: {
+              'order_id': '$statuses.order_id$'
+            , 'created_at': '$statuses.created_at$'
+            }
+          , target: {
+              type: 'select'
+            , table: 'order_statuses'
+            , columns: ['order_id', 'max(created_at) as created_at']
+            , groupBy: 'order_id'
+            }
+          }
+        }
+      }
+    };
+
     Model.prototype.find.apply(this, arguments);
   }
 }, {table: 'orders'});

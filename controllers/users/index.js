6ce3989731a224d8c52de68ab02b4f65b05091d8
Geoffrey Parker <grp@gparker.co>
2013-08-07 11:06:37 -0500

    switch over existing user controllers to using db/queries

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,19 +1,12 @@
-var
-  async = require('async')
+var db = require('../../db');
+var queryies = require('../../db/queries');
+var errors = require('../../errors');
+var utils = require('../../utils');
 
-, db = require('../../db')
-, errors = require('../../errors')
-, utils = require('../../utils')
-;
 
 module.exports.list = function(req, res) {
-  var query = {
-    type: 'select'
-  , table: 'users'
-  , columns: (utils.isNotBlank(req.query.fields)) ? req.query.fields.split(',') : ['*']
-  , limit: (utils.isNotBlank(req.query.limit) && !isNaN(req.query.limit) && req.query.limit+0<100) ? parseInt(req.query.limit) : 100
-  , offset: (utils.isNotBlank(req.query.offset) && !isNaN(req.query.offset) && req.query.offset+0<100) ? parseInt(req.query.offset) : 0
-  };
+  var inputs = utils.pick(JSON.parse(req.query), ['columns', 'limit', 'offset']);
+  var query = queries.user.list(inputs.columns, inputs.limit, inputs.offset);
 
   var sql = db.builder.sql(query);
   db.query(sql.query, sql.values, function(error, results){
@@ -31,39 +24,30 @@ module.exports.create = function(req, res) {
       });
     }
   , create: function(hash, callback) {
-      var query = {
-        type: 'insert'
-      , table: 'users'
-      , values: {
-          first_name: req.body.first_name
-        , last_name: req.body.last_name
-        , email: req.body.email.toLowerCase()
-        , password: hash
-        , organization: req.body.organization
-        }
-      , returning: ['id']
-      };
+      var query queries.user.create({
+        first_name: req.body.first_name
+      , last_name: req.body.last_name
+      , email: req.body.email.toLowerCase()
+      , password: hash
+      , organization: req.body.organization
+      });
 
       var sql = db.builder.sql(query);
       db.query(sql.query, sql.values, function(error, results){
         if (error) return res.error(parseInt(error.code) === 23505 ? errors.registration.EMAIL_TAKEN : errors.internal.DB_FAILURE, error, callback);
         var user = results[0];
-        req.session = utils.extend({}, req.session, {user: {id: user.id}});
+        utils.extend(req.session, {user: {id: user.id}});
         return res.redirect('/');
       });
     }
   }
 
-  async.waterfall([flow.encrypt, flow.create]);
+  utils.async.waterfall([flow.encrypt, flow.create]);
 }
 
 module.exports.get = function(req, res) {
-  var query = {
-    type: 'select'
-  , table: 'users'
-  , columns: (req.query.fields != null && req.query.field != '') ? req.query.fields.split(',') : ['*']
-  , where: {id: parseInt(req.params.id)}
-  };
+  var inputs = JSON.parse(req.query);
+  var query = queries.user.get(inputs.columns);
 
   var sql = db.builder.sql(query);
   db.query(sql.query, sql.values, function(error, results){
@@ -72,12 +56,9 @@ module.exports.get = function(req, res) {
   });
 }
 
+// TODO: we don't really want to delete uses.  come up with some sort of deactivate user account thing.
 module.exports.del = function(req, res) {
-  var query = {
-    type: 'delete'
-  , table: 'users'
-  , where: {id: parseInt(req.params.id)}
-  };
+  var query = queries.user.del(parseInt(req.params.id));
 
   var sql = db.builder.sql(query);
   db.query(sql.query, sql.values, function(error, results){

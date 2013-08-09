604e2cc152cd69d8c5d6bfaf8caa3ea3ef0aa0c6
Geoffrey Parker <grp@gparker.co>
2013-08-09 10:12:23 -0500

    add script blocks to layout with extend and block helpers

--- app.js
+++ app.js
@@ -87,6 +87,24 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-//app.use(require('./collections/users/server'));
+
+// handle these bars
+var blocks = {};
+hbs.registerHelper('extend', function(name, context) {
+  var block = blocks[name];
+  if (!block) {
+    block = blocks[name] = [];
+  }
+
+  block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
+});
+
+hbs.registerHelper('block', function(name) {
+  var val = (blocks[name] || []).join('\n');
+
+  // clear the block
+  blocks[name] = [];
+  return val;
+});
 
 routes.register(app);

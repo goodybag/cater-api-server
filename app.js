501e67ffdb51f4c74cc73c01e6e679dfc1e2e3c6
Geoffrey Parker <grp@gparker.co>
2013-08-09 16:10:10 -0500

    fix case of null address

--- app.js
+++ app.js
@@ -110,9 +110,9 @@ hbs.registerHelper('address', function(loc) {
   if (!loc) return '';
   var line1 = loc.street ? loc.street : utils.joinIf([loc.street1, loc.street2], ', ');
   // TODO: put in <abbr> tag for state
-  var line2 = utils.joinIf([utils.joinIf([utils.capitalize(loc.city), loc.state.toUpperCase()], ', '), loc.zip], ' ');
-  return utils.joinIf([line1 ? '<span class="addr addr-street">' + line1 + '</span>' : null,
-                       line2 ? '<span class="addr addr-city-state-zip">' + line2 + '</span>' : null], '\n');
+  var line2 = utils.joinIf([utils.joinIf([utils.capitalize(loc.city), (loc.state||'').toUpperCase()], ', '), loc.zip], ' ');
+  // return utils.joinIf([line1 ? '<span class="addr addr-street">' + line1 + '</span>' : null,
+  //                      line2 ? '<span class="addr addr-city-state-zip">' + line2 + '</span>' : null], '\n');
 });
 
 routes.register(app);

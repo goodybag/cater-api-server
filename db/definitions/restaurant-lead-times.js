c3388ceb1b0e620b9f4a80438ee1aa4f78d6d3e5
Lalit Kapoor <lalitkapoor@gmail.com>
2013-08-15 10:34:17 -0500

    added schema for restaurant lead times

---
+++ db/definitions/restaurant-lead-times.js
@@ -0,0 +1,48 @@
+/**
+ * Users Schema
+ */
+
+if (typeof module === 'object' && typeof define !== 'function') {
+  var define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+var
+  types = require('../data-types')
+;
+
+define(function(require) {
+  var definition = {};
+  definition.name = 'restaurant_lead_times';
+
+  definition.schema = {
+    id: {
+      type: types.serial
+    , pk: true
+    }
+  , created_at: {
+      type: types.timestamp
+    , nullable: false
+    , default: 'NOW()'
+    }
+  , restaurant_id: {
+      type: types.int
+    , nullable: false
+    , references: {table: 'restaurants', column: 'id'}
+    }
+  , max_guests: {
+      type: types.int
+    , nullable: false
+    }
+  , hours: {
+      type: types.int
+    , default: 0
+    , nullable: false
+    }
+  };
+
+  definition.indices = {};
+
+  return definition;
+});

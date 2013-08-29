6e8e7e797655f1809d0eafa2572a2f119a2280eb
Lalit Kapoor <lalitkapoor@gmail.com>
2013-08-29 15:16:35 -0500

    added date/time selectors to order params

--- public/js/views/order-params-view.js
+++ public/js/views/order-params-view.js
@@ -6,40 +6,35 @@ var OrderParamsView = Backbone.View.extend({
     // the OrderParams model should be passed in
     // the el should also be passed in
     this.listenTo(this.model, 'change', this.updateFields, this);
+
+    this.render();
   }
+
+, render: function() {
+    this.datepicker = this.$el.find("input[name='date']").eq(0).pickadate({
+      format: 'mm/dd/yyyy'
+    , min: new Date()
+    }).pickadate('picker');
+
+    this.timepicker = this.$el.find("input[name='time']").eq(0).pickatime({
+      format: 'hh:i A'
+    , interval: 15
+    }).pickatime('picker');
+  }
+
 , updateFields: function(model, value, options) {
     for (var key in model.changed) {
-      if(key == 'time' && model.changed[key]) {
-        var time = moment(model.changed[key]);
-        this.$el.find('input[name=' + key + ']').val(time.format('h:mm A'));
-      } else {
-        this.$el.find('input[name=' + key + ']').val(model.changed[key]);
-      }
+      this.$el.find('input[name=' + key + ']').val(model.changed[key]);
     }
   }
+
 , submit: function (e) {
     e.preventDefault();
 
-    // parse time and send ISOString
-    var time = this.$("input[name='time']").val() || null;
-    if (time) {
-      try{
-        var ampm = time.split(' ')[1].toLowerCase();
-        var timeParts = time.split(' ')[0].split(':');
-
-        if (ampm == 'pm') timeParts[0] = parseInt(timeParts[0])+12;
-        time = moment();
-        time.hour(timeParts[0]).minute(timeParts[1]).second(0).millisecond(0);
-        time = time.toISOString();
-      } catch(e) {
-        console.log('invalid time');
-        time = null;
-      }
-    }
     var form = {
       zip: this.$("input[name='zip']").val() || null
-    , date: this.$("input[name='date']").val() || null
-    , time: time || null
+    , date: this.datepicker.get() || null
+    , time: this.timepicker.get() || null
     , guests: this.$("input[name='guests']").val() || null
     };
     this.model.save(form, {

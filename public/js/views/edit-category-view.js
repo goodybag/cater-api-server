ed49c942f599d923e02c5f0031b44bc3e17a54f3
Geoffrey Parker <grp@gparker.co>
2013-08-29 13:06:27 -0500

    setup category FormView stuff, save button show/hide.

--- public/js/views/edit-category-view.js
+++ public/js/views/edit-category-view.js
@@ -1,11 +1,30 @@
 var EditCategoryView = FormView.extend({
   events: {
-    'click .new-item': 'newItem'
+    'click .new-item': 'newItem',
+    'keyup .category-form .form-control': 'onChange',
+    'change .category-form .form-control': 'onChange'
   },
 
   initialize: function(options) {
     this.items = [];
     this.listenTo(this.model.items, 'sort', this.sortItems, this);
+  },
+
+  fieldMap: {
+    name: '.category-form .category-name',
+    description: '.category-form .category-description',
+    order: '.category-form .category-order'
+  },
+
+  fieldGetters: {
+    order: function() {
+      var val = this.$el.find(this.fieldMap.order).val().trim();
+      return val ? parseInt(val) : null;
+    }
+  },
+
+  onChange: function(e) {
+    this.$el.find('.category-form .category-save').toggleClass('hide', !this.getDiff());
   },
 
   newItem: function(e) {

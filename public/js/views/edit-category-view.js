4a8671d40640fb2f0ec335de16dc496cf6aedc12
Geoffrey Parker <grp@gparker.co>
2013-08-29 12:49:43 -0500

    don't sort on initial item add

--- public/js/views/edit-category-view.js
+++ public/js/views/edit-category-view.js
@@ -12,7 +12,7 @@ var EditCategoryView = FormView.extend({
     var itemModel = new Item({category_id: this.model.id}, {category: this.model})
     var itemView = new EditItemView({model: itemModel, category: this});
 
-    this.model.items.add(itemModel);
+    this.model.items.add(itemModel, {sort: false});
     this.items.push(itemView);
 
     itemView.render().attach();

8963a5eefc4f43e25628e82e38b46e67eeb49a8c
Geoffrey Parker <grp@gparker.co>
2013-09-03 16:44:11 -0500

    add twilio

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies
  */
@@ -34,6 +33,10 @@ var config = {
   , mailgun: {
       apiKey: 'key-8ffj79wzb2dda3s6r7u93o4yz07oxxu8'
     , publicApiKey: 'pubkey-45a1-ynm6dw4tmk8egc6izhgqntwir79'
+    }
+  , twilio: {
+      account: 'AC4ec9863aecd8248803144972fc51bac0'
+    , token: 'f45e26c40cd7481c872c3552676b598b'
     }
   , emails: {
       support: 'support@goodybag.com'

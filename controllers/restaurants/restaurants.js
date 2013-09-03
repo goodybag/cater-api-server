35d7bcb5db3b8fc2c00dca0dc5516d53d7fba790
John Fawcett <jrf0110@gmail.com>
2013-09-03 18:14:46 -0500

    lotsa list-group item and menu work

--- controllers/restaurants/restaurants.js
+++ controllers/restaurants/restaurants.js
@@ -63,6 +63,7 @@ module.exports.get = function(req, res) {
     }
 
     res.render('menu', context, function(err, html) {
+      console.log(err)
       if (err) return res.error(errors.internal.UNKNOWN, err);
       return res.send(html);
     });

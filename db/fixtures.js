215fb092d79228882c5525176fd079b09c3e2c5e
Geoffrey Parker <grp@gparker.co>
2013-09-04 10:19:21 -0500

    use test phone number if present

--- db/fixtures.js
+++ db/fixtures.js
@@ -53,7 +53,7 @@ var inserts = {
       , city: faker.Address.city()
       , state: faker.Address.usState(true)
       , zip: faker.Address.zipCodeFormat(0)
-      , phone: parseInt(faker.PhoneNumber.phoneNumberFormat(faker.definitions.phone_formats.length-1))
+      , phone: config.testPhone || parseInt(faker.PhoneNumber.phoneNumberFormat(faker.definitions.phone_formats.length-1))
       , email: config.testEmail || faker.Internet.email()
       , price: faker.Helpers.randomNumber(5) + 1
       , cuisine: faker.Lorem.words(faker.Helpers.randomNumber(4))

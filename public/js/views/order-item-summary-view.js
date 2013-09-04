353296f495b12dcfc232c0299bc35773b0499b6a
John Fawcett <jrf0110@gmail.com>
2013-09-04 16:32:09 -0500

    new order-summary markup, order summary style tweaks, and fixed responsiveness on order params

--- public/js/views/order-item-summary-view.js
+++ public/js/views/order-item-summary-view.js
@@ -5,7 +5,7 @@ var OrderItemSummaryView = Backbone.View.extend({
 
   id: function() { return 'order-item-' + this.model.id; },
 
-  template: Handlebars.partials.order_item_summary,
+  template: Handlebars.partials.order_summary,
 
   events: {
     'click .edit': 'openEditModal',

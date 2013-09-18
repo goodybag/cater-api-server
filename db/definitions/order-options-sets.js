f22b4f0402352f822780db6e46a8c7c8e22366cc
Geoffrey Parker <grp@gparker.co>
2013-09-18 17:37:09 -0500

    add tables for options copy on orders

---
+++ db/definitions/order-options-sets.js
@@ -0,0 +1,48 @@
+/**
+ * Options Set Schema
+ */
+
+if (typeof module === 'object' && typeof define !== 'function') {
+  var define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+var
+  types = require('../data-types')
+;
+
+define(function(require) {
+  var definition = {};
+  definition.name = 'options_sets';
+
+  definition.schema = {
+    id: {
+      type: types.serial
+    , pk: true
+    }
+  , created_at: {
+      type: types.timestamptz
+    , nullable: false
+    , default: 'NOW()'
+    }
+  , item_id: {
+      type: types.int
+    , nullable: false
+    , references: {table: 'items', column: 'id'}
+    }
+  , name: {
+      type: types.text
+    , nullable: true
+    }
+  , type: {
+      type: types.optionsettype,
+    , nullable: false
+    , default: 'checkbox'
+    }
+  };
+
+  definition.indices = {};
+
+  return definition;
+});
---
+++ db/definitions/order-options.js
@@ -0,0 +1,51 @@
+/**
+ * Options Schema
+ */
+
+if (typeof module === 'object' && typeof define !== 'function') {
+  var define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+var
+  types = require('../data-types')
+;
+
+define(function(require) {
+  var definition = {};
+  definition.name = 'options';
+
+  definition.schema = {
+    id: {
+      type: types.serial
+    , pk: true
+    }
+  , created_at: {
+      type: types.timestamptz
+    , nullable: false
+    , default: 'NOW()'
+    }
+  , options_set_id: {
+      type: types.int
+    , nullable: false
+    , references: {table: 'options_sets', column: 'id'}
+    }
+  , name: {
+      type: types.text
+    , nullable: false
+    }
+  , price: {
+      type: types.integer
+    , nullable: true
+    }
+  , state: {
+      type: types.bool
+    , nullable: false
+    }
+  };
+
+  definition.indices = {};
+
+  return definition;
+});

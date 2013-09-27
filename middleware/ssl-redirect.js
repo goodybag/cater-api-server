04c958300845b0fc223834b94a86a38c57535ab8
Lalit Kapoor <lalitkapoor@gmail.com>
2013-09-27 10:59:14 -0500

    need config in middleware

--- middleware/ssl-redirect.js
+++ middleware/ssl-redirect.js
@@ -1,3 +1,5 @@
+var config = require('../config');
+
 module.exports =  function(req,res,next) {
   // this is how you check it on heroku, because https terminates before it reaches our app
   (req.headers['x-forwarded-proto']!='https') ? res.redirect(config.baseUrl+req.url) : next();

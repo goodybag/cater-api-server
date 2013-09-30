f3376b330b1657bbc8723d28288daa218424d864
John Fawcett <jrf0110@gmail.com>
2013-09-30 10:01:13 -0500

    don't generated uuids client-side

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,13 +1,6 @@
 (function( exports ){
   var utils = exports.utils = exports.utils || {};
 
-  utils.uuid = function(){
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
-  };
-
   utils.getPrice = function( $el ) {
     var val = $el.val().trim();
     return val ? Math.round(parseFloat(val) * 100) : null;

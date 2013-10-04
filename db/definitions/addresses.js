19f6c2343738fdbce7653d0a1ef332c3f9aef366
Preston Pham <preston@goodybag.com>
2013-10-04 11:04:32 -0500

    Added addresses schema and db setup

---
+++ db/definitions/addresses.js
@@ -0,0 +1,54 @@
+/**
+ * Addresses Schema
+ */
+
+if (typeof module === 'object' && typeof define !== 'function') {
+  var define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+var
+  types = require('../data-types')
+;
+
+define(function(require) {
+  var definition = {};
+  definition.name = 'addresses';
+
+  definition.schema = {
+    id: {
+      type: types.serial
+    , pk: true
+    }
+  , user_id: {
+      type: types.int
+    , nullable: false
+    , references: {table: 'users', column: 'id'}
+    }
+  , street: {
+      type: types.text
+    , nullable: false
+    }
+  , city: {
+      type: types.text
+    , nullable: false
+    }
+  , state: {
+      type: types.varchar(2)
+    , nullable: false
+    }
+  , zip: {
+      type: types.varchar(5)
+    , nullable: false
+    }
+  , is_default: {
+      type: types.bool
+    , nullable: false
+    }
+  };
+
+  definition.indices = {};
+
+  return definition;
+});

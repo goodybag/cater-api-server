b63ee6a8cf3e0fcb38be2f46d44d8d4f2bd70c5c
John Fawcett <jrf0110@gmail.com>
2013-10-08 10:44:07 -0500

    only run deltas that need to run

--- db/tasks/run-deltas.js
+++ db/tasks/run-deltas.js
@@ -8,6 +8,7 @@ var
 
 , options   = {
     deltasDir: '../deltas'
+  , deltasQuery: 'select * from deltas'
   }
 ;
 
@@ -22,24 +23,44 @@ module.exports.run = function( callback ){
   console.log("######################################");
   console.log("");
 
-  // Get file paths of deltas, sort, then get the contents
-  var deltas = fs.readdirSync(
-    path.join( __dirname, options.deltasDir )
-  ).filter( function( file ){
-    console.log('  * ' + file);
+  // Get the deltas that have already been run
+  db.query( options.deltasQuery, function( error, results ){
+    if ( error ) return callback( error );
 
-    return (
-      fs.statSync( path.join( __dirname, options.deltasDir, file ) ).isFile() &&
-      file.slice( -4 ) === '.sql'
+    // Get results in filename format for easier workin' with
+    results = results.map( function( r ){
+      return r.version + '.sql';
+    });
+
+    // Get file paths of deltas not already run, sort, log
+    var deltas = fs.readdirSync(
+      path.join( __dirname, options.deltasDir )
+    ).filter( function( file ){
+      return (
+        fs.statSync( path.join( __dirname, options.deltasDir, file ) ).isFile() &&
+        file.slice( -4 ) === '.sql'
+      );
+    }).filter( function( file ){
+      // Reject files that have already been run
+      return results.indexOf( file ) === -1;
+    }).sort();
+
+    // Log our batch
+    deltas.forEach( function( f ){
+      console.log('  * ' + f);
+    });
+
+    // Get file contents
+    deltas = deltas.map( function( file ){
+      return fs.readFileSync( path.join( __dirname, options.deltasDir, file ) ).toString();
+    });
+
+    async.series(
+      deltas.map( function( delta ){
+        return function( done ){ db.query( delta, done ); }
+      })
+    , callback
     );
-  }).sort().map( function( file ){
-    return fs.readFileSync( path.join( __dirname, options.deltasDir, file ) ).toString();
   });
 
-  async.series(
-    deltas.map( function( delta ){
-      return function( done ){ db.query( delta, done ); }
-    })
-  , callback
-  );
 };

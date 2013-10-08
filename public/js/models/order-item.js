d86c1d608f6eec3984949ac821bf37236f4ae741
Geoffrey Parker <grp@gparker.co>
2013-10-08 14:51:51 -0500

    add order-item schema

--- public/js/models/order-item.js
+++ public/js/models/order-item.js
@@ -1,4 +1,63 @@
 var OrderItem = Backbone.Model.extend({
+  schema: {
+    type: 'object',
+    properties: {
+      quantity: {
+        type: 'integer',
+        minimum: 1,
+        required: true
+      },
+      notes: {
+        type: ['string', 'null'],
+        minLength: 1,
+        required: false
+      },
+      options_sets: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            name: {
+              type: ['string', 'null'],
+              minLength: 1,
+              required: false
+            },
+            type: {
+              type: 'string',
+              enum: ['radio', 'checkbox'],
+              required: true
+            },
+            options: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  name: {
+                    type: 'string',
+                    minLength: 1,
+                    required: true
+                  },
+                  description: {
+                    type: ['string', 'null'],
+                    minLength: 1,
+                    required: false
+                  },
+                  price: {
+                    type: ['integer', 'null'],
+                    required: false
+                  },
+                  state: {
+                    type: 'boolean'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+
   initialize: function(attrs, options) {
     this.on('change:price change:quantity change:options_sets', function() {
       var total = this.get('price');

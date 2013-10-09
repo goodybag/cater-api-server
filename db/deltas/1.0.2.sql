ff7b8ba31b3b9b9833a189ed2bbec16327782caf
Geoffrey Parker <grp@gparker.co>
2013-10-09 13:37:38 -0500

    make add column idempotent

--- db/deltas/1.0.2.sql
+++ db/deltas/1.0.2.sql
@@ -1,6 +1,15 @@
 -- Update version
 insert into deltas (version, date) values ('1.0.2', 'now()');
 
-alter table orders
-      add adjustment_amount integer,
-      add adjustment_description text;
+-- there's no add column if not exists
+DO $$
+    BEGIN
+        BEGIN
+        alter table orders
+                add adjustment_amount integer,
+                add adjustment_description text;
+        EXCEPTION
+            WHEN duplicate_column THEN RAISE NOTICE 'column <column_name> already exists in <table_name>.';
+        END;
+    END;
+$$

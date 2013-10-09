24225b926279c6fd0099f4d8824447dda6bac838
John Fawcett <jrf0110@gmail.com>
2013-10-09 17:52:46 -0500

    make order params view search link update on change, update moment to fix order params middleware

--- middleware/order-params.js
+++ middleware/order-params.js
@@ -15,34 +15,27 @@ var orderParamsFields = [
 
 var transforms = {
   time: function( d, req ){
-    var datetime = moment();
-    var hhmm = d.split(':');
-    // datetime.hour( hhmm[0] );
-    // datetime.minute( hhmm[1] );
-
-    return datetime.format('hh:mm');
+    var datetime = moment( '2013-01-01 ' + d );
+console.log( '2013-01-01 ' + d, datetime.format('HH:mm') )
+    return datetime.format('HH:mm');
   }
 };
 
 module.exports = function(){
   return function( req, res, next ){
-    var orderParams = {}, added = false;
+    req.session.orderParams = {};
 
     for (var i = 0, l = orderParamsFields.length, key; i < l; ++i){
       key = orderParamsFields[i];
 
       if ( !req.param( key ) ) continue;
 
-      added = true;
-
       // Add transformed value
-      orderParams[ key ] = ( key in transforms
+      req.session.orderParams[ key ] = ( key in transforms
         ? transforms[ key ]( req.param( key ), req )
         : req.param( key )
       );
     }
-
-    if ( added ) req.session.orderParams = orderParams;
 
     next();
   };
--- public/js/views/order-params-view.js
+++ public/js/views/order-params-view.js
@@ -3,12 +3,15 @@ var OrderParamsView = Backbone.View.extend({
     'submit form': 'onFormSubmit'
   , 'click .form-group': 'focusInputs'
   , 'click .btn-search': 'onSearchClick'
+  , 'keyup input':       'onInputChange'
   }
 
 , initialize: function() {
     // the OrderParams model should be passed in
     // the el should also be passed in
     this.listenTo(this.model, 'change', this.updateFields, this);
+
+    this.onInputChange = _.debounce( _.bind( this.updateSearchHref, this ), 300 );
 
     this.datepicker = this.$el.find("input[name='date']").eq(0).pickadate({
       format: 'mm/dd/yyyy'
@@ -19,6 +22,9 @@ var OrderParamsView = Backbone.View.extend({
       format: 'h:i A'
     , interval: 15
     }).pickatime('picker');
+
+    this.datepicker.on( 'set', this.onInputChange );
+    this.timepicker.on( 'set', this.onInputChange );
 
     this.$searchBtn = this.$el.find('.btn-search');
   }
@@ -54,13 +60,19 @@ var OrderParamsView = Backbone.View.extend({
     return {
       zip: this.$("input[name='zip']").val() || null
     , date: (this.datepicker.get()) ? dateTimeFormatter(this.datepicker.get()) : null
-    , time: (this.timepicker.get()) ? timeFormatter(this.timepicker.get()) : null
+    , time: this.timepicker.get()
     , guests: this.$("input[name='guests']").val() || null
     };
   }
 
+, updateSearchHref: function(){
+  console.log('update')
+    this.$searchBtn.attr( 'href',  utils.queryParams( this.getProps() ) );
+    return this;
+  }
+
 , onSearchClick: function(e){
-    e.target.href = utils.queryParams( this.getProps() );
+    this.updateSearchHref();
   }
 
 , onFormSubmit: function (e) {

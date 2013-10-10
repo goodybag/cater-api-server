f20070ffd98b9f2a539906debda6b61eb3937711
Geoffrey Parker <grp@gparker.co>
2013-10-10 11:22:23 -0500

    forgot column type

--- db/deltas/1.0.2.sql
+++ db/deltas/1.0.2.sql
@@ -5,7 +5,7 @@ insert into deltas (version, date) values ('1.0.2', 'now()');
 DO $$
     BEGIN
         BEGIN
-        ALTER TABLE restaurants ADD is_hidden NOT NULL DEFAULT TRUE;
+        ALTER TABLE restaurants ADD is_hidden boolean NOT NULL DEFAULT TRUE;
         EXCEPTION
             WHEN duplicate_column THEN RAISE NOTICE 'column <column_name> already exists in <table_name>.';
         END;

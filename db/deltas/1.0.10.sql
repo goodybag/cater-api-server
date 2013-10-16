22b96204c2ab7b4169056c2fc3de93d4ccf420f4
Preston Pham <preston@goodybag.com>
2013-10-16 17:28:45 -0500

    updated schema

--- db/deltas/1.0.10.sql
+++ db/deltas/1.0.10.sql
@@ -2,21 +2,18 @@
 insert into deltas (version, date) values ('1.0.10', 'now()');
 
 -- #278: Dietary Tags
-DROP TYPE if exists "tag_type" CASCADE;
-CREATE TYPE "tag_type" AS ENUM (
-  'Vegan', 
-  'Vegetarian', 
-  'Gluten Free'
+CREATE TABLE if not exists "tags" (
+  name text primary key
 );
 
 CREATE TABLE if not exists "item_tags" (
-  id                  serial primary key,
-  item_id             int references items(id) on delete cascade,
-  tag                 tag_type not null
+  item_id   int  references items(id)  on delete cascade,
+  tag       text references tags(name) on delete cascade,
+  primary key (item_id, tag)
 );
 
 CREATE TABLE if not exists "restaurant_tags" (
-  id                  serial primary key,
-  restaurant_id       int references restaurants(id) on delete cascade,
-  tag                 tag_type not null
+  restaurant_id   int  references restaurants(id) on delete cascade,
+  tag             text references tags(name)      on delete cascade,
+  primary key (restaurant_id, tag)
 );

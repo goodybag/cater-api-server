69a9fb179c1c848a42c2ad98a79ca326b2f7f592
John Fawcett <jrf0110@gmail.com>
2013-10-23 15:15:18 -0500

    only build receipt when its requested and it does not exist

--- middleware/build-receipt.js
+++ middleware/build-receipt.js
@@ -1,11 +1,35 @@
+/**
+ * Middleware: Build Receipt
+ *
+ * Ensure that a PDF version of a receipt exists. If it does not,
+ * send add a build job to the receipt queue.
+ *
+ * usage: app.get('/route', m.buildReceipt(), ... )
+ */
+
+var path    = require('path');
+var fs      = require('fs');
 var receipt = require('../lib/receipt');
+var config  = require('../config');
 
 module.exports = function(){
   return function( req, res, next ){
     if ( !req.param('oid') ) return next();
 
-    receipt.build( +req.param('oid'), function( error, result ){
-      next();
+    var filename = path.resolve(
+      __dirname, '../'
+    , config.receipt.dir
+    , config.receipt.fileName.replace( ':id', req.param('oid') )
+    );
+
+    fs.exists( filename, function( exists ){
+      if ( exists ) return next();
+
+      receipt.build( +req.param('oid'), function( error, result ){
+        if ( error ) return res.status(500).render('500');
+
+        next();
+      });
     });
   }
 };

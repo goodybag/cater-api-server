8b79ed4f552c054a4409017aeefea1913270673d
Preston Pham <preston@goodybag.com>
2013-10-24 12:27:17 -0500

    added dairy free

--- db/deltas/1.0.11.sql
+++ db/deltas/1.0.11.sql
@@ -1,9 +1,9 @@
 -- Update version
 insert into deltas (version, date) values ('1.0.11', 'now()');
 
--- #366: Add Kosher, Halal tags
+-- #366: Add Kosher, Halal, Dairy-Free tags
 
 INSERT INTO tags (name)
 SELECT existing.*
-FROM (SELECT unnest(array['kosher','halal']) as tag) AS existing
+FROM (SELECT unnest(array['kosher','halal', 'dairyFree']) as tag) AS existing
 WHERE NOT EXISTS (SELECT name from tags WHERE existing.tag = tags.name);

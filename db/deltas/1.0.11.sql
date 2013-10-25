801bfad560b6344c13a958ad37dc88bcb41bfe96
Lalit Kapoor <lalitkapoor@gmail.com>
2013-10-25 18:11:36 -0500

    update delta to catch execptions

--- db/deltas/1.0.11.sql
+++ db/deltas/1.0.11.sql
@@ -1,31 +1,39 @@
 -- Update version
 insert into deltas (version, date) values ('1.0.11', 'now()');
 
-alter table "orders" add column "status" order_status default 'pending';
+DO $$
+  BEGIN
+    BEGIN
+      alter table "orders" add column "status" order_status default 'pending';
+    EXCEPTION
+      WHEN duplicate_column THEN RAISE NOTICE 'column <column_name> already exists in <table_name>.';
+    END;
+  END;
+$$;
 
-update orders 
+update orders
 set status = "s"."status"
 from (
-  select "statuses"."order_id", "statuses"."status" 
-  from "order_statuses" "statuses" 
+  select "statuses"."order_id", "statuses"."status"
+  from "order_statuses" "statuses"
   inner join (
-    select "order_statuses"."order_id", max(created_at) as created_at 
+    select "order_statuses"."order_id", max(created_at) as created_at
     from "order_statuses"
     group by "order_statuses"."order_id"
-  ) "recent" 
+  ) "recent"
   on "recent"."order_id" = "statuses"."order_id" and "recent"."created_at" = "statuses"."created_at"
 ) s
 where "orders"."id" = "s"."order_id"
 ;
 
-create function process_updated_order() returns trigger as $process_updated_order$
+create or replace function process_updated_order() returns trigger as $process_updated_order$
   begin
     insert into order_statuses(order_id, status) values(NEW.id, NEW.status);
     return null;
   end
 $process_updated_order$ language plpgsql;
 
-create function process_new_order() returns trigger as $process_new_order$
+create or replace function process_new_order() returns trigger as $process_new_order$
   begin
     insert into order_statuses(order_id, status) values(NEW.id, NEW.status);
     return null;

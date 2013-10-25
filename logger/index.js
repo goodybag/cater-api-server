43709afde99a4cfa1f6c52450bea508f05f4c703
Lalit Kapoor <lalitkapoor@gmail.com>
2013-10-25 18:11:59 -0500

    add debit logger

--- logger/index.js
+++ logger/index.js
@@ -2,6 +2,7 @@ var setup = require('./setup');
 
 module.exports.routes = setup({app: 'cater', component: 'routes'});
 module.exports.models = setup({app: 'cater', component: 'models'});
+module.exports.debit = setup({app: 'cater', component: 'debit'});
 module.exports.db = setup({app: 'cater', component: 'db'});
 
 var expressLogger = setup({app: 'cater', component: 'express'});

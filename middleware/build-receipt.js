629e81cf1112176c637d07f9b602c910d031fc2a
John Fawcett <jrf0110@gmail.com>
2013-10-28 11:14:44 -0500

    add force rebuild query parameter

--- middleware/build-receipt.js
+++ middleware/build-receipt.js
@@ -2,7 +2,8 @@
  * Middleware: Build Receipt
  *
  * Ensure that a PDF version of a receipt exists. If it does not,
- * send add a build job to the receipt queue.
+ * send a build job to the receipt queue. If rebuild=true is in
+ * the query params, the receipt will forcefully re-build.
  *
  * usage: app.get('/route', m.buildReceipt(), ... )
  */
@@ -16,18 +17,23 @@ module.exports = function(){
   return function( req, res, next ){
     if ( !req.param('oid') ) return next();
 
-    var filename = path.resolve(
-      __dirname, '../'
-    , config.receipt.dir
-    , config.receipt.fileName.replace( ':id', req.param('oid') )
-    );
+    (function( buildReceipt ){
+      if ( req.param('rebuild') ) return buildReceipt();
 
-    fs.exists( filename, function( exists ){
-      if ( exists ) return next();
+      var filename = path.resolve(
+        __dirname, '../'
+      , config.receipt.dir
+      , config.receipt.fileName.replace( ':id', req.param('oid') )
+      );
 
+      fs.exists( filename, function( exists ){
+        if ( exists ) return next();
+
+        buildReceipt();
+      });
+    })( function(){
       receipt.build( +req.param('oid'), function( error, result ){
         if ( error ){
-          console.log(error);
           if ( 'httpStatus' in error && error.httpStatus === 404 ){
             return res.status(404).render('404');
           }

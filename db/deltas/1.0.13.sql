3038a68c3bcd7aaea1c481681293a0ad28f85761
Preston Pham <preston@goodybag.com>
2013-10-28 13:45:21 -0500

    added delta

---
+++ db/deltas/1.0.13.sql
@@ -0,0 +1,19 @@
+-- Update version
+insert into deltas (version, date) values ('1.0.13', 'now()');
+
+-- #369 - Add address fields to restaurants, orders schema
+
+DO $$
+  BEGIN
+    BEGIN
+      ALTER TABLE restaurants
+        ADD COLUMN street2 text,
+        ADD COLUMN delivery_instructions text;
+      ALTER TABLE orders
+        ADD COLUMN street2 text,
+        ADD COLUMN delivery_instructions text;
+    EXCEPTION
+      WHEN duplicate_column THEN RAISE NOTICE 'column <column_name> already exists in <table_name>.';
+    END;
+  END;
+$$;

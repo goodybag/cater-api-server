f8533a685b3f2217d2512b44d82169f1e607ee8d
Lalit Kapoor <lalitkapoor@gmail.com>
2013-10-30 18:10:54 -0500

    adding additional failsafe check

--- workers/debit/index.js
+++ workers/debit/index.js
@@ -5,6 +5,18 @@ var models = require('../../models');
 
 var _ = utils._;
 
+var checkForExistingDebit = function (order, callback) {
+  var TAGS = [process.domain.uuid];
+  utils.balanced.Debits.list({'meta.order_id': order.attributes.id}, function (error, debits) {
+    if (error) return callback(error);
+
+    //TODO: come up with a better solution for identifying a transaction for an order (some kind of uuid)
+    if (debits && debits.total > 1) return callback(new Error('multiple debits for a single order')); // this is going to be an issue for testing in dev/staging
+    if (debits && debits.total == 1) callback (null, debits.items[0]);
+    return callback(null, null);
+  });
+}
+
 var debitCustomer = function (order, callback) {
   var TAGS = [process.domain.uuid];
 
@@ -15,10 +27,10 @@ var debitCustomer = function (order, callback) {
   , customer_uri: order.attributes.user.balanced_customer_uri
   , on_behalf_of_uri: order.attributes.restaurant.balanced_customer_uri
   , appears_on_statement_as: 'GB ORDER #'+ order.attributes.id
-  , meta: {
-      user: {id: order.attributes.user.id}
-    , restaurant: {id: order.attributes.restaurant.id}
-    , order: {id: order.attributes.id}
+  , meta: { // note, cannot search on nested properties so keep searchable properties top-level
+      user_id: order.attributes.user.id
+    , restaurant_id: order.attributes.restaurant.id
+    , order_id: order.attributes.id
     }
   }, function (error, debit) {
     if (error) return order.setPaymentError(error.uri, error, callback);
@@ -44,28 +56,42 @@ var task = function (message, callback) {
     if(!order) return utils.queues.debit.del(message.id, utils.noop), callback();
     if (_.contains(['invoiced', 'paid'], order.payment_status)) return utils.queues.debit.del(message.id, utils.noop), callback();
 
-    // if the status is processing there is no need to set the payment status to processing again
-    if (order.payment_status != 'processing') {
-      order.attributes.payment_status = 'processing';
-      order.save(function (error) {
-        if (error) return logger.db.error(TAGS, error), utils.rollbar.reportMessage(error), callback(error);
-        debitCustomer(order, function(error) {
+    // check to see if a debit was already successfuly processed for this order
+    // if so it means that there was an error in updating our system with the
+    // debit information and we should attempt to update our system again.
+    checkForExistingDebit(order, function (error, debit) {
+      if (error) return callback(error); // attempt to process this from the queue again later
+
+      if (debit) {
+        logger.debit.info(TAGS, 'found existing debit for order: ' + order.attributes.id);
+        return order.setPaymentPaid('debit', debit.uri, debit, callback);
+      }
+
+      // if no debit then continue to process
+
+      // if the status is processing there is no need to set the payment status to processing again
+      if (order.attributes.payment_status != 'processing') {
+        order.attributes.payment_status = 'processing';
+        order.save(function (error) {
+          if (error) return logger.db.error(TAGS, error), utils.rollbar.reportMessage(error), callback(error);
+          debitCustomer(order, function (error) {
+            utils.queues.debit.del(message.id, utils.noop);
+            if (error) logger.debit.error(TAGS, error);
+            return callback(error);
+          });
+        });
+      } else {
+        debitCustomer(order, function (error) {
           utils.queues.debit.del(message.id, utils.noop);
           if (error) logger.debit.error(TAGS, error);
           return callback(error);
         });
-      });
-    } else {
-      debitCustomer(order, function(error) {
-        utils.queues.debit.del(message.id, utils.noop);
-        if (error) logger.debit.error(TAGS, error);
-        return callback(error);
-      });
-    }
+      }
+    });
   });
 };
 
-var worker = function(message, callback) {
+var worker = function (message, callback) {
   var d = domain.create();
   d.uuid = utils.uuid.v4();
   d.on('error', function (error) {

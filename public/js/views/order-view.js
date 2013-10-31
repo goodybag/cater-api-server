a23b5acc33baa4fc47489aafabb0e02a5d5b02e7
Geoffrey Parker <grp@gparker.co>
2013-10-31 14:05:47 -0500

    abstract out non checkout page specific stuff

---
+++ public/js/views/order-view.js
@@ -0,0 +1,55 @@
+var OrderView = FormView.extend({
+  events: {
+    'click .btn-cancel': 'cancel',
+  },
+
+  fieldMap: {
+    datetime: '.order-datetime',
+    guests: '#order-guests',
+    name: '.order-name',
+    notes: '#order-notes',
+    // adjustment: '.adjustment .form-control',
+    tip: '.order-tip',
+    tip_percent: '.tip-percent'
+  },
+
+  fieldGetters: {
+    guests: _.partial(FormView.intGetter, 'guests'),
+
+    tip: _.partial(FormView.dollarsGetter, 'tip'),
+
+    datetime: function() {
+      var date = this.$el.find("#order-form #order-date").val().trim();
+      var time = this.$el.find("#order-form #order-time").val().trim();
+      var datepart = date ? dateTimeFormatter(date) : null;
+      var timepart = time ? timeFormatter(time, 'HH:mm:ss') : null;
+
+
+      if(!datepart || !timepart) return null;
+
+      // since we cannot determine offset, cannot format as ISO 8601 String
+      // using "YYYY-MM-DD HH:mm:ss" to represent the date and time
+      var datetime = datepart + ' ' + timepart;
+      var date = moment(datetime);
+      return date.isValid() ? datetime : null;
+    }
+  },
+
+  getDiff: function() {
+    var diff = FormView.prototype.getDiff.apply(this, arguments);
+    var addrDiff = this.addressView.getDiff.apply(this.addressView, arguments)
+    return diff || addrDiff ? _.extend({}, diff, addrDiff) : null;
+  },
+
+  initialize: function(options) {
+    this.addressView = new OrderAddressView({el: '.delivery-info', model: this.model.address, orderView: this});
+    this.tipView = new TipView({el: '.tip-area', model: this.model, orderView: this});
+  },
+
+  cancel: function() {
+    this.model.changeStatus('canceled', function(err, data) {
+      if (err) return alert(err); // TODO: error handling
+      window.location.reload();
+    });
+  }
+});

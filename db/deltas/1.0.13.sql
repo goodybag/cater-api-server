9e250959e406dce164c86a8e9c568336abd6d6b1
John Fawcett <jrf0110@gmail.com>
2013-11-04 16:15:06 -0600

    comment out broken delta, fix version

--- db/deltas/1.0.13.sql
+++ db/deltas/1.0.13.sql
@@ -1,40 +1,40 @@
 begin;
 -- Update version
-insert into deltas (version, date) values ('1.0.11', 'now()');
+insert into deltas (version, date) values ('1.0.13', 'now()');
 
 -- fix some address stuff, especially uniqueness
-UPDATE addresses SET street2='' WHERE street2 IS NULL;
+-- UPDATE addresses SET street2='' WHERE street2 IS NULL;
 
-ALTER TABLE addresses
-      ALTER COLUMN street SET NOT NULL,
-      ALTER COLUMN city SET NOT NULL,
-      ALTER COLUMN "state" SET NOT NULL,
-      ALTER COLUMN zip SET NOT NULL,
-      ALTER COLUMN is_default SET NOT NULL,
-      ALTER COLUMN phone SET NOT NULL,
-      ALTER COLUMN street2 SET NOT NULL,
-      ALTER COLUMN street2 SET DEFAULT '';
+-- ALTER TABLE addresses
+--       ALTER COLUMN street SET NOT NULL,
+--       ALTER COLUMN city SET NOT NULL,
+--       ALTER COLUMN "state" SET NOT NULL,
+--       ALTER COLUMN zip SET NOT NULL,
+--       ALTER COLUMN is_default SET NOT NULL,
+--       ALTER COLUMN phone SET NOT NULL,
+--       ALTER COLUMN street2 SET NOT NULL,
+--       ALTER COLUMN street2 SET DEFAULT '';
 
-ALTER TABLE addresses DROP CONSTRAINT IF EXISTS addresses_pkey cascade;
+-- ALTER TABLE addresses DROP CONSTRAINT IF EXISTS addresses_pkey cascade;
 
-ALTER TABLE addresses ALTER COLUMN id SET NOT NULL;
-ALTER TABLE addresses ADD UNIQUE (id);
+-- ALTER TABLE addresses ALTER COLUMN id SET NOT NULL;
+-- ALTER TABLE addresses ADD UNIQUE (id);
 
-ALTER TABLE addresses ADD PRIMARY KEY (user_id, street, street2, zip);
+-- ALTER TABLE addresses ADD PRIMARY KEY (user_id, street, street2, zip);
 
-ALTER TABLE restaurants ADD FOREIGN KEY(address_id) REFERENCES addresses(id);
+-- ALTER TABLE restaurants ADD FOREIGN KEY(address_id) REFERENCES addresses(id);
 
 
--- add tip percentage to orders
-DO $$
-  BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'tip_percentage') THEN
-      CREATE TYPE tip_percentage AS ENUM('0', 'custom', '5', '10', '15', '18', '20', '25');
-    END IF;
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'orders' AND column_name = 'tip_percent') THEN
-      ALTER TABLE orders ADD COLUMN tip_percent tip_percentage;
-    END IF;
-  END;
-$$;
+-- -- add tip percentage to orders
+-- DO $$
+--   BEGIN
+--     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'tip_percentage') THEN
+--       CREATE TYPE tip_percentage AS ENUM('0', 'custom', '5', '10', '15', '18', '20', '25');
+--     END IF;
+--     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'orders' AND column_name = 'tip_percent') THEN
+--       ALTER TABLE orders ADD COLUMN tip_percent tip_percentage;
+--     END IF;
+--   END;
+-- $$;
 
-commit;
+-- commit;

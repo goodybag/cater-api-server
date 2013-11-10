bfc0caaa7c4d3331df4c545090461cff401084d2
Lalit Kapoor <lalitkapoor@gmail.com>
2013-11-10 00:42:00 -0600

    add to res.locals

--- middleware/get-user.js
+++ middleware/get-user.js
@@ -3,9 +3,14 @@ var models = require('../models');
 module.exports = function(req, res, next) {
   if(!req.session || !req.session.user || req.session.user.id == null) return next();
 
-  models.User.findOne({where: {id: req.session.user.id}}, function(error, user){
+  models.User.findOne({where: {id: req.session.user.id}}, function (error, user) {
     if (error) return next(error);
+
     req.user = user;
+
+    res.locals.user = user.toJSON();
+    delete res.locals.user.password;
+
     next();
   });
 }

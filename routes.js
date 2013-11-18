8b76c76325fe856ea32177bd97a24688e12619e9
Geoffrey Parker <grp@gparker.co>
2013-11-18 13:43:12 -0600

    re-enable auth for all order subresources

--- routes.js
+++ routes.js
@@ -193,7 +193,7 @@ module.exports.register = function(app) {
    *  Order resource.  An individual order.
    */
 
-  app.all('/orders/:id', controllers.orders.auth);
+  app.all('/orders/:id/?*', controllers.orders.auth);
 
   app.get('/orders/:id'
     // If they're using ?receipt=true, make sure we restrict the group

a0349896db8e4a35686102d32c8d8b0ebe53679c
John Fawcett <jrf0110@gmail.com>
2013-11-19 10:49:22 -0600

    add spinner module
    Signed-off-by: John Fawcett <jrf0110@gmail.com>

---
+++ public/js/spinner.js
@@ -0,0 +1,64 @@
+var spinner = (function(){
+  'use strict';
+
+  var options = {
+    lines: 13 // The number of lines to draw
+  , length: 20 // The length of each line
+  , width: 10 // The line thickness
+  , radius: 30 // The radius of the inner circle
+  , corners: 1 // Corner roundness (0..1)
+  , rotate: 0 // The rotation offset
+  , direction: 1 // 1: clockwise, -1: counterclockwise
+  , color: '#000' // #rgb or #rrggbb or array of colors
+  , speed: 1 // Rounds per second
+  , trail: 60 // Afterglow percentage
+  , shadow: false // Whether to render a shadow
+  , hwaccel: false // Whether to use hardware acceleration
+  , className: 'spinner' // The CSS class to assign to the spinner
+  , zIndex: 2e9 // The z-index (defaults to 2000000000)
+  , top: 'auto' // Top position relative to parent in px
+  , left: 'auto' // Left position relative to parent in px
+  };
+
+  var exports = {
+    start: function( target ){
+      if ( target ){
+        return new Spinner().spin( target );
+      }
+
+      exports.$holder.css( 'display', 'block' );
+    }
+
+  , stop: function(){
+      exports.$holder.css( 'display', 'none' );
+    }
+  };
+
+  $(function(){
+    exports.$holder = $('<div id="spinner-holder"></div>');
+
+    exports.$holder.css({
+      position: 'fixed'
+    , top: 0
+    , left: 0
+    , width: '100%'
+    , height: '100%'
+    , background: "rgba(0, 0, 0, 0.1)"
+    , display: 'none'
+    });
+
+    // Something stupid is happening somewhere
+    setTimeout( function(){
+      $( document.body ).append( exports.$holder );
+    }, 1000 );
+
+    exports.main = new Spinner( options ).spin( exports.$holder[0] );
+
+    $( exports.main.el ).css({
+      left: '50%'
+    , top: '240px'
+    });
+  });
+
+  return exports;
+})();

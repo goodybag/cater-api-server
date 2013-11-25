3d44f6d1cb745e9702fa28a089a3e9ae819b26f4
John Fawcett <jrf0110@gmail.com>
2013-11-25 16:40:25 -0600

    initial scraper

--- db/deltas/1.1.3.sql
+++ db/deltas/1.1.3.sql
@@ -3,19 +3,28 @@
 DO $$
   declare version       text := '1.1.3';
   declare tbl_name      text := 'restaurants';
-  declare col_name      text := 'yelp_business_id';
-  declare col_type      text := 'text';
+  declare col_name      text;
+  declare col_type      text;
 begin
   raise notice '## Running Delta v% ##', version;
-
-  raise notice 'Using col_name: %, tbl_name: %', col_name, tbl_name;
 
   -- Update version
   execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
 
   -- Add col
+  col_name := 'yelp_business_id';
+  col_type := 'text';
   if not exists ( select 1 from information_schema.columns where table_name = tbl_name and column_name = col_name ) then
     raise notice 'Adding column `%` to table `%`', col_name, tbl_name;
     execute 'alter table "' || tbl_name || '" add column "' || col_name || '" ' || col_type;
+  end if;
+
+  col_name := 'yelp_data';
+  col_type := 'json';
+  if not exists ( select 1 from information_schema.columns where table_name = tbl_name and column_name = col_name ) then
+    raise notice 'Adding column `%` to table `%` type `%`', col_name, tbl_name, col_type;
+    alter table "restaurants" add column "yelp_data" json default '{}';
+    -- this is not effing working
+    -- execute "alter table " || tbl_name || " add column " || col_name || " " || col_type || " default '{}'";
   end if;
 end$$;
---
+++ workers/yelp/index.js
@@ -0,0 +1,80 @@
+/**
+ * Yelp Worker
+ */
+
+var util      = require('util');
+var utils     = require('../utils');
+var Models    = require('../models');
+
+var stats = {
+  businessesAttempted:  { text: 'Businesses attempted', value: 0 }
+, completed:            { text: 'Businesses completed', value: 0 }
+};
+
+var tickOut = function( val ){
+  util.puts( val ? 'x' : '.' );
+};
+
+var logStats = function(){
+  console.log("\n\n");
+  console.log("*########################*");
+  console.log("*##### Lovely Stats #####*");
+  console.log("*########################*");
+  console.log("\n\n");
+  for ( var key in stats ){
+    console.log("  ", stats[ key ].text, ": ", stat[ key ].value);
+  }
+  console.log("\n\n");
+};
+
+var onComplete = function(){
+  logStats();
+};
+
+var onError = function( error ){
+  if ( error ){
+    console.log( error );
+    process.exit(1);
+  }
+};
+
+var getGbBusinesses = function( callback ){
+
+};
+
+var getBusinessYelpData = function( id, callback ){
+
+};
+
+var saveBusinessYelpData = function( id, data, callback ){
+
+};
+
+getBusinesses( function( error, businesses ){
+  if ( error ) return onError( error );
+
+  stats.businessesAttempted.value = businesses.length;
+
+  var fns = businesses.map( function( business ){
+
+    return function( done ){
+      var localOnError = function( error ){
+        if ( error ) return console.log( error ), done();
+      };
+
+      getBusinessYelpData( business.id, function( error, data ){
+        if ( error ) return localOnError( error );
+
+        saveBusinessYelpData( business.id, data, function( error ){
+          if ( error ) return console.log( error ), done();
+
+          stats.completed.value++;
+          tickOut();
+          done();
+        });
+      });
+    };
+  });
+
+  utils.series( fns );
+});

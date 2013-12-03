9c9608d589c1e682354bc3ada228548ccec575f2
Geoffrey Parker <grp@gparker.co>
2013-12-03 14:11:01 -0600

    getChange actually working

--- models/change.js
+++ models/change.js
@@ -1,20 +1,25 @@
 var Model = require('./model');
-var Order = require('./order.js');
+var Order = require('./order');
 
 module.exports = Model.extend({}, {
   table: 'order_changes',
-  getChange: function(orderId, callback) {
+  getChange: function(orderId, isAdmin, callback) {
+    if (callback == null) {
+      callback = isAdmin;
+      isAdmin = false;
+    }
+    var self = this;
     Order.findOne(orderId, function(err, order) {
-      if (err) callback(err);
-      if (!order) callback(404);
+      if (err) return callback(err);
+      if (!order) return callback(404);
 
       // Check if change can be made.  Order must be in accepted state and still be cancelable.
-      if (!(order.attributes.status === 'accepted' && order.toJSON().cancelable))
-        callback(403);
+      if (!isAdmin && !(order.attributes.status === 'accepted' && order.toJSON().cancelable))
+        return callback(403);
 
-      this.findOne({order_id: orderId}, function(err, change) {
-        if (err) callback(err);
-        if (!change) change = new this({order_id: orderId, order_json: order.toJSON()});
+      self.findOne({order_id: orderId}, function(err, change) {
+        if (err) return callback(err);
+        if (!change) change = new self({order_id: orderId, order_json: order.toJSON()});
         return callback(null, change);
       });
     });

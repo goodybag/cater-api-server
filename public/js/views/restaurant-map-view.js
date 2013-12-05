019990b3c8494933ac9b91cad68e5e51476a9b73
Preston Pham <prestonp08@gmail.com>
2013-12-05 01:14:57 -0600

    adding initial view files

---
+++ public/js/views/restaurant-map-view.js
@@ -0,0 +1,17 @@
+/**
+ * Dependencies:
+ *   - Leaflet.js
+ *
+ * Model:
+ *   - Restaurant
+ */
+
+var RestaurantMapView = Backbone.View.extend({
+  initialize: function() {
+
+  },
+
+  render: function() {
+
+  }
+});

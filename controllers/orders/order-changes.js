b6b72bcd59244cb780eda5a8a24ff9c5e13f621c
Geoffrey Parker <grp@gparker.co>
2013-12-09 14:50:37 -0600

    abstract commonalities out to decorator

--- controllers/orders/order-changes.js
+++ controllers/orders/order-changes.js
@@ -20,44 +20,50 @@ module.exports.get = function(req, res, next) {
   });
 };
 
-// NOTE: We will need to do much the same thing for order item create / update / delete.
-// Once that is done, commonalities should be abstracted out, probably to the model.
-module.exports.orderUpdate = function(order, req, res, next) {
+
+// Decorator for creating / updating changes
+// Takes a function which takes a change object and a request and modifies the change object appropriately
+//
+function changer(func) {
+  return function(order, req, res, next) {
+  var isAdmin = req.session.user && utils.contains(req.session.user.groups, 'admin');
+  models.Change.getChange(order, isAdmin, function(err, change) {
+    if (err)
+      return utils.contains([404, 403], err) ? res.json(err, {}) : res.error(errors.internal.DB_FAILURE, err);
+
+    func(change, req, function(err) {
+      if (err) return res.error(errors.internal.UNKNOWN, err);
+      change.save(function(err, rows, result) {
+        if (err) return res.error(errors.internal.DB_FAILURE, err);
+        res.json(201, change.toJSON());  // TODO: is this best?
+      });
+    });
+  };
+};
+
+module.exports.orderUpdate = changer(function(change, req, done) {
   // like update, but create a pending change instead of applying it immedietly
-  var isAdmin = req.session.user && utils.contains(req.session.user.groups, 'admin');
-  models.Change.getChange(req.params.oid, isAdmin, function(err, change) {
-    if (err)
-      return utils.contains([404, 403], err) ? res.json(err, {}) : res.error(errors.internal.DB_FAILURE, err);
-
-    var delta = utils.pick(req.body, models.Order.updateableFields);
-    var changeSummaries = utils.map(delta, function(val, key, obj) {
-      return ['Change', key, 'from', order.attributes[key], 'to', val].join(' ');
-    });
-
-    utils.extend(change.attributes.order_json, delta);
-    change.attributes.change_summaries = (change.attributes.change_summaries || []).concat(changeSummaries);
-
-    change.save(function(err, rows, result) {
-      if (err) return res.error(errors.internal.DB_FAILURE, err);
-      res.json(201, change.toJSON());  // TODO: is this best?
-    });
+  var delta = utils.pick(req.body, models.Order.updateableFields);
+  var changeSummaries = utils.map(delta, function(val, key, obj) {
+    return ['Change', key, 'from', order.attributes[key], 'to', val].join(' ');
   });
-};
 
+  utils.extend(change.attributes.order_json, delta);
+  change.attributes.change_summaries = (change.attributes.change_summaries || []).concat(changeSummaries);
+  done();
+});
 
-module.exports.addItem = function(order, req, res, next) {
-  var isAdmin = req.session.user && utils.contains(req.session.user.groups, 'admin');
-  models.Change.getChange(req.params.oid, isAdmin, function(err, change) {
-    if (err)
-      return utils.contains([404, 403], err) ? res.json(err, {}) : res.error(errors.internal.DB_FAILURE, err);
-  });
-};
+module.exports.addItem = changer(function(change, req, done) {
+  done();
+});
 
-module.exports.removeItem = function(order, req, res, next) {
-};
+module.exports.removeItem = changer(function(change, req, done) {
+  done();
+});
 
-module.exports.updateItem = function(order, req, res, next) {
-};
+module.exports.updateItem = changer(function(change, req, done) {
+  done();
+});
 
 module.exports.update = function(req, res, next) {
   var adminUpdates = utils.omit(req.body, ['status']);

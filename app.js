6bc300fda5413311cd6bcd92f65242d740cb203c
John Fawcett <jrf0110@gmail.com>
2013-12-11 16:54:56 -0600

    move static file server before unnecessary middleware

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var app = module.exports = express();
 app.configure(function(){
   app.use(express.favicon(__dirname + '/public/favicon.ico'));
   app.use(express.compress());
+  app.use(express.static(__dirname + '/public'));
+
   app.use(logger.expressError);
   app.use(express.cookieParser('WOOT THE FUCK'));
   app.use(express.cookieSession());
@@ -44,8 +46,8 @@ app.configure(function(){
   app.use(middleware.domains);
   app.use(middleware.cors);
 
-  app.use(middleware.getUser);
-
+  app.use(middleware.getUser());
+app.use( function fuck(req, res, next){ console.log('ohai'); next(); })
   app.use(middleware.requestLogger({
     connStr: config.requestLogger.connStr
   , table: config.requestLogger.table
@@ -119,4 +121,3 @@ hbs.registerPartials('./public/partials');
 });
 
 routes.register(app);
-app.use(express.static(__dirname + '/public'));

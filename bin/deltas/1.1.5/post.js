939d0ada37ae0f4cf15818f8674a02b043afd5ea
Preston Pham <preston@goodybag.com>
2013-12-12 14:49:48 -0600

    added update script

---
+++ bin/deltas/1.1.5/post.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+var utils     = require('../../../utils')
+  , models    = require('../../../models')
+  , db        = require('../../../db')
+  , queries   = require('../../../db/queries');
+
+
+var tasks = [
+  function getCategories(callback) {
+    models.Category.find({limit: 1000}, function(error, rows) {
+      if (error) return callback(error);
+
+      console.log('Updating', rows.length, 'categories...');
+      var categories = utils.invoke(rows, 'toJSON');
+      callback(null, categories);
+    });
+  },
+
+  function updateCategories(categories, callback) {
+    var labels = ['[Group]', '[Individual]', '[ Group ]', '[ Individual ]'];
+
+    var updateTasks = [];
+    utils.each(categories, function(cat) {
+
+      // Strip category labels off name
+      var menus = [];
+
+      utils.each(labels, function(label) {
+        if (cat.name.indexOf(label) >= 0) {
+          cat.name = cat.name.replace(label, '').trim();
+          menus.push(label.replace(/[\[\]]/g, '"').toLowerCase().trim());
+        }
+      });
+
+      // Update `name` and `menus` column
+      var updates = {
+        name: cat.name
+      , menus: '{' + menus.join(',') + '}'
+      };
+
+      // Default unlabeled categories to group
+      if (updates.menus === '{}') {
+        updates.menus = '{groups}';
+      }
+
+      var query = queries.category.update(updates, cat.id);
+      var sql = db.builder.sql(query);
+      updateTasks.push(function(next) {
+        db.query(sql.query, sql.values, function(error, rows, result) {
+          if (error) return next(error);
+          next(null);
+        });
+      });
+    });
+
+    var limit = 5;
+
+    // Update in batches 
+    utils.async.parallelLimit(updateTasks, limit, function(error, results) {
+      if (error) return callback(error);
+      callback(null);
+    });
+  }
+];
+
+utils.async.waterfall(tasks, function(err, result) {
+  if (err) return console.log(err);
+  console.log('Completed processing category labels and menus');
+});

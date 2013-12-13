1b3fd6c3227ec90e767bcb2b3e4b864fc8e2ee18
Preston Pham <preston@goodybag.com>
2013-12-13 14:05:44 -0600

    add view updating

--- public/js/app/views/menu-order-params-view.js
+++ public/js/app/views/menu-order-params-view.js
@@ -1,3 +1,7 @@
+/**
+ * This view handles the order params
+ * displayed on the menu pages.
+ */
 define(function(require, exports, module) {
   var Backbone = require('backbone');
   var Handlebars = require('handlebars');
@@ -6,19 +10,45 @@ define(function(require, exports, module) {
 
   return module.exports = Backbone.View.extend({
     events: {
+      'click .btn-change-order-params': 'showOrderModal'
     }
 
   , template: template
 
   , initialize: function() {
-      this.model.on("all", function(e) {
-        console.log(e);
+      var this_ = this;
+
+      /**
+       * Event `change:orderparams` triggered by
+       * adding items or clicking change order 
+       * params.
+       */
+      this.model.on("change:orderparams", function(e) {
+        this_.render();
       });
     }
 
-  , render: function(){
+  , render: function() {
+      var $el = $( this.template({
+        order: this.model.toJSON()
+      }));
 
-      return this;
+      this.$el.html( $el.html() );
+    }
+
+  , showOrderModal: function(e) {
+      e.preventDefault();
+      var this_ = this;
+
+      this.options.orderModal.show({
+        success: function(model, response, options) {
+            model.trigger('change:orderparams');
+            this_.options.orderModal.hide();
+          }
+        , error: function(){
+            alert('sorry we were unable to change your order information, please refresh page and try again');
+          }
+      });
     }
   });
 });

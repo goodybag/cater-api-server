247a26cbbd86a49fa11c9ac8c65f9d49fbc72579
John Fawcett <jrf0110@gmail.com>
2013-12-18 16:17:12 -0600

    add between days from now helper

--- db/helpers.js
+++ db/helpers.js
@@ -19,3 +19,14 @@ mosql.registerConditionalHelper( '$overlap', {cascade: false}, function( column,
 mosql.registerConditionalHelper('$notExists', {cascade: false}, function( column, value, values, collection ){
   return 'not exists (' + mosql.sql( value, values ).toString() + ')';
 });
+
+mosql.registerConditionalHelper(
+  '$between_days_from_now'
+, { cascade: false }
+, function( column, value, values, table, query ){
+    return [
+      [ column, " >= date_trunc(\'day\', now() + interval '", value.from, " days')" ].join('')
+    , [ column, "  < date_trunc(\'day\', now() + interval '", value.to, " days')" ].join('')
+    ].join(' and ');
+  }
+);

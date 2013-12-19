f9b9058e2dcce9c2f4a020eec96f9f57fc3dd8ae
John Fawcett <jrf0110@gmail.com>
2013-12-19 15:35:34 -0600

    make reminder module only alert restaurants, we'll make a similar module that notifies users as well

---
+++ workers/reminder/reminders/restaurant-tomorrow-orders.js
@@ -0,0 +1,61 @@
+/**
+ * Restaurant Tomorrow Orders
+ *
+ * Description:
+ *   Send notifications to restaurants with orders tomorrow
+ */
+
+var Models  = require('../../../models');
+var utils   = require('../../../utils');
+
+var getTomorrowOrders = function( callback ){
+  var query = {
+    where: {
+      datetime: {
+        $between_days_from_now: { from: 1, to: 2 }
+      }
+    }
+  };
+
+  Models.Order.find( query, callback );
+};
+
+module.exports.name = 'Restaurant Tomorrow Orders';
+
+module.exports.schema = {
+  lastNotified: true
+};
+
+module.exports.check = function( storage, callback ){
+  getTomorrowOrders( function( error, results ){
+    if ( error ) return callback( error );
+
+    // Filter out restaurants that have already been notified
+    results = results.filter( function( result ){
+      if ( !storage.lastNotified[ result.attributes.id ] ) return true;
+
+      var notified = storage.lastNotified[ result.attributes.id ];
+      notified = new Date( notified );
+
+      var today = new Date();
+      today.setHours( 0, 0, 0, 0 );
+
+      return today > notified;
+    });
+
+    return callback( null, results.length > 0 );
+  });
+};
+
+module.exports.work = function( storage, callback ){
+  var stats = {
+    usersNotified:  { text: 'Users Notified', value: 0 }
+  };
+
+  getTomorrowOrders( function( error, orders ){
+    if ( error ) return callback( error );
+
+    console.log(orders);
+    return callback( null, stats );
+  });
+};

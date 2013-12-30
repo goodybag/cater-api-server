cdd35e0213d7f234040d9d3b312209063c00fbc3
John Fawcett <jrf0110@gmail.com>
2013-12-30 17:10:58 -0600

    mostly finish up reminder logic

--- workers/reminder/reminders/restaurant-tomorrow-orders.js
+++ workers/reminder/reminders/restaurant-tomorrow-orders.js
@@ -3,7 +3,8 @@
  *
  * Description:
  *   Send notifications to restaurants with accepted orders to
- *   fulfill tomorrow
+ *   fulfill tomorrow. This module ensures that it does not send
+ *   the same notification twice within the same window.
  */
 
 var Models  = require('../../../models');
@@ -17,24 +18,39 @@ module.exports.schema = {
   lastNotified: true
 };
 
-function notifyOrder( order ){
-  return utils.partial( utils.async.parallel, {
+// Return the function for carrying out all the notifications
+// for an order
+function notifyOrderFn( order ){
+  return utils.partial( utils.async.parallelNoBail, {
     email: function( done ){
-      utils.sendMail2({
-        to:       order.attributes.emails
-      , from:     config.emails.orders
+      views.render( 'order-email/order-reminder', {
+        layout: 'email-layout'
+      , config: config
+      , order:  order.toJSON()
+      }, function( error, html ){
+        if ( error ) return done( error );
 
-      , html:     views.order_reminder({
-                    config: config
-                  , order: order.toJSON()
-                  })
+        utils.sendMail2({
+          to:       order.attributes.emails
+        , from:     config.emails.orders
+        , html:     html
 
-      , subject:  [
-                    'Goodybag Reminder: Order #'
-                  , order.attributes.id
-                  , ' to be delivered tomorrow'
-                  ].join('')
-      }, done );
+        , subject:  [
+                      'Goodybag Reminder: Order #'
+                    , order.attributes.id
+                    , ' to be delivered tomorrow'
+                    ].join('')
+        }, function( error ){
+          // If successful, we want an easy way to know on the receiving end
+          // So just pass back the original order object as the results
+          done( error, error ? null : order );
+        });
+      })
+    }
+
+  , sms: function( done ){
+      // TODO:
+      done( null, order );
     }
   });
 };
@@ -69,8 +85,10 @@ module.exports.check = function( storage, callback ){
 module.exports.work = function( storage, callback ){
   var stats = {
     restaurantsNotified:  { text: 'Restaurants Notified', value: 0 }
-  , peopleNotified:       { text: 'People Notified', value: 0 }
+  , emailsSent:           { text: 'Emails Sent', value: 0 }
+  , smsSent:              { text: 'Text Messages Sent', value: 0 }
   , ordersHandled:        { text: 'Orders Handled', value: 0 }
+  , errors:               { text: 'Errors', value: 0, objects: [] }
   };
 
   var $query = {
@@ -95,13 +113,49 @@ module.exports.work = function( storage, callback ){
   Models.Order.findTomorrow( $query, function( error, orders ){
     if ( error ) return callback( error );
 
-    stats.ordersHandled.value = orders.length;
-
     utils.async.parallel(
-      orders.map( notifyOrder )
-    , utils.partial( callback, null, stats )
-    );
+      orders.map( function( o ){
+        return function( done ){ o.getOrderItems( done ); }
+      })
+    , function( error ){
+        if ( error ) return callback( error );
 
-    return callback( null, stats );
+        utils.async.parallelNoBail(
+          orders.map( notifyOrderFn )
+        , function( errors, results ){
+            if ( error ){
+              errors.forEach( function( e ){
+                Object.keys( e ).forEach( function( k ){
+                  stats.errors.value++;
+                  stats.errors.objects.push( e[ k ] );
+                });
+              });
+            }
+
+            // Anything that came back in results is considered a success
+            results.forEach( function( result, i ){
+              if ( !result || Object.keys( result ).length === 0 ) return;
+
+              stats.ordersHandled.value++;
+              storage.lastNotified[ orders[ i ].attributes.id ] = new Date().toString();
+
+              if ( result.email ){
+                stats.emailsSent.value += result.email.attributes.emails.length;
+              }
+
+              if ( result.sms ){
+                stats.smsSent.value += result.sms.attributes.sms_phones.length;
+              }
+            });
+
+            stats.restaurantsNotified.value = utils.unique( results, function( result ){
+              return result[ Object.keys( result )[0] ].attributes.restaurant_id;
+            }).length;
+
+            return callback( errors, stats );
+          }
+        );
+      }
+    );
   });
 };

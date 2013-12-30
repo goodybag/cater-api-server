674500410faee5e253f3bc4459596a1569b48d12
John Fawcett <jrf0110@gmail.com>
2013-12-30 17:03:58 -0600

    just pull in the express app for easier view rendering with layouts

--- workers/reminder/lib/views.js
+++ workers/reminder/lib/views.js
@@ -1,25 +1,41 @@
-var fs    = require('fs');
-var path  = require('path');
-var utils = require('../../../utils');
+// var fs    = require('fs');
+// var path  = require('path');
+// var hbs   = require('hbs');
+// var app   = require('express')();
 
-var initialDir = path.resolve( __dirname, '../../../views' );
-var dirs = [ initialDir ];
-var dir;
+// app.configure( function(){
+//   app.set( 'view engine', 'hbs' );
+//   app.set( 'views', path.resolve( __dirname, '../../../views' ) );
+//   app.set( 'config', require('../../../config') );
+// });
 
-while ( dir = dirs.pop() ){
-  fs.readdirSync( dir ).filter( function( file ){
-    if ( fs.statSync( path.join( dir, file ) ).isDirectory() ){
-      dirs.push( path.join( dir, file ) );
-      return false;
-    }
+// require('../../../helpers').register( hbs );
+// require('../../../lib/partials').register( hbs );
 
-    return file.slice(-4) === '.hbs' && fs.statSync( path.join( dir, file ) ).isFile();
-  }).map( function( file ){
-    return {
-      name:     file.replace( '.hbs', '' ).replace( /\-/g, '_' )
-    , contents: fs.readFileSync( path.join( dir, file ) ).toString()
-    }
-  }).forEach( function( file ){
-    module.exports[ file.name ] = utils.template( file.contents );
-  });
-}
+// var initialDir = path.resolve( __dirname, '../../../views' );
+// var dirs = [ initialDir ];
+// var dir;
+
+// while ( dir = dirs.pop() ){
+//   fs.readdirSync( dir ).filter( function( file ){
+//     if ( fs.statSync( path.join( dir, file ) ).isDirectory() ){
+//       dirs.push( path.join( dir, file ) );
+//       return false;
+//     }
+
+//     return file.slice(-4) === '.hbs' && fs.statSync( path.join( dir, file ) ).isFile();
+//   }).map( function( file ){
+//     return {
+//       name:     file.replace( '.hbs', '' ).replace( /\-/g, '_' )
+//     , contents: fs.readFileSync( path.join( dir, file ) ).toString()
+//     }
+//   }).forEach( function( file ){
+//     if ( file.name === 'order_reminder' ) console.log( file.contents );
+//     module.exports[ file.name ] = hbs.compile( file.contents );
+//   });
+// }
+
+/**
+ * Just proxy to app so we can access the `render` method
+ */
+module.exports = require('../../../app');

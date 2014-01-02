52f343178cf0ad8aa205aff807d6987d6079c269
John Fawcett <jrf0110@gmail.com>
2014-01-02 15:35:35 -0600

    add  function

--- db/sql/extensions.sql
+++ db/sql/extensions.sql
@@ -1,3 +1,14 @@
 
 -- enable uuid generation
 create extension "uuid-ossp";
+
+-- Add column to table
+create or replace function add_column( tbl_name text, col_name text, col_type text )
+returns void as $$
+begin
+  if not exists ( select 1 from information_schema.columns where table_name = tbl_name and column_name = col_name ) then
+    raise notice 'Adding column `%` to table `%`', col_name, tbl_name;
+    execute 'alter table "' || tbl_name || '" add column "' || col_name || '" ' || col_type;
+  end if;
+end;
+$$ language plpgsql;

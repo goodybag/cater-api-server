beb70b47991927cef8d87783f3d02acf41445bf2
Preston Pham <preston@goodybag.com>
2014-01-03 10:58:00 -0600

    add create restaurant event

--- controllers/restaurants/events.js
+++ controllers/restaurants/events.js
@@ -7,16 +7,31 @@ var models = require('../../models');
  * GET /restaurants/:rid/events?date=
  */
 module.exports.list = function(req, res, next) {
-  // TODO - implement
-  res.send(400);
+  var query = {
+    where: { 
+      restaurant_id: req.params.rid 
+    }
+  };
+
+  models.RestaurantEvent.find(query, function( error, results) {
+    if (error) return res.error(errors.internal.DB_FAILURE, error);
+    res.render('restaurant/availability', {events: results});
+  });
 }
 
 /**
  * POST /restaurants/:rid/events
  */
 module.exports.create = function(req, res, next) {
-  // TODO - implement
-  res.send(400);
+  var query = utils.extend({}, req.body, {restaurant_id: req.params.rid});
+  console.log(query);
+
+  var restaurantEvent = new models.RestaurantEvent(query);
+
+  restaurantEvent.save(function(error, restaurantEvent) {
+    if (error) return res.error(errors.internal.DB_FAILURE, error);
+    res.send(restaurantEvent);
+  });
 };
 
 /**

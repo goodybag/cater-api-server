73e8dd3c748421e96ce96552fbba0428fd676625
Preston Pham <preston@goodybag.com>
2014-01-03 12:33:15 -0600

    add listing events on calendar

--- public/js/app/views/restaurant/availability-view.js
+++ public/js/app/views/restaurant/availability-view.js
@@ -1,5 +1,8 @@
 /**
+ * Restaurant Availability View
+ *
  * Dependencies:
+ *   - collections.restaurant-events // todo: generalize restaurant events?
  */
 
 define(function(require, exports, module) {
@@ -12,9 +15,35 @@ define(function(require, exports, module) {
 
   return module.exports = Backbone.View.extend({
     initialize: function() {
-      this.$el.find('#calendar').fullCalendar({
 
+      // cache dom objects
+      this.$calendar = this.$el.find('#calendar');
+
+      // set up
+      this.convertEvents();
+      this.setupCalendar();
+    },
+
+    setupCalendar: function() {
+      this.$calendar.fullCalendar({
+        events: events
+      });
+    },
+
+    /**
+     * Convert RestaurantEvents into FullCalendar.Event format
+     */
+    convertEvents: function() {
+      events = events.map(function(event) {
+        var date_range = event.get('date_range');
+        date_range = date_range.replace( /[\[\]\(\)]/g,'').split(',');;
+        return {
+          title: event.get('name'),
+          start: date_range[0],
+          end: date_range[1]
+        };
       });
     }
+
   });
 });

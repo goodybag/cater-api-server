09ef84d7c6fc34acb49eae07ee209e588849acad
John Fawcett <jrf0110@gmail.com>
2014-01-06 10:22:45 -0600

    fix delta to actually work

--- db/deltas/1.2.0.sql
+++ db/deltas/1.2.0.sql
@@ -8,13 +8,6 @@ begin
   -- Update version
   execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
 
-  perform add_column( 'orders', 'pickup_person_name',   'text' );
-  perform add_column( 'orders', 'pickup_person_phone',  'character varying(10)' );
-  perform add_column( 'orders', 'pickup_location_id',   'id references locations("id")' );
-  perform add_column( 'orders', 'is_pickup',            'boolean not null default false' );
-
-  perform add_column( 'restaurants', 'allows_pickup',   'boolean not null default true' );
-
   create table if not exists "locations" (
     id              serial primary key
   , restaurant_id   int references restaurants ("id")
@@ -23,7 +16,7 @@ begin
   , lon             double precision default 0
   , street          text
   , street2         text
-  , city            text check ( similar to '[[:digit:]]{5}' )
+  , city            text check ( city similar to '[[:digit:]]{5}' )
   , state           varchar(2)
   , zip             varchar(5)
   , sms_phones      varchar(10)[]
@@ -41,4 +34,11 @@ begin
   , end_time        time without time zone not null
   , created_at      time with time zone not null default now()
   );
+
+  perform add_column( 'orders', 'pickup_person_name',   'text' );
+  perform add_column( 'orders', 'pickup_person_phone',  'character varying(10)' );
+  perform add_column( 'orders', 'pickup_location_id',   'int references locations("id")' );
+  perform add_column( 'orders', 'is_pickup',            'boolean not null default false' );
+
+  perform add_column( 'restaurants', 'allows_pickup',   'boolean not null default true' );
 end$$;

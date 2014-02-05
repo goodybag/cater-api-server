8bf50cf0dea42ce941854e6372d6954108676258
John Fawcett <jrf0110@gmail.com>
2014-02-05 15:20:25 -0600

    check dev, invert logic

--- bin/check-build.js
+++ bin/check-build.js
@@ -3,7 +3,7 @@
 var proc    = require('child_process');
 var config  = require('../config');
 
-if ( !config.isProduction ) return process.exit( 0 );
+if ( config.isDev ) return process.exit( 0 );
 
 proc.spawn(
   './node_modules/grunt-cli/bin/grunt', ['build']

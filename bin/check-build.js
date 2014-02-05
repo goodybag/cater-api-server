9a92a249b0ab3c6ce748e620ad97b631d543e26f
John Fawcett <jrf0110@gmail.com>
2014-02-05 10:04:03 -0600

    dat formatting

--- bin/check-build.js
+++ bin/check-build.js
@@ -5,4 +5,6 @@ var config  = require('../config');
 
 if ( !config.isProduction ) return process.exit( 0 );
 
-proc.spawn('./node_modules/grunt-cli/bin/grunt', ['build']).stdout.pipe( process.stdout );
+proc.spawn(
+  './node_modules/grunt-cli/bin/grunt', ['build']
+).stdout.pipe( process.stdout );

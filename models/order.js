8d235c757cf7a096e4d34ce2c7a4cb45e46a4a63
Lalit Kapoor <lalitkapoor@gmail.com>
2014-02-19 18:10:23 -0500

    add points to order object

--- models/order.js
+++ models/order.js
@@ -46,6 +46,7 @@ var modifyAttributes = function(callback, err, orders) {
         var rate = 1.0825; // default Austin, TX sales tax for now, in future store in and get from restaurant table
         var totalPreTip = (parseInt(order.attributes.sub_total) + parseInt(order.attributes.restaurant.delivery_fee)) * parseFloat(rate);
         order.attributes.total = Math.round(totalPreTip + order.attributes.tip); // in cents
+        order.attributes.points = Math.floor(order.attributes.total / 100);
       } else {
         order.attribtues.restaurant = null;
       }

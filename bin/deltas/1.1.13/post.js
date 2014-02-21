32af1cd1a5f3f29bff0be754f7572ce7a842e265
Preston Pham <preston@goodybag.com>
2014-02-21 13:30:41 -0600

    fix index not created error

--- bin/deltas/1.1.13/post.js
+++ bin/deltas/1.1.13/post.js
@@ -3,25 +3,33 @@ var models = require('../../../models');
 var config = require('../../../config');
 var elastic = require('../../../lib/elastic');
 
-models.Restaurant.find({}, function(error, restaurants) {
-  if (error) return console.error('error finding restaurants');
-
-  var tasks = [];
-  
-  utils.each(restaurants, function (restaurant) {
-    restaurant = restaurant.attributes;
-
-    tasks.push(function(callback) {
-      elastic.save('restaurant', utils.pick(restaurant, 'id', 'name', 'logo_url'), callback);
-    });
-  });
-
-  utils.async.parallel(tasks, function(err, results) {
-    if (err) 
-      console.log('Error:', err);
-    else
-      console.log(results.length + ' restaurants indexed');
-
+elastic.ensureIndex(function(error, result) {
+  if (error) {
+    console.log(error);
     process.exit();
+  }
+
+  models.Restaurant.find({}, function(error, restaurants) {
+    if (error) {
+      console.error('error finding restaurants');
+      process.exit();
+    }
+
+    var tasks = [];
+
+    utils.each(restaurants, function (restaurant) {
+      restaurant = restaurant.attributes;
+      tasks.push(function(callback) {
+        elastic.save('restaurant', utils.pick(restaurant, 'id', 'name', 'logo_url'), callback);
+      });
+    });
+
+    utils.async.parallelLimit(tasks, 2, function(err, results) {
+      if (err) 
+        console.log('Error:', err);
+      else
+        console.log(results.length + ' restaurants indexed');
+      process.exit();
+    });
   });
 });

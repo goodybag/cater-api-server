17a6746381cd613948a0815212d1f26047fd531e
Preston Pham <prestonp08@gmail.com>
2014-02-28 17:01:07 -0600

    yelp

--- public/js/app/views/restaurant/edit-basic-info-view.js
+++ public/js/app/views/restaurant/edit-basic-info-view.js
@@ -10,10 +10,11 @@ define(function(require, exports, module) {
     },
 
     fieldMap: {
-      name:           '#input-name'
-    , description:    '#input-description'
-    , websites:       '#input-websites'
-    , is_hidden:      '#input-hidden'
+      name:                   '#input-name'
+    , description:            '#input-description'
+    , websites:               '#input-websites'
+    , yelp_business_id:       '#input-yelp'
+    , is_hidden:              '#input-hidden'
     },
 
     fieldGetters: {
@@ -23,6 +24,15 @@ define(function(require, exports, module) {
 
       is_hidden: function() {
         return this.$el.find(this.fieldMap.is_hidden).is(':checked');
+      },
+
+      yelp_business_id: function() {
+        var url = this.$el.find(this.fieldMap.yelp_business_id).val();
+        if (!url) return null;
+        if (url.indexOf('/') === -1) return null;
+
+        url = url.split('/').pop();
+        return url.split('#')[0];
       }
     },
 

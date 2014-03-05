cf389998945c71d8aaa12f0b883480656e4021d1
Preston Pham <prestonp08@gmail.com>
2014-03-05 11:19:56 -0600

    highlight link

--- public/js/app/views/menu-share-link-view.js
+++ public/js/app/views/menu-share-link-view.js
@@ -4,10 +4,21 @@
 define(function(require, exports, module) {
   var Backbone = require('backbone');
   var Handlebars = require('handlebars');
+  var utils = require('utils');
 
   var template = Handlebars.partials.menu_order_params;
 
   return module.exports = Backbone.View.extend({
-    events: { }
+    events: {
+      'click .highlight-link': 'highlightLink'
+    },
+
+    highlightLink: function(e) {
+      var 
+        x = window.scrollX
+      , y = window.scrollY;
+      this.$el.find('.share-link').focus();
+      utils.defer(window.scrollTo.bind(window, x, y));
+    }
   });
 });

35ebe79ab33a7281dc523cbf61099c5782233cfd
John Fawcett <jrf0110@gmail.com>
2014-03-11 14:59:19 -0500

    points and pending points integrated with DB

--- controllers/users/rewards.js
+++ controllers/users/rewards.js
@@ -7,26 +7,27 @@ var utils   = require('../../utils');
 
 module.exports.list = function( req, res, next ){
   var tasks = {
-    pendingPoints:  Models.User.getPendingPoints.bind( null, req.param('uid') )
-  , pendingOrders:  Models.User.getOrdersWithPendingPoints.bind( null, req.param('uid'), {
-                      order: { datetime: 'desc' }
-                    })
+    'pendingPoints':  Models.User.getPendingPoints.bind( Models.User, req.param('uid') )
 
-  , orders:         Models.Order.find.bind( null, {
-                      where: {
-                        status: { $or: ['submitted', 'accepted', 'delivered'] }
-                      , points_awarded: true
-                      , user_id: req.param('uid')
-                      }
-                    })
+  , 'pendingOrders':  Models.User.getOrdersWithPendingPoints.bind( Models.User, req.param('uid'), {
+                        order: { datetime: 'desc' }
+                      })
+
+  , 'orders':         Models.Order.find.bind( Models.Order, {
+                        where: {
+                          status: { $or: ['submitted', 'accepted', 'delivered'] }
+                        , points_awarded: true
+                        , user_id: req.param('uid')
+                        }
+                      })
   };
 
   utils.async.parallel( tasks, function( error, results ){
     if ( error ) return console.log( error ), res.error( error );
-console.log(results);
+
     req.session.user.pendingPoints  = results.pendingPoints;
-    res.locals.pendingOrders        = utils.invoke( 'toJSON', results.pendingOrders );
-    res.locals.orders               = utils.invoke( 'toJSON', results.orders );
+    res.locals.pendingOrders        = utils.invoke( results.pendingOrders, 'toJSON' );
+    res.locals.orders               = utils.invoke( results.orders, 'toJSON' );
 
     res.render('my-rewards');
   });

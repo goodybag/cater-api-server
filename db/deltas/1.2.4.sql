4ca44f21059c478ff87e00b3dfc8bfe8f5bb8d74
John Fawcett <jrf0110@gmail.com>
2014-03-26 15:21:46 -0500

    add some nice functions and fee to delivery zips

---
+++ db/deltas/1.2.4.sql
@@ -0,0 +1,72 @@
+-- #769 Tiered Delivery Zips
+
+-- Check for table existence
+create or replace function table_exists( tbl_name text )
+returns boolean as $$
+begin
+  return exists (
+    select 1 from pg_catalog.pg_tables where tablename = tbl_name
+  );
+end;
+$$ language plpgsql;
+
+-- Check for column existence
+create or replace function column_exists( tbl_name text, col_name text )
+returns boolean as $$
+begin
+  return exists (
+    select column_name from information_schema.columns
+    where table_name = tbl_name and column_name = col_name
+  );
+end;
+$$ language plpgsql;
+
+-- Check constraint existence
+create or replace function constraint_exists( c_type text, tbl_name text, col_name text )
+returns boolean as $$
+begin
+  return exists (
+    select 1 from
+      information_schema.constraint_column_usage usage
+    left join information_schema.table_constraints constraints
+      on constraints.constraint_name = usage.constraint_name
+    where constraints.constraint_type = c_type
+      and usage.table_name = tbl_name
+      and usage.column_name = col_name
+  );
+end;
+$$ language plpgsql;
+
+-- Add column to table
+create or replace function add_column( tbl_name text, col_name text, col_type text )
+returns void as $$
+begin
+  if exists ( select 1 from column_exists( tbl_name, col_name ) where column_exists = false ) then
+    raise notice 'Adding column `%` to table `%`', col_name, tbl_name;
+    execute 'alter table "' || tbl_name || '" add column "' || col_name || '" ' || col_type;
+  end if;
+end;
+$$ language plpgsql;
+
+-- Add type
+create or replace function add_type( type_name text, type_def text )
+returns void as $$
+begin
+  if not exists ( select 1 from pg_type where typname = type_name ) then
+    raise notice 'Adding Type `%`', type_name;
+    execute 'create type ' || type_name || ' as ' || type_def;
+  end if;
+end;
+$$ language plpgsql;
+
+DO $$
+  declare version       text := '1.2.4';
+
+begin
+  raise notice '## Running Delta v% ##', version;
+
+  -- Update version
+  execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
+
+  perform add_column( 'restaurant_delivery_zips', 'fee', 'int' );
+end$$;

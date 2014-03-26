338e92fa7d27d109ac7bf47f2d520ce0d36d211c
John Fawcett <jrf0110@gmail.com>
2014-03-26 15:06:54 -0500

    remove some old chit

--- public/js/app/views/item-view.js
+++ public/js/app/views/item-view.js
@@ -1,16 +1,14 @@
 define(function(require, exports, module) {
   var Backbone = require('backbone');
-console.log('wuuuuuuuuuuuuuuut');
+
   return module.exports = Backbone.View.extend({
     events: {
       'click': 'showModal'
     },
 
     showModal: function(e) {
-      console.log('showModal');
       e.preventDefault();
       this.options.itemModalView.provideModel(this.model).show();
-      this.$el.addClass('in');
     }
   });
 });

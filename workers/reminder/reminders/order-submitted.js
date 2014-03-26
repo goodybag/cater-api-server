f7d34e74651adf111f85a3383f57244f594a12d7
Preston Pham <prestonp08@gmail.com>
2014-03-26 11:23:29 -0500

    wip work fn

--- workers/reminder/reminders/order-submitted.js
+++ workers/reminder/reminders/order-submitted.js
@@ -6,7 +6,11 @@
  *   at appropriate times (not in the middle of the night)
  */
 
-var config = require('../../config');
+var config = require('../../../config');
+var Models  = require('../../../models');
+var views   = require('../lib/views');
+
+// The date we switch implementation from event triggered to reminder
 var pivot = config.submittedNotificationStartDate;
 
 module.exports.name = 'Order Submitted Notifcations';
@@ -50,12 +54,48 @@ module.exports.work = function( storage, callback ){
   , errors:              { text: 'Errors', value: 0, objects: [] }
   };
 
-  Models.Order.find( getQuery( storage ), function( error, orders ) {
-    if ( error ) return callback( error );
+  /*
+   * 1 Find submitted orders not notified
+   * 2 Render email
+   * 3 Send mail
+   */
 
-    
+  utils.auto({
+    findSubmittedOrders: function(callback) {
+      Models.Order.find(getQuery(storage), function(error, orders) {
+        callback(error, orders);
+      });
+    },
+
+    renderEmails: ['findSubmittedOrders', function(callback, results) {
+
+    }],
+
+    sendEmails: ['renderEmails', function(callback, orders) {
+
+    }]
   });
-  stats.myStat.value++;
+  //
+  // Models.Order.find( getQuery( storage ), function( error, orders ) {
+  //   if ( error ) return callback( error );
+  //   orders.forEach(function(order) {
+  //     var view = {
+  //       order:    order.toJSON({ review: true })
+  //     , config:   config
+  //     , layout:   'email-layout'
+  //     };
+  //     views.render('order-email/order-submitted', view, function(error, html) {
+  //       utils.sendMail2({
+  //         to:   order.attributes.restaurant.emails
+  //       , from: config.emails.orders
+  //       , html: html
+  //       , subject: subject(order)
+  //       }, function(error) {
+  //
+  //       })
+  //     });
+  //   })
+  // });
 
-  callback( null, stats );
+  // callback( null, stats );
 };

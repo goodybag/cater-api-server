0a077d6aedaba786f413735828a37e286871007d
John Fawcett <jrf0110@gmail.com>
2014-04-01 16:00:17 -0500

    fix post script to NOT welcome all of our existing users

--- bin/deltas/1.2.5/post.js
+++ bin/deltas/1.2.5/post.js
@@ -15,14 +15,16 @@ utils.async.parallel({
     limit: 'all'
   , where: {
       'id::text': {
-        type: 'select'
-      , table: 'reminders'
-      , columns: [{
-          type: 'json_object_keys'
-        , expression: 'data->\'users\''
-        }]
-      , where: {
-          name: 'Welcome Email'
+        $nin: {
+          type: 'select'
+        , table: 'reminders'
+        , columns: [{
+            type: 'json_object_keys'
+          , expression: 'data->\'users\''
+          }]
+        , where: {
+            name: 'Welcome Email'
+          }
         }
       }
     }
@@ -31,30 +33,15 @@ utils.async.parallel({
   if ( error ) throw error;
 
   results.reminder = results.reminder ? ( results.reminder.attributes || {} ) : {};
-
   reminder.ensureSchema( schema, results.reminder );
 
   results.users.forEach( function( user ){
     results.reminder.users[ user.attributes.id ] = true;
   });
 
-  reminder.saveStorage( reminderName, result, callback );
+  reminder.saveStorage( 'Welcome Email', results.reminder, function( error ){
+    if ( error ) throw error;
+
+    process.exit(0);
+  });
 });
-
-// Ensure the schema exists for reminder
-Models.Reminder.findOne({
-  where: { name: reminderName }
-}, function( error, result ){
-  if ( error ){
-    logger.error( 'Error fetching reminders in `markUserAsWelcomed` for userId: ' + userId, error );
-    return callback ? callback( error ) : null;
-  }
-
-  result = result ? ( result.attributes || {} ) : {};
-
-  reminder.ensureSchema( schema, result );
-
-  reminder.saveStorage( reminderName, result, callback );
-});
-
-Models.User.find( usersQuery )

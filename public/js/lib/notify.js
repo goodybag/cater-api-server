c27471ea4dd0e8120dfcc279240c3adf5c7f9abf
Preston Pham <prestonp08@gmail.com>
2014-04-03 15:11:18 -0500

    wrap msg

--- public/js/lib/notify.js
+++ public/js/lib/notify.js
@@ -2,7 +2,8 @@ define(function(){
   var notify = {};
 
   notify.error = function( msg ){
-    if ( typeof msg === 'object' ) msg = JSON.stringify( msg );
+    // segment.io requires properties to be an object
+    if ( typeof msg !== 'object' ) msg = { error: msg };
     analytics.track('Error', msg);
     console.error( msg );
   };

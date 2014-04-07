a1e4309d16d81d12a3f74450a5ec1c9e225f4c62
John Fawcett <jrf0110@gmail.com>
2014-04-07 15:20:02 -0500

    add record not found error

--- errors.js
+++ errors.js
@@ -41,6 +41,15 @@ errors.internal.UNKNOWN = {
 };
 errors[errors.internal.UNKNOWN.code] = errors.internal.UNKNOWN;
 
+errors.internal.NOT_FOUND = {
+  type: "internal"
+, code: "0004"
+, httpCode: "404"
+, name: "NOT_FOUND"
+, message: "Could not find that record"
+};
+errors[errors.internal.NOT_FOUND.code] = errors.internal.NOT_FOUND;
+
 /**
  * Authentication Errors
  */

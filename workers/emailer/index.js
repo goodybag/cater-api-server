4b620d3912eef423c49a030814314666672a49ca
John Fawcett <jrf0110@gmail.com>
2014-04-07 16:53:15 -0500

    fix placement of cron declaration, logging, finish up loop

--- workers/emailer/index.js
+++ workers/emailer/index.js
@@ -2,16 +2,11 @@
  * Emailer worker
  */
 
+var config  = require('../../config');
 var emailer = require('../../lib/emailer');
 var db      = require('../../db');
 var utils   = require('../../utils');
 var logger  = require('../../logger').emailerWorker;
-
-var job = new utils.CronJob({
-  cronTime: '00 * * * * *'
-, start:    true
-, onTick:   onTick
-});
 
 var loggerTags = ['emailer-worker'];
 
@@ -19,27 +14,39 @@ var loggerTags = ['emailer-worker'];
   logger[ level ] = logger[ level ].bind( logger, loggerTags );
 });
 
-module.exports.onTick = function onTick(){
+logger.info('Starting emailer worker');
+
+module.exports.onTick = function (){
   logger.info('onTick');
 
   utils.async.waterfall([
     emailer.getReadyToSend.bind( emailer )
 
   , function( emails, next ){
-      logger.info( 'Preparing to send emails', { emails: emails } );
-
       var onEmail = function( email, done ){
+        logger.info( 'Preparing to email', { email: email } );
+
         emailer.send( email, function( error ){
           if ( error ){
             logger.warn( 'Error sending email', email );
-
           }
+
+          next();
         });
       };
 
       utils.async.each( emails, onEmail, next );
     }
   ], function( error ){
-
+    // We don't send error in loop, so this realllly shouldn't happen, but log it
+    if ( error ){
+      logger.error( 'Error sending emails', error );
+    }
   });
 };
+
+var job = new utils.CronJob({
+  cronTime: config.emailer.cronTime
+, start:    true
+, onTick:   module.exports.onTick
+});

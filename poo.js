ce439ae0aa23d2a1833a1050b63c7abd2d0ac0bb
Preston Pham <prestonp08@gmail.com>
2014-04-08 13:46:40 -0500

    remove

--- poo.js
+++
@@ -1,8 +0,0 @@
-var utils = require('./utils');
-var moment = require('moment');
-
-for (var i =0; i < 14; i++) {
-  var now = moment(new Date()).hour(i);
-  console.log('now = ' + now.toString());
-  console.log('converted = ' + utils.saneDatetime(now));
-}

43ecc9cf02692732b89dc6f886c6add31ff7966a
Preston Pham <prestonp08@gmail.com>
2014-04-10 15:55:10 -0500

    using event emitter

--- poo.js
+++ poo.js
@@ -1,14 +1,27 @@
-var scheduler = require('./scheduler');
+// var scheduler = require('./scheduler');
+//
+//
+// var date = new Date();
+// date.setMinutes(date.getMinutes() + 2);
+// scheduler.queue('make-call', date, {
+//   to: 'aa7135178077',
+//   from: 'a5122706555',
+//   url: 'https://www.goodybag.com/orders/1066/voice',
+//   ifMachine: 'Continue',
+//   method: 'GET'
+// }, function() {
+//   console.log(arguments);
+// });
 
 
-var date = new Date();
-date.setMinutes(date.getMinutes() + 2);
-scheduler.queue('make-call', date, {
-  to: 'aa7135178077',
-  from: 'a5122706555',
-  url: 'https://www.goodybag.com/orders/1066/voice',
-  ifMachine: 'Continue',
-  method: 'GET'
-}, function() {
+var Scheduler = require('./lib/scheduler');
+var scheduler = new Scheduler();
+
+scheduler.on('email', function(job) {
+  console.log(job.data);
+});
+scheduler.emit('email', {data: 'poop'});
+
+scheduler.queue('email', new Date(), { to: 'hop', from: 'doddy'}, function(err, result) {
   console.log(arguments);
 });

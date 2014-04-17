ab850b29ee7fedc285fb6e62c9c5fad6b1cacc7b
John Fawcett <jrf0110@gmail.com>
2014-04-17 13:41:46 -0500

    test by zip filter complete

--- test/functional/restaurant-search.js
+++ test/functional/restaurant-search.js
@@ -17,7 +17,7 @@ var tutils    = require('../../lib/test-utils');
 require('../../lib/selenium-utils');
 
 test.describe( 'Restaurant Search', function(){
-  it( 'Search by zip', function( done ){
+  it( 'Filter by zip', function( done ){
     var options = {
       timeout:      100000
     , email:        config.testEmail
@@ -44,9 +44,14 @@ test.describe( 'Restaurant Search', function(){
     driver.get( config.baseUrl );
 
     utils.async.waterfall([
-      tutils.login.bind( null, driver, options )
+      function( next ){ console.log(''); next(); }
 
-    , function( next ){ console.log(''); next(); }
+    , function( next ){
+        test.log('Login')
+        next();
+      }
+
+    , tutils.login.bind( null, driver, options )
 
     , function( next ){
         test.log('Get restaurant_delivery_zips')
@@ -77,12 +82,42 @@ test.describe( 'Restaurant Search', function(){
 
       // View restaurants
     , function( zips, next ){
-        test.log('Ensuring we\'re on the restaurants listing');
+        test.log('Ensure we\'re on the restaurants listing');
 
         driver.ensureSelector( '.page-restaurants', function( error, result ){
           assert.equal( !!error, false, 'Not on Restaurants page' );
           assert.equal( !!result, true, 'Not on Restaurants page' );
           next( null, zips );
+        });
+      }
+
+      // Search for zip
+    , function( zips, next ){
+        test.log( 'Sending: ' + zips[0].zip );
+
+        driver.ensureSelector( '.order-params-bar [name="zip"]', function( error, el ){
+          assert.equal( !!error, false, 'Could not find zip field' );
+          assert.equal( !!el, true, 'Could not find zip field' );
+
+          el.sendKeys( zips[0].zip + '\n' );
+
+          next( null, zips );
+        });
+      }
+
+      // Check number of listings
+    , function( zips, next ){
+        test.log('Ensure number of restaurants');
+
+        driver.waitUntilSelector( '.search-info', function( error, el ){
+          assert.equal( !!error, false, 'Page did not reload' );
+
+          driver.find( '.list-group-restaurants > a.list-group-item', function( error, els ){
+            assert.equal( !!error, false, 'Did not find any restaurant elements' );
+            assert.equal( els.length, zips.length, 'Number of restaurants did not match zips' );
+
+            next();
+          });
         });
       }
     ], done );

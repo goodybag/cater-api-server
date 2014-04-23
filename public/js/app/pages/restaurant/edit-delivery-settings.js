cac82a2f2af29592fc6d831520988dd993ab1d12
Preston Pham <prestonp08@gmail.com>
2014-04-23 16:43:42 -0500

    lead times front end scripting

--- public/js/app/pages/restaurant/edit-delivery-settings.js
+++ public/js/app/pages/restaurant/edit-delivery-settings.js
@@ -1,13 +1,19 @@
 define(function(require){
   var $ = require('jquery');
   require('jquery-loaded');
+  var utils = require('utils');
 
   var Views = {
     EditDeliverySettingsView: require('app/views/restaurant/edit-delivery-settings-view')
+  , EditHoursView: require('app/views/edit-hours-view')
   , AlertView: require('app/views/alert-view')
   };
 
-  var data = require('data')
+  var Models = {
+    Hours: require('app/models/hours')
+  };
+
+  var data = require('data');
   var page = {
     init: function(){
       var alertView = new Views.AlertView({
@@ -18,6 +24,13 @@ define(function(require){
         el : '.restaurant-edit'
       , model: data.models.restaurant
       , alertView: alertView
+      });
+
+      var hours = utils.map(data.models.restaurant.attributes.delivery_times, function(times, idx) {
+        return new Views.EditHoursView({
+          model: new Models.Hours({ day: idx, times: times })
+        , el: '#hours-' + idx
+        });
       });
     },
 

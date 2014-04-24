aa28d2283e8b32cec7ec636d37860999c3d34c5d
Preston Pham <prestonp08@gmail.com>
2014-04-24 10:08:38 -0500

    finished lead times

--- public/js/app/views/restaurant/edit-lead-times-view.js
+++ public/js/app/views/restaurant/edit-lead-times-view.js
@@ -7,17 +7,43 @@ define(function(require, exports, module) {
 
     events: {
       'submit .form-delivery-settings': 'save'
+    , 'click .add-lead-time': 'addLeadTime'
+    , 'click .remove-lead-time': 'removeLeadTime'
     },
 
     fieldMap: {
       delivery_times: '.restaurant-form .time'
+    , lead_times: '.restaurant-form .lead-times'
     },
 
     fieldGetters: {
       delivery_times: function() {
         var models = _.pluck(this.options.hours, 'model')
         return _.object(_.invoke(models, 'get', 'day'), _.invoke(models, 'toJSON'));
+      },
+
+      lead_times: function() {
+        return _.compact(_.map(this.$el.find('.lead-time'), function(el) {
+          var guests = parseInt($(el).find('.lead-max-guests').val());
+          var hours = parseInt($(el).find('.lead-hours').val());
+          var cancel = parseInt($(el).find('.lead-cancel-time').val());
+          return !_.isNaN(guests) && !_.isNaN(hours) ? {
+            max_guests: !_.isNaN(guests) ? guests : null,
+            lead_time:!_.isNaN(hours) ? hours : null,
+            cancel_time: !_.isNaN(cancel) ? cancel : null
+          } : null;
+        }));
       }
+    },
+
+    addLeadTime: function(e) {
+      this.$el.find('.lead-times-list').append(Handlebars.partials.lead_time({}));
+    },
+
+    removeLeadTime: function(e) {
+      e.preventDefault();
+      $(e.target).closest('.lead-time').remove();
+      this.onChange(e);
     },
 
     initialize: function() {

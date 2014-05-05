fdf70c4786c39aa0e36e6059ed2b57902538f566
John Fawcett <jrf0110@gmail.com>
2014-05-05 11:42:16 -0500

    add shortcut module for registering all order notifications

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-var fs      = require('fs');
-var path    = require('path');
 var forky   = require('forky');
 var config  = require('./config');
 var rollbar = require('rollbar');
@@ -19,16 +17,7 @@ var app = require('./app')
   , http = require('http');
 
 require('./lib/events');
-
-// Require all order notification definitions
-var dir = path.resolve( __dirname, './lib/order-notifications' );
-fs.readdirSync( dir ).filter( function( f ){
-  return fs.statSync( path.join( dir, f ) ).isFile();
-}).filter( function( f ){
-  return f.slice(-3) === '.js';
-}).forEach( function( f ){
-  require( path.join( dir, f ) );
-});
+require('./lib/order-notifications');
 
 var server = http.createServer(app);
 

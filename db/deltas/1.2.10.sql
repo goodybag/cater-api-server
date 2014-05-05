8c058d90e35039c2246aca3999306a3f6e088ef4
John Fawcett <jrf0110@gmail.com>
2014-05-05 17:31:55 -0500

    default status and datetime for easier insertion

--- db/deltas/1.2.10.sql
+++ db/deltas/1.2.10.sql
@@ -9,4 +9,7 @@ begin
   execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
 
   perform add_column( 'scheduled_jobs', 'predicate_id', 'integer references scheduled_jobs(id)' );
+
+  alter table scheduled_jobs alter column status set default 'pending';
+  alter table scheduled_jobs alter column datetime set default now();
 end$$;

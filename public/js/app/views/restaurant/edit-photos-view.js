02e99c2379d5e7cdbd3818d64132328af15c66de
Preston Pham <prestonp08@gmail.com>
2014-05-08 18:12:08 -0500

    update and remove working

--- public/js/app/views/restaurant/edit-photos-view.js
+++ public/js/app/views/restaurant/edit-photos-view.js
@@ -8,6 +8,8 @@ define(function(require, exports, module) {
     events: {
       'change input[type="filepicker"]': 'onFilePickerChange'
     , 'submit .form-add-photo': 'addPhoto'
+    , 'submit .form-update-photo': 'updatePhoto'
+    , 'click .btn-remove-photo': 'removePhoto'
     },
 
     initialize: function() {
@@ -24,13 +26,40 @@ define(function(require, exports, module) {
       this.render();
     },
 
+    updatePhoto: function(e) {
+      e.preventDefault();
+      var $photo = $(e.target).closest('li');
+      var id = $photo.data('id');
+      this.collection.get(id).save({
+        url:          $photo.find('input[name="url"]').val()
+      , name:         $photo.find('input[name="name"]').val()
+      , description:  $photo.find('textarea[name="description"]').val()
+      });
+
+      this.render();
+      // todo hook into success/fail callbacks
+      // to show respective alert view
+    },
+
+    removePhoto: function(e) {
+      var $photo = $(e.target).closest('li');
+      var id = $photo.data('id');
+      this.collection.get(id).destroy();
+
+      // todo hook into success/fail callbacks
+      // to show respective alert view
+      this.render();
+    },
+
     render: function() {
       var html = this.template({
         restaurant_photos: this.collection.toJSON()
       , config: { defaultLogo: this.options.defaultLogo }
       });
       this.$el.html(html);
-      filepicker.constructWidget(this.$el.find('input[type="filepicker"]'));
+      this.$el.find('input[type="filepicker"]').each(function() {
+        filepicker.constructWidget( this );
+      });
     },
 
     onFilePickerChange: function(e){

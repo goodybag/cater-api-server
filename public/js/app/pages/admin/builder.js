641f86da9683d1a65f89d3f0ef85f43d1285e4d8
John Fawcett <jrf0110@gmail.com>
2014-05-08 12:37:45 -0500

    allow admin javascript builds

---
+++ public/js/app/pages/admin/builder.js
@@ -0,0 +1,18 @@
+define([
+  'app/pages/admin/order'
+, 'app/pages/admin/restaurant/edit-address'
+, 'app/pages/admin/restaurant/edit-basic-info'
+, 'app/pages/admin/restaurant/edit-delivery-settings'
+, 'app/pages/admin/restaurant/edit-tags'
+, 'app/pages/admin/restaurant/edit-lead-times'
+
+, 'app/pages/contacts'
+
+  // Also include stuff missed from edit-menu.hbs
+, 'app/views/edit-restaurant-view'
+, 'app/views/edit-category-view'
+, 'app/views/edit-item-view'
+, 'app/models/hours'
+, 'app/views/edit-hours-view'
+, 'jquery-ui'
+], function(){});
--- public/js/app/pages/admin/restaurant/edit-tags.js
+++ public/js/app/pages/admin/restaurant/edit-tags.js
@@ -1,27 +1,25 @@
 define(function(require){
   var $ = require('jquery');
   require('jquery-loaded');
-  var utils = require('utils');
 
   var Views = {
-    EditDeliverySettingsView: require('app/views/restaurant/edit-delivery-settings-view')
-  , EditHoursView: require('app/views/edit-hours-view')
+    EditTagsView: require('app/views/restaurant/edit-tags-view')
   , AlertView: require('app/views/alert-view')
   };
 
-  var data = require('data');
   var page = {
-    init: function(){
+    init: function( options ){
       var alertView = new Views.AlertView({
         el: '.alert-container'
       });
 
-      var restaurantEditView = new Views.EditDeliverySettingsView({
+      var restaurantEditView = new Views.EditTagsView({
         el : '.restaurant-edit'
-      , model: data.models.restaurant
+      , model: options.models.restaurant
       , alertView: alertView
       });
     },
+
   };
 
   return page;

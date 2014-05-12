346fd031acdb0ff2cc824512413166dbe387355b
Preston Pham <prestonp08@gmail.com>
2014-05-12 16:35:43 -0500

    fix create/update

--- public/js/app/views/restaurant/edit-photos-view.js
+++ public/js/app/views/restaurant/edit-photos-view.js
@@ -13,18 +13,22 @@ define(function(require, exports, module) {
     },
 
     initialize: function() {
-      render();
+      this.sortable();
     },
 
     addPhoto: function(e) {
       // todo validate url / show error alert
+      var this_ = this;
       e.preventDefault();
+      var $form = this.$el.find('.form-add-photo');
       this.collection.create({
-        url:          this.$el.find('input[name="url"]').val()
-      , name:         this.$el.find('input[name="name"]').val()
-      , description:  this.$el.find('textarea[name="description"]').val()
+        url:          $form.find('input[name="url"]').val()
+      , name:         $form.find('input[name="name"]').val()
+      , description:  $form.find('textarea[name="description"]').val()
+      }, { 
+        wait: true 
+      , success: this.render.bind(this)
       });
-      this.render();
     },
 
     updatePhoto: function(e) {
@@ -32,7 +36,7 @@ define(function(require, exports, module) {
       var $photo = $(e.target).closest('li');
       var id = $photo.data('id');
       this.collection.get(id).save({
-        url:          $photo.find('input[name="url"]').val()
+        url:          $photo.find('img').attr('src')
       , name:         $photo.find('input[name="name"]').val()
       , description:  $photo.find('textarea[name="description"]').val()
       });
@@ -64,8 +68,12 @@ define(function(require, exports, module) {
 
       // pass in `stop` function for updating the collection
       // convert all forms to just a global batch update
-      this.$el.find('.photo-list').sortable();
+      this.sortable();
     },
+
+    sortable: function() {
+      this.$el.find('.photo-list').sortable();
+    }
 
     onFilePickerChange: function(e){
       var $input = $(e.originalEvent.target);

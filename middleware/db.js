602a6ca027708b1f4a61fc5b0e0bd171f200f322
John Fawcett <jrf0110@gmail.com>
2014-05-12 13:33:39 -0500

    little bit of docs

--- middleware/db.js
+++ middleware/db.js
@@ -1,3 +1,27 @@
+/**
+ * Attach db result object/objects to res.locals
+ * Pretty simple right now. The queries are static
+ * ( as in you can really respond to url parameters )
+ * So, whatever data you're putting on there is purely
+ * auxiliary and doesn't really rely on external state
+ *
+ * Usage:
+ *
+ *   // res.locals.orders = [...];
+ *   app.get('/users'
+ *     m.db.orders.find( {}, { limit: 'all' } )
+ *   , m.view('users', db.users, { operation: 'find' } )
+ *   );
+ *
+ * If you're doing a findOne/insert, is uses the singular form:
+ *
+ *   // res.locals.someThing = { ... }
+ *   app.get('/users'
+ *     m.db.someThings.findOne( 1 )
+ *   , m.view('users', db.users, { operation: 'find' } )
+ *   );
+ */
+
 var dirac = require('dirac');
 var db    = require('../db');
 var utils = require('../utils');

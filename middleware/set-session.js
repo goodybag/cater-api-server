16cc89524326ccdb580a843abb3a36babaed87ce
Preston Pham <prestonp08@gmail.com>
2014-05-13 17:52:44 -0500

    remember me fixed

--- middleware/set-session.js
+++ middleware/set-session.js
@@ -6,21 +6,14 @@ var utils = require('../utils');
 
 module.exports = function( options ){
   return function( req, res, next ){
-
-    // Expire in two weeks of inactivity
-    if ( req.session.remember )
-      req.session.cookie.maxAge = 14 * 24 * 60 * 60 * 1000;
-
     req.setSession = function( user, remember ){
       req.session.user = user;
 
-      // Remember me checked, triggered by /login
-      if ( remember )
-        req.session.remember = remember;
-
-      // req.session.save(function(err) {
-      //   console.log(err);
-      // });
+      // remember me triggered by /login
+      // expire in two weeks of inactivity
+      if ( remember ) {
+        req.session.cookie.maxAge = 14 * 24 * 60 * 60 * 1000;
+      }
     };
 
     next();

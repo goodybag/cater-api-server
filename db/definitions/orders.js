e092ff69eac55ca2981c60c8b0283f7bc155e0bf
Preston Pham <prestonp08@gmail.com>
2014-06-13 11:38:28 -0500

    Use dirac dals

--- db/definitions/orders.js
+++ db/definitions/orders.js
@@ -157,6 +157,16 @@ define(function(require) {
       type: types.boolean
     , default: false
     }
+  , sales_tax: {
+      type: types.int
+    , nullable: false
+    , default: 0
+    }
+  , delivery_fee: {
+      type: types.int
+    , nullable: false
+    , default: 0
+    }
   };
 
   definition.indices = {};

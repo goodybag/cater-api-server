dbcd11b07d2798b505333e0eb968d4db6c0acdf2
John Fawcett <jrf0110@gmail.com>
2014-06-19 14:53:25 -0500

    update order totals also update delivery fee. Do not trigger events for delivery_fee update

---
+++ db/deltas/1.2.20.sql
@@ -0,0 +1,30 @@
+-- #957
+
+DO $$
+  declare version       text := '1.2.20';
+  declare r orders;
+begin
+  raise notice '## Running Delta v% ##', version;
+
+  -- Update version
+  execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
+
+  drop trigger if exists order_total_change on orders;
+
+  for r in (
+    select * from orders where restaurant_id in (
+      select id from restaurants
+    )
+    order by id asc
+  )
+  loop
+    raise notice 'Updating Order #%', r.id;
+    perform update_order_totals( r );
+  end loop;
+
+  create trigger order_total_change
+    after insert or update of tip, adjustment_amount
+    on orders
+    for each row
+    execute procedure on_order_total_change();
+end$$;

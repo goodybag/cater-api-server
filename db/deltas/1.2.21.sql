d0a9181068a9fdf00db0e8cdbe7970778a00d7d8
Preston Pham <prestonp08@gmail.com>
2014-06-23 15:29:37 -0500

    created scheduler action for redemption logging

---
+++ db/deltas/1.2.21.sql
@@ -0,0 +1,21 @@
+-- Delta
+
+DO $$
+  declare version       text := '1.2.21';
+begin
+  raise notice '## Running Delta v% ##', version;
+
+  -- Update version
+  execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
+
+  drop table if exists "users_redemptions";
+
+  create table if not exists "users_redemptions" (
+    id              serial primary key
+  , created_at      timestamp not null default now()
+  , user_id         int not null references users(id) on delete cascade
+  , location        text
+  , amount          int not null default 0
+  , cost            int not null default 0
+  );
+end$$;

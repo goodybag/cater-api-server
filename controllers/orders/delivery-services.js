4bf35bcef08bd00869ccf64c12ac52d265aa0be5
Preston Pham <prestonp08@gmail.com>
2014-07-08 10:56:49 -0500

    update accept templates

--- controllers/orders/delivery-services.js
+++ controllers/orders/delivery-services.js
@@ -2,7 +2,7 @@ var db = require('../../db');
 
 module.exports.accept = function(req, res) {
   if ( !res.locals.order ) return res.send(400, 'Order could not be found');
-  if ( res.locals.order.ds_token_used ) return res.send(400, 'You have already accepted this order, no further action needed!');
+  if ( res.locals.order.ds_token_used ) return res.render('delivery-service/token-used');
 
   db.orders.update(res.locals.order.id, { ds_token_used: 'now()' }, function(err) {
     if ( err ) return res.send(400, 'Could not update order');

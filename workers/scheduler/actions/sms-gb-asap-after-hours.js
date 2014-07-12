7561ad7c6ca733d8569c6374195bdcd8ca33a9ad
Preston Pham <prestonp08@gmail.com>
2014-07-12 16:35:10 -0500

    add weekend notification

---
+++ workers/scheduler/actions/sms-gb-asap-after-hours.js
@@ -0,0 +1,24 @@
+var utils  = require('../../../utils');
+var config = require('../../../config');
+var twilio = require('twilio')(config.twilio.account, config.twilio.token);
+var logger = require('../../../logger').scheduler;
+
+module.exports = function(job, jobDone) {
+  var msg = {
+    from: config.phone.orders
+  , body: [
+      'Goodybag ASAP Order #'
+    , job.data.orderId
+    , 'was placed over the weekend or after hours. Please ensure the delivery service'
+    , 'can fulfill this order.'
+    ].join(' ')
+  };
+
+  utils.async.each(config.deliveryServices.supportPhones, function(phone, textDone) {
+    msg.to = phone;
+    twilio.sendSms(msg, function(error) {
+      if ( error ) logger.error('Could not send sms for job #' + job.id, error);
+      textDone( error );
+    });
+  }, jobDone);
+}

fe3c8b0a46f5f76930fcb062a2d27ac1b3044e03
John Fawcett <jrf0110@gmail.com>
2014-07-29 12:53:03 -0500

    use correct minute

--- models/restaurant.js
+++ models/restaurant.js
@@ -642,8 +642,8 @@ var Restaurant = module.exports = Model.extend({
       // supports either standard or military time
       var datetime = moment(orderParams.date + ' ' + (orderParams.time || '11:59 pm'), 'YYYY-MM-DD hh:mm a');
 
-      var formattedDateTime = datetime.format('YYYY-MM-DD HH:MM:59');
-
+      var formattedDateTime = datetime.format('YYYY-MM-DD HH:mm:00');
+console.log("**", formattedDateTime, orderParams, "**");
       query.joins.lead_times = {
         type: 'left'
       , alias: 'lead_times'

a913191d61a05416a868c3cdeaa683b393a3c45c
John Fawcett <jrf0110@gmail.com>
2014-08-06 12:20:00 -0500

    ensure when setting diff with order types that SOME order type is true

--- public/js/app/views/order-summary-view.js
+++ public/js/app/views/order-summary-view.js
@@ -105,6 +105,15 @@ define(function(require, exports, module) {
       diff.is_delivery          = this.model.get('is_delivery');
       diff.is_pickup            = this.model.get('is_pickup');
 
+      // Hacky, but just make sure. This should probably go in order model
+      // But we're having issues with sometimes getting all false order types
+      // So just do a last stop-gap measure here and make sure one is set
+      if ( !diff.is_delivery_service )
+      if ( !diff.is_delivery )
+      if ( !diff.is_pickup ){
+        diff.is_delivery = true;
+      }
+
       var view = this;
       var sent = this.model.save(diff, {
         patch: true,

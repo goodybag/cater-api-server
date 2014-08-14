aa440f00831d6efd5e2af49767206b279c67491a
Preston Pham <prestonp08@gmail.com>
2014-08-14 13:15:21 -0500

    add triggers

--- db/sql/triggers.sql
+++ db/sql/triggers.sql
@@ -11,3 +11,24 @@ create trigger on_order_type_change
     on orders
     for each row
     execute procedure on_order_type_change();
+
+drop trigger if exists orders_search_update on orders;
+create trigger orders_search_update
+  after update of name
+  on orders
+  for each row
+  execute procedure update_orders_search_vector_from_orders();
+
+drop trigger if exists orders_search_update on restaurants;
+create trigger orders_search_update
+  after update of name
+  on restaurants
+  for each row
+  execute procedure update_orders_search_vector_from_restaurants();
+
+drop trigger if exists orders_search_update on users;
+create trigger orders_search_update
+  after update of name, email, organization
+  on users
+  for each row
+  execute procedure update_orders_search_vector_from_users();

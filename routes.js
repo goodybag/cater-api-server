10453ad5881293c15b7eb8f02f854686f33735ae
John Fawcett <jrf0110@gmail.com>
2014-08-14 15:36:26 -0500

    rogging

--- routes.js
+++ routes.js
@@ -43,8 +43,6 @@ module.exports.register = function(app) {
     );
 
     app.post('/request-to-be-a-caterer'
-    , m.log('wut')
-    , function( req, res, next ){ console.log(req.body ); next(); }
     , m.view( 'landing/restaurant', db.restaurant_requests, {
         layout: 'landing/layout'
       , method: 'insert'

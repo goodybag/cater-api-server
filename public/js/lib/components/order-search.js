1fea30b8e5fefaed680e0e98067775147a8ac852
Preston Pham <prestonp08@gmail.com>
2014-08-19 12:25:21 -0500

    basic react vers complete

--- public/js/lib/components/order-search.js
+++ public/js/lib/components/order-search.js
@@ -1,48 +1,89 @@
+/*
+  Component Hierarchy
+  - OrderSearch
+    - SearchBar
+    - SearchResults
+      - SearchRow
+*/
+
 define([
-  'react'
+  'jquery-loaded'
+, 'react'
 ], function(
-  React
+  $
+, React
 ) {
-
-
-  /*
-    Component Hierarchy
-
-    - OrderSearch
-      - SearchBar
-      - SearchResults
-        - SearchRow
-  */
 
   var OrderSearch = React.createClass({
     getInitialState: function() {
-      return { results: [] };
+      return {
+        orders: []
+      , searchText: ''
+      };
+    },
+
+    handleUserInput: function(searchText) {
+      if ( !searchText ) {
+        return this.setState({
+          searchText: searchText
+        , orders: []
+        });
+      }
+
+      var query = { q: searchText };
+      var this_ = this;
+      $.ajax({
+        url: this.props.endpoint
+      , data: query
+      , success: function(orders, status, xhr) {
+          this_.setState({
+            searchText: searchText
+          , orders: orders
+          });
+        }
+      });
     },
 
     render: function() {
       return (
         <div className="orderSearch">
           <h2 className="component-title">Search orders:</h2>
-          <SearchBar />
-          <SearchResults data={this.props.data} />
+          <SearchBar
+            searchText={this.state.searchText}
+            onUserInput={this.handleUserInput}
+          />
+          <SearchResults
+            orders={this.state.orders}
+            searchText={this.state.searchText} 
+          />
         </div>
       );
     }
   });
 
   var SearchBar = React.createClass({
+    handleChange: function() {
+      this.props.onUserInput(this.refs.searchTextInput.getDOMNode().value);
+    },
+
     render: function() {
       return (
-        <input type="text" placeholder="enter search"></input>
+        <input
+          type="text"
+          placeholder="enter search"
+          value={this.props.searchText}
+          ref="searchTextInput"
+          onChange={this.handleChange}
+        />
       );
     }
   });
 
   var SearchResults = React.createClass({
     render: function() {
-      var rows = this.props.data.map(function( result ){
+      var rows = this.props.orders.map(function( order ){
         return (
-          <SearchRow result={result}/>
+          <SearchRow order={order} key={order.id} />
         );
       });
 
@@ -56,11 +97,11 @@ define([
 
   var SearchRow = React.createClass({
     render: function() {
-      var orderUrl = '/orders/' + this.props.result.order.id;
+      var orderUrl = '/orders/' + this.props.order.id;
       return (
         <div>
-          <p><a href={orderUrl}>Order #{this.props.result.order.id}</a> - ${this.props.result.order.total}</p>
-          <p>{this.props.result.restaurant.name} delivering to {this.props.result.user.name}</p>
+          <p><a href={orderUrl}>Order #{this.props.order.id}</a> - ${this.props.order.total}</p>
+          <p>{this.props.order.restaurant.name} delivering to {this.props.order.user.name}</p>
           <hr/>
         </div>
       );

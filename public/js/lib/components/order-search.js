07885a0a589c936ec1c2065ef637f888c4cfe6b8
Preston Pham <prestonp08@gmail.com>
2014-08-19 11:11:31 -0500

    static component

--- public/js/lib/components/order-search.js
+++ public/js/lib/components/order-search.js
@@ -3,6 +3,17 @@ define([
 ], function(
   React
 ) {
+
+
+  /*
+    Component Hierarchy
+
+    - OrderSearch
+      - SearchBar
+      - SearchResults
+        - SearchRow
+  */
+
   var OrderSearch = React.createClass({
     getInitialState: function() {
       return { results: [] };
@@ -10,8 +21,47 @@ define([
 
     render: function() {
       return (
-        <div class="orderSearch">
-          <h1>Search orders:</h1>
+        <div className="orderSearch">
+          <h2 className="component-title">Search orders:</h2>
+          <SearchBar />
+          <SearchResults data={this.props.data} />
+        </div>
+      );
+    }
+  });
+
+  var SearchBar = React.createClass({
+    render: function() {
+      return (
+        <input type="text" placeholder="enter search"></input>
+      );
+    }
+  });
+
+  var SearchResults = React.createClass({
+    render: function() {
+      var rows = this.props.data.map(function( result ){
+        return (
+          <SearchRow result={result}/>
+        );
+      });
+
+      return (
+        <div className="searchResults">
+          {rows}
+        </div>
+      );
+    }
+  });
+
+  var SearchRow = React.createClass({
+    render: function() {
+      var orderUrl = '/orders/' + this.props.result.order.id;
+      return (
+        <div>
+          <p><a href={orderUrl}>Order #{this.props.result.order.id}</a> - ${this.props.result.order.total}</p>
+          <p>{this.props.result.restaurant.name} delivering to {this.props.result.user.name}</p>
+          <hr/>
         </div>
       );
     }

c0a277f75b9cf6f250eaa8277fe2f4307b12ee3b
Preston Pham <prestonp08@gmail.com>
2014-09-08 14:03:21 -0500

    extract findregions into separate mw

--- routes.js
+++ routes.js
@@ -19,13 +19,11 @@ var m = utils.extend(
 
 module.exports.register = function(app) {
   app.before( m.analytics, m.queryParams(), function( app ){
-    var findRegions = m.db.regions.find({ is_hidden: false }, { order: 'name asc' });
-
-    app.get('/', findRegions, controllers.auth.index);
+    app.get('/', m.findRegions, controllers.auth.index);
     app.get('/login', controllers.auth.login);
     app.post('/login', controllers.auth.login);
-    app.get('/join', findRegions, controllers.auth.registerView);
-    app.post('/join', findRegions, controllers.auth.register);
+    app.get('/join', m.findRegions, controllers.auth.registerView);
+    app.post('/join', m.findRegions, controllers.auth.register);
 
     app.get('/rewards', m.view( 'landing/rewards', {
       layout: 'landing/layout'

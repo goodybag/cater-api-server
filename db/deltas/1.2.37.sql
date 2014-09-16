1c6481407455f08d70866aa467bbc7c52e1b2a47
John Fawcett <jrf0110@gmail.com>
2014-09-16 12:14:01 -0500

    initial ds hours

--- db/deltas/1.2.37.sql
+++ db/deltas/1.2.37.sql
@@ -2,6 +2,7 @@
 
 DO $$
   declare version       text := '1.2.37';
+  declare ds            delivery_services;
 begin
   raise notice '## Running Delta v% ##', version;
 
@@ -15,4 +16,19 @@ begin
   perform add_column( 'delivery_service_hours', 'start_time', E'time not null default \'00:00\'' );
   perform add_column( 'delivery_service_hours', 'end_time', E'time not null default \'00:00\'' );
   perform add_column( 'delivery_service_hours', 'created_at', 'timestamp not null default now()' );
+
+  delete from delivery_service_hours;
+
+  -- Add some default Courier hours basically for cap cour
+  for ds in ( select * from delivery_services ) loop
+    raise notice 'Updating Delivery Service %', ds.id;
+
+    insert into delivery_service_hours
+      ( ds_id, day, start_time, end_time ) values
+      ( ds.id, 1, '09:00', '17:00')
+    , ( ds.id, 2, '09:00', '17:00')
+    , ( ds.id, 3, '09:00', '17:00')
+    , ( ds.id, 4, '09:00', '17:00')
+    , ( ds.id, 5, '09:00', '17:00');
+  end loop;
 end$$;

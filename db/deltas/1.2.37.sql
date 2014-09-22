73989e1504863f1c6a24f014ece07dd4d21ee475
Preston Pham <prestonp08@gmail.com>
2014-09-22 15:17:48 -0500

    set null instead of cascade delete

--- db/deltas/1.2.37.sql
+++ db/deltas/1.2.37.sql
@@ -8,24 +8,16 @@ begin
   -- Update version
   execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
 
+
   alter table orders
+    alter column user_id drop not null,
     drop constraint if exists "orders_user_id_fkey",
     add constraint "orders_user_id_fkey" FOREIGN KEY (user_id)
-      REFERENCES users(id) on delete cascade;
-
-  alter table users_groups
-    drop constraint if exists "users_groups_user_id_fkey",
-    add constraint "users_groups_user_id_fkey" FOREIGN KEY (user_id)
-      REFERENCES users(id) on delete cascade;
-
-  alter table order_statuses
-    drop constraint if exists "order_statuses_order_id_fkey",
-    add constraint "order_statuses_order_id_fkey" FOREIGN KEY (order_id)
-      REFERENCES orders(id) on delete cascade;
+      REFERENCES users(id) on delete set null;
 
   alter table payment_summary_items
     drop constraint if exists "payment_summary_items_order_id_fkey",
     add constraint "payment_summary_items_order_id_fkey" FOREIGN KEY (order_id)
-      REFERENCES orders(id) on delete cascade;
+      REFERENCES orders(id) on delete set null;
 
 end$$;

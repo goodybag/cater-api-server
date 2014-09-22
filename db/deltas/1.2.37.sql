0038710d535e09ede01839e9eb688d633de96f54
Preston Pham <prestonp08@gmail.com>
2014-09-22 15:07:27 -0500

    update foreign keys

--- db/deltas/1.2.37.sql
+++ db/deltas/1.2.37.sql
@@ -10,5 +10,22 @@ begin
 
   alter table orders
     drop constraint if exists "orders_user_id_fkey",
-    add constraint "orders_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) on delete restrict;
+    add constraint "orders_user_id_fkey" FOREIGN KEY (user_id)
+      REFERENCES users(id) on delete cascade;
+
+  alter table users_groups
+    drop constraint if exists "users_groups_user_id_fkey",
+    add constraint "users_groups_user_id_fkey" FOREIGN KEY (user_id)
+      REFERENCES users(id) on delete cascade;
+
+  alter table order_statuses
+    drop constraint if exists "order_statuses_order_id_fkey",
+    add constraint "order_statuses_order_id_fkey" FOREIGN KEY (order_id)
+      REFERENCES orders(id) on delete cascade;
+
+  alter table payment_summary_items
+    drop constraint if exists "payment_summary_items_order_id_fkey",
+    add constraint "payment_summary_items_order_id_fkey" FOREIGN KEY (order_id)
+      REFERENCES orders(id) on delete cascade;
+
 end$$;

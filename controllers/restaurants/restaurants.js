66e4ddf068a5eca85112cff8bbf52749ae79cf2f
Preston Pham <prestonp08@gmail.com>
2014-09-26 14:43:58 -0500

    cleanup

--- controllers/restaurants/restaurants.js
+++ controllers/restaurants/restaurants.js
@@ -171,7 +171,6 @@ module.exports.get = function(req, res) {
       restaurant:       results[1] ? results[1].toJSON() : null,
       defaultAddress:   results[2] ? results[2].toJSON() : null,
       photos:           results[3],
-      defaultPhoto:     results[3][0],
       orderParams:      orderParams
     }
 

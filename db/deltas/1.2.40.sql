c4d0905056101c20810b303ee05d10e4adae3d7c
John Fawcett <jrf0110@gmail.com>
2014-10-06 15:30:46 -0500

    fix description type

--- db/deltas/1.2.40.sql
+++ db/deltas/1.2.40.sql
@@ -9,6 +9,6 @@ begin
   execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
 
   perform add_column( 'orders', 'user_adjustment_amount', 'int not null default 0' );
-  perform add_column( 'orders', 'user_adjustment_description', 'int not null default 0' );
+  perform add_column( 'orders', 'user_adjustment_description', 'text' );
   perform add_column( 'orders', 'restaurant_total', 'int not null default 0' );
 end$$;

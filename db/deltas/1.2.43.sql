e19b7abe9b7771d20e76478f256fa942431a1dc8
Preston Pham <prestonp08@gmail.com>
2014-10-20 15:06:13 -0500

    refactor plain sql

--- db/deltas/1.2.43.sql
+++ db/deltas/1.2.43.sql
@@ -2,8 +2,7 @@
 
 DO $$
   declare version       text := '1.2.43';
-  declare r             record;
-  declare count         integer := 0;
+  declare count         text;
 begin
   raise notice '## Running Delta v% ##', version;
 
@@ -11,23 +10,24 @@ begin
   execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
 
   -- Hook up orders to the correct payment methods
-  for r in ( select * from transaction_errors where id > 245 or id between 212 and 238 )
-  loop
-    count:= count + 1;
-    update orders
-      set
-        payment_method_id = (
-          select payment_method_id
-          from users_payment_methods
-          where users_payment_methods.id = orders.payment_method_id
-        ),
-        payment_status = null
-      where
-        payment_status = 'error' and
-        datetime >= '2014-10-03' and
-        orders.id = r.order_id;
-  end loop;
+  with updated_rows as (update orders
+    set
+      payment_method_id = (
+        select payment_method_id
+        from users_payment_methods
+        where users_payment_methods.id = orders.payment_method_id
+      ),
+      payment_status = null
+    from transaction_errors
+    where
+      payment_status = 'error' and
+      datetime >= '2014-10-03' and
+      transaction_errors.order_id = orders.id  and
+      (transaction_errors.id > 245 or transaction_errors.id between 212 and 238)
+    returning orders.*
+  )
+  select count(*) from updated_rows into count;
+
   raise notice 'Updated % orders', count;
-  return;
 
 end$$;

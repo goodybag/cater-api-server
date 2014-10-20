057d214fde6d30f180b3356febcd7de139d747f1
John Fawcett <jrf0110@gmail.com>
2014-10-20 16:16:57 -0500

    re-register partials in dev mode

--- app.js
+++ app.js
@@ -77,6 +77,15 @@ app.configure(function(){
     return next();
   });
 
+  // Re-register partials
+  app.use( function( req, res, next ){
+    if ( config.isDev ){
+      partials.register( hbs );
+    }
+
+    return next();
+  });
+
   app.use(app.router);
   app.use( function( req, res, next ){
     if ( req.route && req.route.path ){

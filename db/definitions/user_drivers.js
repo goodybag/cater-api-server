d90bf7ad48ce342b7993957df3b539588eea1d10
John Fawcett <jrf0110@gmail.com>
2014-10-22 10:16:08 -0500

    WIP

---
+++ db/definitions/user_drivers.js
@@ -0,0 +1,30 @@
+/**
+ * User Drivers Schema
+ */
+
+if (typeof module === 'object' && typeof define !== 'function') {
+  var define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+var
+  types = require('../data-types')
+;
+
+define(function(require) {
+  var definition = {};
+  definition.name = 'user_drivers';
+
+  definition.schema = {
+    id:                 { type: types.serial, pk: true }
+    user_id:            { type: types.serial
+                        , nullable: false
+                        , references: { table: 'users', column: 'id', onDelete: 'cascade' }
+                        }
+  };
+
+  definition.indices = {};
+
+  return definition;
+});

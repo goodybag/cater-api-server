fb9699edfcb3db9b5eadde8ecdba8a3461558424
John Fawcett <jrf0110@gmail.com>
2014-10-23 17:00:37 -0500

    WIP

--- public/js/app/models/payment-summary.js
+++ public/js/app/models/payment-summary.js
@@ -1,7 +1,23 @@
 define(function(require, exports, module) {
   var utils = require('utils');
+  var PaymentSummaryItems = require('app/collections/payment-summary-items');
 
   return module.exports = utils.Model.extend({
+    initialize: function( attr, options ){
+      if ( attr.items ){
+        this.items = new PaymentSummaryItems( attr.items );
+        delete attr.items;
+      }
+    }
 
+  , toJSON: function(){
+      var json = utils.Model.prototype.toJSON.call( this );
+
+      if ( this.items ){
+        json.items = this.items.toJSON();
+      }
+
+      return json;
+    }
   });
 });
--- public/js/app/pages/admin/ol-greg.js
+++ public/js/app/pages/admin/ol-greg.js
@@ -4,6 +4,10 @@ define(function(require){
   var async         = require('async');
   var utils         = require('utils');
   var PMSItem       = require('app/models/payment-summary-item');
+  var Summaries     = require('app/collections/payment-summaries');
+  var Orders        = require('app/collections/orders');
+  var Order         = require('app/models/order');
+
 
   return Object.create({
     init: function( options ){
@@ -12,20 +16,32 @@ define(function(require){
     }
 
   , generateSummary: function( rid, d1, d2, callback ){
-      // Get orders for restaurant date range
-      $.ajax({
-        type:   'GET'
-      , url:    [ '/api/restaurants'
-                , rid
-                , 'orders'
-                , utils.queryParams({
-                    start_date: d1
-                  , end_date:   d2
-                  , limit:      'all'
-                  , status:     'accepted'
-                  })
-                ]
-      })
+      var orders = new Orders( null, { restaurant_id: rid } );
+      var summaries = new Summaries( null, {
+        restaurant_id: options.restaurant.id
+      });
+
+      var fetchOrdersAndSaveNewSummary = utils.async.parallel.bind( utils.async, {
+        orders: function( done ){
+          orders.fetch({
+            start_date: d1
+          , end_date:   d2
+          , limit:      'all'
+          , status:     'accepted'
+          , success:    function(){ done( null, orders ); }
+          , error:      done
+          })
+        }
+
+      , summary: function( done ){
+          var summary = summaries.createModel();
+          summary.save( null, {
+            success: function(){ done( null, summary ); }
+          , error:   done
+          });
+        }
+      });
+
       // Create new payment summary
       // for each order, create new pms item with order
       // save payment summary

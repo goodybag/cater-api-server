9d9c24d85deda142c1e231df91e9c0f945c05d92
Preston Pham <prestonp08@gmail.com>
2014-10-23 13:10:35 -0500

    init

---
+++ db/definitions/amenities.js
@@ -0,0 +1,59 @@
+/**
+ * Amenities Schema
+ */
+
+if (typeof module === 'object' && typeof define !== 'function') {
+  var define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+var
+  types = require('../data-types')
+;
+
+define(function(require) {
+  var definition = {};
+  definition.name = 'amenities';
+
+  definition.schema = {
+    id: {
+      type: types.serial
+    , nullable: false
+    , unique: true
+    }
+  , created_at: {
+      type: types.timestamptz
+    , nullable: false
+    , default: 'NOW()'
+    }
+  , name: {
+      type: types.text
+    }
+  , description: {
+      type: types.text
+    }
+  , price: {
+      type: types.int
+    , nullable: false
+    , default: 0
+    }
+  , restaurant_id: {
+      type: types.int
+    , nullable: false
+    , references: {
+        table: 'restaurants'
+      , column: 'id'
+      , onDelete: 'cascade'
+      }
+    }
+  , enabled: {
+      type: types.boolean
+    , default: false
+    }
+  };
+
+  definition.indices = {};
+
+  return definition;
+});
---
+++ db/definitions/order-amenities.js
@@ -0,0 +1,53 @@
+/**
+ * order,amenities Schema
+ */
+
+if (typeof module === 'object' && typeof define !== 'function') {
+  var define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+var
+  types = require('../data-types')
+;
+
+define(function(require) {
+  var definition = {};
+  definition.name = 'order-amenities';
+
+  definition.schema = {
+    id: {
+      type: types.serial
+    , nullable: false
+    , unique: true
+    }
+  , created_at: {
+      type: types.timestamptz
+    , nullable: false
+    , default: 'NOW()'
+    }
+  , order_id: {
+      type: types.int
+    , nullable: false
+    , references: {
+        table: 'orders'
+      , column: 'id'
+      , onDelete: 'cascade'
+      }
+    }
+  , amenity_id: {
+      type: types.int
+    , nullable: false
+    , references: {
+        table: 'amenities'
+      , column: 'id'
+      , onDelete: 'cascade'
+      }
+    }
+  };
+
+  definition.indices = {};
+
+  return definition;
+});

ea2d6c733af96a86596d1a0b020a5aebfc5090a5
John Fawcett <jrf0110@gmail.com>
2014-10-23 11:48:18 -0500

    logic scaffolding for generate fn

--- public/js/app/pages/admin/ol-greg.js
+++ public/js/app/pages/admin/ol-greg.js
@@ -8,5 +8,12 @@ define(function(require){
     init: function(){
 
     }
+
+  , generateSummary: function( rid, d1, d2, callback ){
+      // Get orders for restaurant date range
+      // Create new payment summary
+      // for each order, create new pms item with order
+      // save payment summary
+    }
   });
 });

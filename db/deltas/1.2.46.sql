7e6467b96633808b693b56c4a59fea8474a22fc3
John Fawcett <jrf0110@gmail.com>
2014-10-24 11:10:47 -0500

    resolve delta conflict

---
+++ db/deltas/1.2.46.sql
@@ -0,0 +1,15 @@
+-- Delta
+
+DO $$
+  declare version       text := '1.2.46';
+begin
+  raise notice '## Running Delta v% ##', version;
+
+  -- Update version
+  execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
+
+  create table if not exists "user_drivers" ();
+
+  perform add_column( 'user_drivers', 'id', 'serial primary key' );
+  perform add_column( 'user_drivers', 'user_id', 'int references user("id") on delete cascade' );
+end$$;

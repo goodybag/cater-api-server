e7f8ed02820543d8442dd82393e4e47009bb7f22
Preston Pham <prestonp08@gmail.com>
2014-11-10 15:58:52 -0600

    set up rewards views

--- workers/scheduler/actions/redeem-reward.js
+++ workers/scheduler/actions/redeem-reward.js
@@ -20,7 +20,12 @@ var notifyGb = function( reward, userId ){
     };
 
     app.render('rewards/notify-gb-confirmation', context, function(err, html) {
-      if (err) slogger.error(err);
+      if ( err ) {
+        console.log(err);
+        slogger.error('Could not notify gb', { error: err });
+        return done(err);
+      }
+
       utils.sendMail2({
         to:       config.emails.rewards
       , from:     config.emails.info
@@ -33,25 +38,26 @@ var notifyGb = function( reward, userId ){
 
 var notifyUser = function( reward, userId ){
   return function(done, results) {
-    var user = results.getUser;
+    var context = {
+      layout: 'email-layout'
+    , reward: reward
+    , user:   results.getUser
+    , config: config
+    };
 
-    utils.sendMail2({
-      to: user.email
-    , from: config.emails.info
-    , subject: [ '[Rewards] Redemption Confirmation' ].join('')
-    , html: [
-        '<p>Thank you for ordering with Goodybag! This email is to confirm that you have redeemed a giftcard.</p>'
-      , '<br>'
-      , '<strong>Details:</strong><br>'
-      , '<ul>'
-      , '  <li>Type: ', reward.location, '</li>'
-      , '  <li>Amount: $', helpers.dollars( reward.amount ), '</li>'
-      , '  <li>Points Used: ', reward.cost, '</li>'
-      , '</ul>'
-      , '<p>Your giftcard is on its way! Note that delivery may take up to two weeks. Please contact us if you have further inquiries.</p>'
-      , '<p>Goodybag</p>'
-      ].join('')
-    }, done);
+    app.render('rewards/notify-user-confirmation', context, function(err, html) {
+      if ( err ) {
+        slogger.error('Could not notify user', err);
+        return done(err);
+      }
+
+      utils.sendMail2({
+        to:     results.getUser.email
+      , from:   config.emails.info
+      , subject: '[Rewards] Redemption Confirmation'
+      , html:   html
+      }, done);
+    });
   };
 };
 

7719a5a15b0638072efd9823720d0c47194fd9fb
Preston Pham <prestonp08@gmail.com>
2015-01-02 10:14:42 -0600

    add fancy tests

---
+++ test/unit/user.js
@@ -0,0 +1,30 @@
+var utils   = require('../../utils');
+var db      = require('../../db');
+var Models  = require('../../models');
+var assert = require('assert');
+
+describe ('models.User', function(){
+  it('isRestaurant for non restaurant owner', function() {
+    var user = new Models.User();
+    assert(!user.isRestaurant(34));
+  });
+
+  it('isRestaurant for restaurant owner', function() {
+    var user = new Models.User();
+    user.attributes.groups = ['restaurant'];
+    user.attributes.restaurant_ids = [1,2,3];
+    assert(user.isRestaurant(2));
+  });
+
+  it('isAdmin for admin user', function() {
+    var user = new Models.User();
+    user.attributes.groups = ['client', 'admin'];
+    assert(user.isAdmin());
+  });
+
+  it('isAdmin for client user', function() {
+    var user = new Models.User();
+    user.attributes.groups = ['client'];
+    assert(!user.isAdmin());
+  });
+});

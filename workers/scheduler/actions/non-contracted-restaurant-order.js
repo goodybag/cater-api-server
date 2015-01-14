fc104b7c71f857e51dbed0c84443428fcd5f1b72
Preston Pham <prestonp08@gmail.com>
2015-01-14 12:20:14 -0600

    add sms notification

---
+++ workers/scheduler/actions/non-contracted-restaurant-order.js
@@ -0,0 +1,22 @@
+var config    = require('../../../config');
+var utils     = require('../../../utils');
+var slogger   = require('../logger');
+var db        = require('../../../db');
+var notifier  = require('../../../lib/order-notifier');
+
+require('../../../lib/order-notifications');
+
+module.exports = function(job, done) {
+  var logger = slogger.create('Notify CS of non-contracted restaurant order', {
+    data: job
+  });
+
+  var orderId = job.data.orderId;
+
+  logger.info('Sending CS sms and email notifications');
+
+  utils.async.parallel([
+    notifier.send.bind( notifier, 'cs-non-contracted-order-email', orderId )
+  , notifier.send.bind( notifier, 'cs-non-contracted-order-sms', orderId )
+  ], done );
+};

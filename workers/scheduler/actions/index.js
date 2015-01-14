4505b680adf7e9d9219cb9d75da3d4e3832bc5dc
Preston Pham <prestonp08@gmail.com>
2015-01-14 13:58:07 -0600

    refactor

--- workers/scheduler/actions/index.js
+++ workers/scheduler/actions/index.js
@@ -2,18 +2,14 @@ var fs = require('fs');
 var path = require('path');
 var scheduler = require('../../../lib/scheduler');
 var actions = [];
-
 fs.readdirSync( __dirname ).filter( function( f ){
   return fs.statSync( path.join( __dirname, f ) ).isFile();
 }).filter( function( f ){
   return f.slice(-3) === '.js' && f !== 'index.js';
 }).forEach( function( f ){
   var action = require( path.join( __dirname, f ) );
-  actions.push(action);
-});
-
-actions.forEach( function( action ){
   scheduler.registerAction(action.name, action.fn);
+  actions.push(action);
 });
 
 module.exports = actions;

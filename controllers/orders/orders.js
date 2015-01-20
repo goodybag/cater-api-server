a7b274bbbac59c922d41e695f1e32a733128165d
John Fawcett <jrf0110@gmail.com>
2015-01-19 21:36:50 -0600

    ensure we add legacy group `restaurant` when restaurnat has order authed

--- controllers/orders/orders.js
+++ controllers/orders/orders.js
@@ -42,6 +42,7 @@ module.exports.auth = function(req, res, next) {
   // There was a review token, so this is a restaurant
   if ( reviewToken && (reviewToken === req.order.review_token) ){
     req.order.isRestaurantManager = true;
+    req.user.attributes.groups.push('restaurant');
     req.user.attributes.groups.push('order-restaurant');
   }
 

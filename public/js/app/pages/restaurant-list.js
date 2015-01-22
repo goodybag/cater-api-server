2b2d72c61cd9f393d53224e88fa252405961e09a
Preston Pham <prestonp08@gmail.com>
2015-01-22 10:28:11 -0600

    fix inline views

--- public/js/app/pages/restaurant-list.js
+++ public/js/app/pages/restaurant-list.js
@@ -1,18 +1,44 @@
 define( function( require ){
-  var $               = require('jquery');
+  var $               = require('jquery-loaded');
   var utils           = require('utils');
 
   var page = {
     init: function(options) {
       var ListView = require('app/views/restaurants-list-view');
+      var SortView = require('app/views/restaurant-sort-view');
+      var OrderParamsView = require('app/views/order-params-view')
+      var OrderParams = require('app/models/order-params');
+      var FiltersView = require('app/views/restaurant-filters-view');
+      var SearchView = require('app/views/restaurant/search-view');
+
+      var sortView = new SortView({
+        el: '#sort'
+      , inputSelector: '[name="list-sort"]:checked'
+      });
+
       var listView = new ListView({
         el: '#main'
-        , searchUrl: '/restaurants'
+      , searchUrl: '/restaurants'
       });
-      listView.addFilter(options.orderParamsView);
-      listView.addFilter(options.sortView);
-      listView.addFilter(options.filtersView);
-      listView.addFilter(options.searchView);
+
+      var orderParamsView = new OrderParamsView({
+        model: new OrderParams()
+      , el: '.restaurant-params'
+      });
+
+      var filtersView = new FiltersView({ el: '#filters' });
+
+      var searchView = new SearchView({
+        el: '#search'
+      , inputSelector: '.search-input'
+      });
+
+      listView.addFilter(sortView);
+      listView.addFilter(orderParamsView);
+      listView.addFilter(filtersView);
+      listView.addFilter(searchView);
+
+      $('[data-role="collapsible"]').gb_collapsible();
     }
   };
 

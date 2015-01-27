f8f45f8b2364eae6e73215822f8bf39facd1968d
Preston Pham <prestonp08@gmail.com>
2015-01-26 18:14:37 -0600

    wip

--- public/js/app/views/admin/table-list-view.js
+++ public/js/app/views/admin/table-list-view.js
@@ -1,11 +1,30 @@
 define(function(require){
   var utils = require('utils');
 
+  // todo extract
+  var ToggleHiddenView = utils.View.extend({
+    events: {
+      'click .toggle-hidden': 'toggleHiddenOnClick'
+    }
+  , isHidden: function() {
+      return this.$el.find('').data('is-hidden') === 'true';
+    }
+  , toggleHiddenOnClick: function(e) {
+      // var model = new Restaurant ({id: this.options.id, isHidden: this.isHidden() });
+    }
+  });
+
   return utils.View.extend({
     initialize: function() {
       var this_ = this;
       // Enable sub components
       $('[data-role="popover"]').gb_popover();
+
+      this.$el.find('.table-list-item').each(function(idx, el) {
+        // var $el = $(el);
+        // new thv = new ToggleHiddenView({ el: '.toggle-selector-here', restaurantId: $el.data('id') });
+
+      });
     },
   });
 });

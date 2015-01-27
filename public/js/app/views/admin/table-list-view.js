3a1f7233cb1a4d20e5f18d36e144befab944caf4
Preston Pham <prestonp08@gmail.com>
2015-01-27 09:22:57 -0600

    toggle setup

--- public/js/app/views/admin/table-list-view.js
+++ public/js/app/views/admin/table-list-view.js
@@ -1,30 +1,53 @@
 define(function(require){
   var utils = require('utils');
+  var Restaurant = require('app/models/restaurant');
 
   // todo extract
   var ToggleHiddenView = utils.View.extend({
-    events: {
-      'click .toggle-hidden': 'toggleHiddenOnClick'
+    events: function() {
+      var ev = 'click ' + this.options.toggleSelector;
+      var events = {};
+      events[ev] = 'toggleHiddenOnClick';
+      return events;
     }
-  , isHidden: function() {
-      return this.$el.find('').data('is-hidden') === 'true';
+
+  , initialize: function() {
+      this.options.toggleSelector = this.options.toggleSelector || '.is-hidden-toggle';
     }
+
   , toggleHiddenOnClick: function(e) {
-      // var model = new Restaurant ({id: this.options.id, isHidden: this.isHidden() });
+      var restaurant = this.options.restaurant;
+      restaurant.save({ is_hidden: !restaurant.get('is_hidden') }, {
+        success: function(model, response, options) {
+          // swap toggle icon
+          console.log('success', arguments);
+        },
+
+        error: function(model, response, options) {
+          // alert!!!
+          console.log('error', arguments);
+        }
+      });
     }
   });
 
   return utils.View.extend({
     initialize: function() {
       var this_ = this;
-      // Enable sub components
+
+      // Enable popovers
       $('[data-role="popover"]').gb_popover();
 
-      this.$el.find('.table-list-item').each(function(idx, el) {
-        // var $el = $(el);
-        // new thv = new ToggleHiddenView({ el: '.toggle-selector-here', restaurantId: $el.data('id') });
-
+      // Init toggle visibility views
+      this.options.itemSelector = this.options.itemSelector || '.table-list-item';
+      this.$el.find(this.options.itemSelector).each(function(idx, el) {
+        var $el = $(el);
+        var thv = new ToggleHiddenView({
+          el: el,
+          restaurant: new Restaurant($el.data('restaurant'))
+        });
       });
+
     },
   });
 });

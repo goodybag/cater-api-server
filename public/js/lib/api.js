4958d08cbdcf9bcb0ab2bfc6c55e4c6df3b35682
John Fawcett <jrf0110@gmail.com>
2015-01-28 09:00:27 -0600

    rogs

--- public/js/lib/api.js
+++ public/js/lib/api.js
@@ -1,8 +1,8 @@
 define(function(require){
-  var utils = require('utils');
-  var config = require('config');
-  var resource = require('./resource');
-console.log('setting api at', config.baseUrl);
+  var utils     = require('utils');
+  var config    = require('config');
+  var resource  = require('./resource');
+
   var api = resource( config.baseUrl + '/api' );
 
   api.users       = api('users');

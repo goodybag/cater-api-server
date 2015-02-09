f601b4d3e0c8f0ca248e14e6af2df94ee7140bed
John Fawcett <jrf0110@gmail.com>
2015-02-09 15:42:15 -0600

    fix dat js

--- public/js/lib/table-list.js
+++ public/js/lib/table-list.js
@@ -15,14 +15,19 @@
     this.$el = $(el);
     this.options = this.getOptions(options);
 
-    this.initEvents();
-    this.numSelected = this.getNumSelected();
+    this.init();
 
     return this;
   };
 
   TableList.DEFAULTS = {
 
+  };
+
+  TableList.prototype.init = function(){
+    this.$checkboxes = this.$el.find('.list-items [type="checkbox"]');
+    this.initEvents();
+    this.numSelected = this.getNumSelected();
   };
 
   TableList.prototype.getDefaults = function() {
@@ -35,16 +40,57 @@
   };
 
   TableList.prototype.initEvents = function( first_argument ){
-    this.$el.find('.list-header > .item-selector > input').change( this.onItemSelectorAllChange );
-    this.$el.find('.list-items .item-selector > input').change( this.onItemSelectorChange );
-  };
-
-  TableList.prototype.state = function( state ){
-
+    this.$el.find('.list-header > .item-selector > input').change( this.onItemSelectorAllChange.bind( this ) );
+    this.$el.find('.list-items .item-selector > input').change( this.onItemSelectorChange.bind( this ) );
   };
 
   TableList.prototype.getNumSelected = function(){
-    return this.$el.find('.list-items .item-selector > [checked]').length;
+    return this.$checkboxes.filter(':checked').length;
+  };
+
+  TableList.prototype.checkNumSelected = function(){
+    this.numSelected = this.getNumSelected();
+
+    if ( this.numSelected === 0 ){
+      this.$el.removeClass('some-checked');
+    } else {
+      this.$el.addClass('some-checked');
+    }
+
+    return this;
+  };
+
+  TableList.prototype.selectAll = function(){
+    this.$checkboxes.each( function(){
+      this.checked = true;
+    });
+
+    this.checkNumSelected();
+
+    return this;
+  };
+
+  TableList.prototype.deselectAll = function(){
+    this.$checkboxes.each( function(){
+      this.checked = false;
+    });
+
+    this.checkNumSelected();
+
+    return this;
+  };
+
+  // Event Handlers
+  TableList.prototype.onItemSelectorAllChange = function( e ){
+    if ( e.target.checked ){
+      this.selectAll();
+    } else {
+      this.deselectAll();
+    }
+  };
+
+  TableList.prototype.onItemSelectorChange = function( e ){
+    this.checkNumSelected();
   };
 
   var Plugin = function( options ){

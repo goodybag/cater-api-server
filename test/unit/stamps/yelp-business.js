41d8bd2a4618049b50c61932443b00a81e604da0
Preston Pham <prestonp08@gmail.com>
2015-02-13 17:10:03 -0600

    mock cuisines for yelp-business

--- test/unit/stamps/yelp-business.js
+++ test/unit/stamps/yelp-business.js
@@ -3,18 +3,13 @@ var config      = require('../../../config');
 var db          = require('../../../db');
 var ybusinesses = require('stamps/yelp-business');
 
-var allCuisines = [];
-
-// Make sure we can inform the yelpbiz what `allCuisines` is
-before(function( done ){
-  db.cuisines.find( {}, { limit: 'all' }, function( error, cuisines ){
-    if ( error ) done( error );
-
-    allCuisines = cuisines.map( function( c ){ return c.name; });
-
-    done();
-  });
-});
+var allCuisines = [
+  'American',
+  'Asian Fusion',
+  'Chinese',
+  'Pizza',
+  'Thai'
+];
 
 describe('Stamps', function(){
   describe('YelpBusiness', function(){

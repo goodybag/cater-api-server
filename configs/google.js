50f246212c2bdbf9bc4ea82138da2211609393de
John Fawcett <jrf0110@gmail.com>
2015-02-19 11:04:31 -0600

    google config

---
+++ configs/google.js
@@ -0,0 +1,15 @@
+/**
+ * Config.Google
+ */
+
+module.exports = {
+  apikey: 'AIzaSyBbsgtm6Tqdh3ZyWfRj2Mg_eSZDW8ajLss'
+
+, geocoding: {
+    limit: { daily: 2500, second: 5 }
+  }
+  
+, directions: {
+    limit: { daily: 10000, second: 2 }
+  }
+};

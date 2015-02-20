8039107beaf2960ad1fafa6fa931ec77f7c22ec7
Preston Pham <prestonp08@gmail.com>
2015-02-20 14:49:34 -0600

    scaffold dummy testss

---
+++ test/unit/restaurant.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var restaurant = require('../../public/js/models/restaurant');
+
+describe('', function() {
+  it('should get lead time for delivery order', function() {
+    assert(false);
+  });
+
+  it('should get lead time for pickup/courier order', function() {
+    assert(false);
+  });
+
+  it('should default lead time to delivery leadtimes', function() {
+    assert(false);
+  });
+});

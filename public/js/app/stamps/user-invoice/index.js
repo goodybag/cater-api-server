c281130b27bac2055ccac492cd4bb38b318fa48d
John Fawcett <jrf0110@gmail.com>
2015-02-24 12:07:51 -0600

    scaffold business logic

---
+++ public/js/app/stamps/user-invoice/index.js
@@ -0,0 +1,28 @@
+/**
+ * User-invoice
+ */
+if ( typeof module === 'object' && module && typeof module.exports === 'object' ){
+  var isNode = true, define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+define( function( require, exports, module ){
+  var stampit = require('stampit');
+  var utils   = require('utils');
+
+  module.exports = stampit();
+
+var stamps = {
+  
+};
+
+module.exports = module.exports.compose.apply(
+  module.exports
+, utils.values( stamps )
+);
+
+utils.extend( module.exports, stamps );
+
+return module.exports;
+});

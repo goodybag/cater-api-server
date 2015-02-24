c2c23a36b1fb0cb44ac51d4d737ec852151f5aba
Preston Pham <prestonp08@gmail.com>
2015-02-24 10:39:47 -0600

    config

--- configs/scheduler.js
+++ configs/scheduler.js
@@ -3,8 +3,8 @@
  */
 
 module.exports = {
-  cron: '*/10 * * * * *'
-, start: true
-, limit: 2 // max # of parallel jobs
-, workers: 1 // # of workers spawned per process
+  cron: '*/10 * * * * *'  // poll db every 10 secs
+, start: true             // enable/disable scheduler
+, limit: 4                // max # of parallel jobs
+, workers: 1              // # of workers spawned per process
 };

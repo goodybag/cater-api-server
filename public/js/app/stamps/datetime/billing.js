a2c83882f9da3b55fa03a5a4861c5cb1876e27e7
John Fawcett <jrf0110@gmail.com>
2015-03-11 12:24:50 -0500

    add getPreviousBillingPeriod() and isStartOfBillingPeriod()

--- public/js/app/stamps/datetime/billing.js
+++ public/js/app/stamps/datetime/billing.js
@@ -32,5 +32,31 @@ define( function( require, exports, module ){
         , endDate:    end.format('YYYY-MM-DD')
         });
       }
+
+    , getPreviousBillingPeriod: function(){
+        var start, end;
+
+        // Zero out values less granular than `month`
+        var d     = this._moment.toString();
+        var start = moment( d ).startOf('month');
+        var end   = moment( d ).startOf('month');
+
+        if ( this._moment.date() < 15 ){
+          start.subtract( 1, 'month' ).set( 'date', 16 );
+          end.subtract( 1, 'month' ).endOf('month');
+        } else {
+          start.set( 'date', 1 );
+          end.set( 'date', 15 );
+        }
+
+        return billingP({
+          startDate:  start.format('YYYY-MM-DD')
+        , endDate:    end.format('YYYY-MM-DD')
+        });
+      }
+
+    , isStartOfBillingPeriod: function(){
+        return [ 1, 16 ].indexOf( this._moment.date() ) > -1;
+      }
     });
 });

1b3f2acdcb2ca92a8a3d8a6864981f5bf3a1d9d8
John Fawcett <jrf0110@gmail.com>
2015-03-11 15:17:15 -0500

    debug

--- workers/invoices/send.js
+++ workers/invoices/send.js
@@ -1,25 +1,31 @@
 var CONCURRENCY = 10;
 
+var db        = require('db');
 var invoices  = require('stamps/user-invoice');
 var now       = require('stamps/datetime')();
 var logger    = require('../../lib/logger').create('Worker-SendInvoices');
 
-if ( !now.isFirstDayOfBillingPeriod() ){
+if ( !now.isStartOfBillingPeriod() ){
+  console.log('not first day');
   process.exit(0);
 }
 
+var errors = [];
+
 var period = now.getPreviousBillingPeriod();
 
-db.user_invoices.findAysnc({
+db.user_invoices.findAsync({
   status: 'pending'
-, billing_period_begin: period.startDate
+, billing_period_start: period.startDate
 , billing_period_end:   period.endDate
 })
 .catch( function( error ){
-  logger.
-  throw error;
-});
+  console.log('error', error);
+  logger.error({ error: error });
+  process.exit(1);
+})
 .map( function( invoice ){;
+  console.log('sending', invoice.id)
   return invoices.create( invoice )
     .sendEmailAsync()
     .error( function( error ){

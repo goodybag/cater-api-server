1dc1eca10713bd8558f3b40dd5f435ac28db1295
John Fawcett <jrf0110@gmail.com>
2015-03-16 16:58:18 -0500

    Update jquery.popover.js

--- public/js/lib/jquery.popover.js
+++ public/js/lib/jquery.popover.js
@@ -78,7 +78,6 @@ define( function( require, exports, module ){
     });
 
     this.$body.on( 'scroll', _.throttle( function( e ){
-    console.log(e.target.scrollTop);
       if ( e.target.scrollTop > 0 ){
         this.$wrapper.addClass('has-scrolled');
       } else {

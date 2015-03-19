ce7ae263b253a4adf2f29e715a7084a5ddd0dd78
paulserraino <pserraino99@gmail.com>
2015-03-19 09:59:26 -0600

    signup steps fsm

---
+++ public/js/app/views/restaurant/join.js
@@ -0,0 +1,37 @@
+define(function (require, exports, module) {
+  var utils = require('utils');
+  var FormView = require('../form-view');
+
+  return module.exports = FormView.extend({
+    events: {
+      'click .btn-continue': 'nextStep'
+    }
+  , initialize: function () {
+      console.log('init');
+      this.step = 1;
+    }
+  , getStep: function (step) {
+      return this.$el.find(
+        '.form-step[data-step=":step"]'.replace(':step', step)
+      );
+  }
+  , nextStep: function (e) {
+      if (e) {
+        e.preventDefault();
+      }
+
+      if ( this.step >= this.options.steps ) {
+        // submit form
+        return;
+      }
+
+      this.$current = this.getStep( this.step );
+      this.$next = this.getStep( this.step + 1 );
+      this.$steps = this.$el.find('.form-step');
+
+      this.$steps.addClass('hide');
+      this.$next.removeClass('hide')
+      this.step++;
+    }
+  });
+});

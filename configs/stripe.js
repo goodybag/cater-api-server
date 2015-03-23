81f86bfc31bcc5f1107d73e0de80b436e560929b
Preston Pham <prestonp08@gmail.com>
2015-03-23 12:44:38 -0500

    set up client side stripe

--- configs/stripe.js
+++ configs/stripe.js
@@ -6,10 +6,12 @@ if ( ['dev', 'production'].indexOf( env ) === -1 ){
 var stripe = {
   dev: {
     secret: 'sk_test_XksbwDuLK1xbpOFaSVLVf9qZ'
+  , public: 'pk_test_9N1srz1fVmWGyRAAhmzAA7aP'
   }
 
 , production: {
     secret: 'sk_live_kjAFUHK2tYc6BRHRyeMlZPON'
+  , public: 'pk_live_kShOkv0NSx9Lj1qWGqxAXotN'
   }
 }
 

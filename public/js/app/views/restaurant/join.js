7c4652ea2094d0c40faca6eeecd53e9aea828231
paulserraino <pserraino99@gmail.com>
2015-03-26 17:33:32 -0600

    wip field mapping

--- public/js/app/views/restaurant/join.js
+++ public/js/app/views/restaurant/join.js
@@ -9,13 +9,63 @@ define(function (require, exports, module) {
       'click .add-lead-time'          : 'addLeadTime'
     , 'click .btn-add-contact'        : 'addContact'
     , 'click .add-hours'              : 'addHours'
+    , 'click .add-days'               : 'addDays'
     , 'click .datetime-days-list > li': 'addDeliveryHours'
     , 'click .btn-continue'           : 'saveAndContinue'
     }
+
+    //The majority of the fields apply to restaurants,
+    // any field related to the user will have the
+    // format user_{field name}
+  , fieldMap: {
+      user_name    : ''
+    , user_phone   : ''
+    , user_email   : ''
+    , name         : ''
+    , websites     : ''
+    , services     : '' // not in db
+    , yelp_url     : '' // not in db
+    , cuisine      : ''
+    , price        : ''
+    , street       : ''
+    , city         : ''
+    , state        : ''
+    , zip          : ''
+    , display_phone: ''
+    , billing_email: ''
+    , meal_style   : ''
+    , tags         : ''
+    , amenities    : '' // map to restaurant
+    , menu         : '' // not in db (requires file uploader)
+    , logo_url     : '' //(requires file uploader)
+    , minimum_order: ''
+    , contacts     : ''
+    , gb_fee       : '' // this may be the wrong column
+    , pickup_lead_time: '' // (require some extra mapping)
+    , delivery_times: '' // (require some extra mapping)
+    , billing_name : '' // not in db
+    , billing_phone: ''// not in db
+    , billing_street: ''
+    , billing_street2: ''
+    , billing_city : ''
+    , billing_state: ''
+    , billing_zip  : ''
+    , terms_name   : '' // not in db
+    , terms_contact_name: '' // not in db
+    , terms_date: '' // not in db
+    }
+  , fieldGetters: {
+
+    }
   , initialize: function () {
       console.log('init');
+
+      //expose model for _dev_
+      window.model = this.model;
+
       // TODO: use local storage to store restaurant data
       this.store = window.localStorage;
+
       //this.model.set(this.store.getItem('gb_restaurant'));
       // TODO: use cookies to store view state
       this.step = 1;
@@ -69,12 +119,18 @@ define(function (require, exports, module) {
       if (e) e.preventDefault();
       this.$el.find('.contact-list').append(Handlebars.partials.restaurant_contact_info_fields({}));
     }
+  , addDays: function (e) {
+      if (e) e.preventDefault();
+      console.log('adding delivery days')
+    }
   , addHours: function (e) {
       if (e) e.preventDefault();
     }
   , addDeliveryHours: function (e) {
       if (e) e.preventDefault();
-      console.log('click', e.target.getAttribute('data-day'))
+      var $el = $(e.target);
+      console.log('click', $el.data('day'))
+      $el.toggleClass('active');
       //this.set('hours_of_operation', hours)
     }
 

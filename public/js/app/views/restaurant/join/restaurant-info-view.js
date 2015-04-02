e8bc475ce32cc21c25d9293311f69b4cb6e7fb27
paulserraino <pserraino99@gmail.com>
2015-04-02 14:53:28 -0600

    wip restaurant info view

--- public/js/app/views/restaurant/join/restaurant-info-view.js
+++ public/js/app/views/restaurant/join/restaurant-info-view.js
@@ -5,9 +5,9 @@ define(function (require, exports, module) {
   var spinner = require('spinner');
 
   return module.exports = BaseView.extend({
-    events: {
-      'click .btn-add-contact'        : 'addContact'
-    }
+    events: utils.extend(BaseView.prototype.events, {
+      'click .btn-add-contact': 'addContact'
+    })
   , fieldMap: {
         yelp_business_id: '.yelp-url' 
       , cuisine      : '.restaurant-cuisine'
@@ -21,9 +21,18 @@ define(function (require, exports, module) {
       , menu         : '.restaurant-menu' // not in db (requires file uploader)
       , logo_url     : '.restaurant-logo' //(requires file uploader)
       , minimum_order: '.order-minimum'
-      , contacts     : ''
+      , contacts     : '.contact-group'
     }
   , fieldGetters: {
+      cuisine: function () {
+        var cuisines = this.$el.find(this.fieldMap.cuisine).val();
+        return cuisines ? cuisines.replace(/\s/g, '').split(',') : [];
+      },
+
+      price: function () {
+        return $(this.fieldMap.price+':checked').val() || null;
+      },
+
       meal_type: function () {
         var types = [];
         this.$el.find(this.fieldMap.meal_type).each(function (i, el) {
@@ -42,14 +51,77 @@ define(function (require, exports, module) {
           }
         });
         return tags;
+      },
+
+      amenities: function () {
+        var amenities = [];
+        this.$el.find(this.fieldMap.amenities).each(function (i, el) {
+          if ( $(el).is(':checked') ) {
+            amenities.push({ amenities: el.value });
+          }
+        });
+        return amenities;
+      },
+
+      display_phone: function () {
+        return this.$el.find(this.fieldMap.display_phone).val().replace(/[^\d]/g, '') || null;
+      },
+
+      contacts: function () {
+        return utils.map(this.$el.find(this.fieldMap.contacts), function (el) {
+          var $el = $(el);
+          var phoneNumber = $el.find('.contact-phone').val();
+          return {
+            name: $el.find('.contact-name').val()
+          , position: $el.find('.contact-position').val()
+          , sms_phones: [phoneNumber]
+          , voice_phones: [phoneNumber]
+          , emails: [$el.find('.contact-email').val()]
+          };
+        });
       }
+
     }
-  , initalize: function () {
+  , initialize: function (options) {
+      BaseView.prototype.initialize.apply(this, options);
       console.log('init restaurant view')
     }
 
   , submit: function (e) {
+      e.preventDefault();
+      this.clearErrors();
+      var fields = {
+          address: this.$el.find(this.fieldMap.address).val()
+        , phone: this.fieldGetters.display_phone.call(this)
+        , orderMinimum: this.$el.find(this.fieldMap.minimum_order).val()
+        , contacts: this.fieldGetters.contacts.call(this)
+      };
 
+      if (!fields.address) {
+        return this.displayErrors([{
+          property: 'address'
+        , message: 'Please provide a valid address.'
+        }]);
+      }
+
+      if (fields.phone === null || fields.phone.length < 10) {
+        return this.displayErrors([{
+          property: 'display_phone'
+        , message: 'Please provide a valid phone number.'
+        }]);
+      }
+
+      if (!fields.orderMinimum || !parseInt(fields.orderMinimum)) {
+        return this.displayErrors([{
+          property: 'minimum_order'
+        , message: 'Please provide a valid order minimum.'
+        }]);
+      }
+
+      //todo: validate contacts. check first contact group
+      // if an additional contact is empty ingore it
+
+      console.log('diff ', this.getDiff());
     }
 
   , addContact: function (e) {

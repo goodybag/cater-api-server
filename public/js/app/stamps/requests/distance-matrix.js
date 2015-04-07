4e862475f2784705f45dc4698614e4e10abbd8b2
John Fawcett <jrf0110@gmail.com>
2015-04-07 11:26:27 -0500

    arbitrary origins and destinations, override send

--- public/js/app/stamps/requests/distance-matrix.js
+++ public/js/app/stamps/requests/distance-matrix.js
@@ -22,12 +22,42 @@ define( function( require, exports, module ){
       this.query( 'key', config.google.apiKey );
     })
     .methods({
-      origin: function( originAddress ){
+      origins:      []
+    , destinations: []
 
+    , origin: function( origin ){
+        this.origins.push( origin );
+        return this;
       }
 
-    , destination: function( destinationAddress ){
+    , destination: function( destination ){
+        this.destinations.push( origin );
+        return this;
+      }
 
+    , getOriginDestinationQuery: function(){
+        return {
+          origin:       this.origins.join('|')
+        , destination:  this.destinations.join('|')
+        };
+      }
+
+    , send: function(){
+        this.query( this.getOriginDestinationQuery() );
+
+        return base.fixed.methods.send.call( this )
+          .then( function( res ){
+            return GeoRes.create({
+              res: res
+            , requestAddress: this.req.qs.address
+            });
+          }.bind( this ))
+          .then( function( geores ){
+            if ( callback ) return callback( null, geores );
+          }.bind( this ))
+          .error( function( error ){
+            if ( callback ) callback( error );
+          }.bind( this ));
       }
     });
 });

659890591cd7e38ad1bc0d69b88d5fa152a4220e
Preston Pham <prestonp08@gmail.com>
2015-04-16 16:21:18 -0500

    add staging stripe env

--- configs/stripe.js
+++ configs/stripe.js
@@ -9,6 +9,11 @@ var stripe = {
   , public: 'pk_test_9N1srz1fVmWGyRAAhmzAA7aP'
   }
 
+  staging: {
+    secret: 'sk_test_XksbwDuLK1xbpOFaSVLVf9qZ'
+  , public: 'pk_test_9N1srz1fVmWGyRAAhmzAA7aP'
+  }
+
 , production: {
     secret: 'sk_live_kjAFUHK2tYc6BRHRyeMlZPON'
   , public: 'pk_live_kShOkv0NSx9Lj1qWGqxAXotN'

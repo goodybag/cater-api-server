cdc6523727f4118be871497eb64e790aaa436989
paulserraino <pserraino99@gmail.com>
2015-04-28 16:11:33 -0500

    request to cater view

---
+++ public/js/app/views/request-to-cater-view.js
@@ -0,0 +1,51 @@
+define(function (require, exports, module) {
+  var Backbone  = require('backbone');
+
+  return module.exports = Backbone.View.extend({
+    events: {
+      'submit': 'submit'
+    }
+
+  , fieldMap: {
+      contact_name: '#request-form-full-name'
+    , contact_email: '#request-form-email'
+    , contact_phone: '#request-form-phone-number'
+    , restaurant_name: '#request-form-restaurant-name'
+    }
+
+  , fieldGetter: {
+      contact_phone: function () {
+        return this.$el.find(this.fieldMap.contact_phone).val().replace(/[^\d]/g, '') || null;
+      }  
+    }
+
+  , initialize: function () {
+    }
+
+  , submit: function (e) {
+      e.preventDefault();
+      var fields = this.getFieldValues();
+
+      $.ajax({
+        type: 'POST'
+      , url: '/request-to-be-a-caterer'
+      , data: fields
+      , success: function () {
+          window.location.reload();
+        }
+      , error: function () {
+          console.error('failed to submit form');
+        }
+      });
+    }
+
+  , getFieldValues: function () {
+      var k, r = {};
+      for (k in this.fieldMap) {
+        r[k] = k in this.fieldGetter ? 
+          this.fieldGetter[k].call(this) : this.$el.find( this.fieldMap[k] ).val();
+      }
+      return r;
+    }
+  });
+});

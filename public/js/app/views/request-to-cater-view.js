8bd3ea1eba1426e3c33977a8c6c9fdcaffdc71d7
paulserraino <pserraino99@gmail.com>
2015-04-29 12:19:18 -0500

    display errors

--- public/js/app/views/request-to-cater-view.js
+++ public/js/app/views/request-to-cater-view.js
@@ -1,5 +1,6 @@
 define(function (require, exports, module) {
   var Backbone  = require('backbone');
+  var Handlebars = require('Handlebars');
 
   return module.exports = Backbone.View.extend({
     events: {
@@ -24,7 +25,12 @@ define(function (require, exports, module) {
 
   , submit: function (e) {
       e.preventDefault();
+      this.clearErrors();
       var fields = this.getFieldValues();
+
+      if (fields.contact_phone.length !== 10) {
+        return this.displayErrors('contact_phone', 'Please provide a valid phone number.');
+      }
 
       $.ajax({
         type: 'POST'
@@ -46,6 +52,29 @@ define(function (require, exports, module) {
           this.fieldGetter[k].call(this) : this.$el.find( this.fieldMap[k] ).val();
       }
       return r;
+    }
+
+  , displayErrors: function (name, message) {
+      var template = Handlebars.partials.alert_error;
+      var $el = $( template( { message: message } ) );
+      var $parent = this.$el.find('[name="' + name + '"]').closest('.form-group');
+
+      $el.css({
+        'position': 'absolute'
+      , 'top': '11px'
+      , 'left': '338px'
+      , 'min-width': '200px'
+      , 'width': '100%'
+      , 'z-index': '1'
+      })
+
+      $parent.prepend( $el );
+      $parent.addClass('has-error');
+    }
+
+  , clearErrors: function() {
+      this.$el.find('.has-error').removeClass('has-error');
+      this.$el.find('.alert').addClass('hide');
     }
   });
 });

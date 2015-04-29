fd5dee2768fe0b8eefb7d3a4d1048e2f4d2f25c7
Preston Pham <prestonp08@gmail.com>
2015-04-29 18:05:40 -0500

    generalize class for other things

--- configs/diets.js
+++ configs/diets.js
@@ -1,10 +1,10 @@
 // Map diet tag enums to display text and icon classes
 
 module.exports = {
-  glutenFree:  { name: 'Gluten-Free', icon: 'diet-tag-gluten-free' }
-, dairyFree:   { name: 'Dairy-Free',  icon: 'diet-tag-dairy-free' }
-, vegetarian:  { name: 'Vegetarian',  icon: 'diet-tag-vegetarian' }
-, vegan:       { name: 'Vegan',       icon: 'diet-tag-vegan' }
-, halal:       { name: 'Halal',       icon: 'diet-tag-halal' }
-, kosher:      { name: 'Kosher',      icon: 'diet-tag-kosher' }
+  glutenFree:  { name: 'Gluten-Free', class: 'gluten-free' }
+, dairyFree:   { name: 'Dairy-Free',  class: 'dairy-free' }
+, vegetarian:  { name: 'Vegetarian',  class: 'vegetarian' }
+, vegan:       { name: 'Vegan',       class: 'vegan' }
+, halal:       { name: 'Halal',       class: 'halal' }
+, kosher:      { name: 'Kosher',      class: 'kosher' }
 };

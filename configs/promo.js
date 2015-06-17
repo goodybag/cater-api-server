1275fad233e843c961f2be3b5de714df3482754c
paulserraino <pserraino99@gmail.com>
2015-06-17 11:02:54 -0500

    add promo codes

--- configs/promo.js
+++ configs/promo.js
@@ -14,6 +14,10 @@ var promo = {
     email: 'jacobparker@goodybag.com'
   , promo_code: ['goodybag315', 'gb415', 'gbaustin15', 'gbhou15', 'gbstar15', 'gbamaz15']
   }
+, toby: {
+    email: 'tobyshields@goodybag.com'
+  , promo_code: ['seattlegb', 'seagoody']
+  }
 , om: {
     email: 'om@goodybag.com'
   , promo_code: [ 'bv250' ]

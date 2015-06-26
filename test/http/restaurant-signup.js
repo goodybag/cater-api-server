4a15ae7a2ae64b8c7a0bd35f0a339109691db43b
paulserraino <pserraino99@gmail.com>
2015-06-26 17:47:57 -0500

    init sign up flow tests

--- test/http/restaurant-signup.js
+++ test/http/restaurant-signup.js
@@ -1,6 +1,7 @@
 process.env['GB_ENV'] = 'dev';
 
 var assert  = require('assert');
+var request = require('request');
 var app     = require('../../app');
 var db      = require('../../db');
 var utils   = require('../../utils');
@@ -52,3 +53,63 @@ describe('Restaurant Signup', function () {
   });
 
 });
+
+/**
+* Signup Flow
+*/
+
+describe('Signup Flow', function () {
+
+  var data = {
+    data: {
+      user_name: 'foo',
+      user_number: '0123456789',
+      name: 'foobar',
+      website: 'foobar.com',
+      service: 'delivery'
+    }
+  };
+
+  after(function (done) {
+    if (data.id) {
+      console.log('deleting id: ' + data.id);
+      db.restaurant_signups.remove({ id: data.id }, done);
+    }
+  });
+
+  it.only('increment step', function (done) {
+
+
+    var jar = request.jar();
+
+    var options = {
+      jar: jar
+    };
+
+    var URI = config.baseUrl + '/api/restaurants/join';
+
+    utils.post(URI, data, options, function (error, res, body) {
+      assert(!error, error);
+
+      data.id = body.id;
+
+      utils.put(
+        URI
+      , data
+      , options
+      , function (error, res, body) {
+          assert(!error, error);
+          assert.equal(res.statusCode, 200);
+
+          assert.equal(res.request.uri.pathname, '/api/restaurants/join');
+
+          done();
+      });
+
+    });
+
+
+  });
+
+});
+

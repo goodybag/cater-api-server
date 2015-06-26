2d29889696f890f5da36ef78443a4119d78e1b7a
paulserraino <pserraino99@gmail.com>
2015-06-26 12:14:19 -0500

    better tests

--- test/http/restaurant-signup.js
+++ test/http/restaurant-signup.js
@@ -8,6 +8,23 @@ var config  = require('../../config');
 
 describe('Restaurant Signup', function () {
 
+  var data = {
+    data: {
+      user_name: 'foo',
+      user_number: '0123456789',
+      name: 'foobar',
+      website: 'foobar.com',
+      service: 'delivery'
+    }
+  };
+
+  after(function (done) {
+    if (data.id) {
+      console.log('deleting id: ' + data.id);
+      db.restaurant_signups.remove({ id: data.id }, done);
+    }
+  });
+
   it('GET /restaurants/join', function (done) {
     utils.test.get('/restaurants/join', function (error, res, body) {
       assert(!error, error);
@@ -19,20 +36,17 @@ describe('Restaurant Signup', function () {
 
   it('POST /api/restaurants/join', function (done) {
 
-    var data = {
-      data: {
-        user_name: 'foo',
-        user_number: '0123456789',
-        name: 'foobar',
-        website: 'foobar.com',
-        service: 'delivery'
-      }
-    };
-
     utils.test.post('/api/restaurants/join', data, function (error, res, body) {
       assert(!error, error);
       assert.equal(res.statusCode, 200);
       assert.equal(res.request.uri.pathname, '/api/restaurants/join');
+
+      body = JSON.parse(body);
+      assert.equal(body.step, 1);
+      assert.equal(body.status, 'pending');
+
+      data.id = body.id;
+
       done();
     });
   });

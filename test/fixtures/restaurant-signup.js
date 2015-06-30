a9b41a3ff06fab84f462b4a286d543a90652faa4
paulserraino <pserraino99@gmail.com>
2015-06-30 12:53:37 -0500

    signup test fixtures

---
+++ test/fixtures/restaurant-signup.js
@@ -0,0 +1,44 @@
+module.exports = {
+  restaurant: {
+    id: null
+  , created_at: new Date()
+  , status: 'pending'
+  , step: 1
+  , data: {
+      billing_city: 'Austin'
+    , billing_name: 'Test'
+    , billing_phone: 1234567890
+    , billing_state: 'TX'
+    , billing_zip: '78758'
+    , city: 'Austin'
+    , contacts: [{
+        emails: ['foo@test.com']
+      , name: 'foobar'
+      , sms_phones: [1234567890]
+      , voice_phones: [1234567890]
+      }]
+    , cuisine: ['Thai']
+    , delivery_fee: '12'
+    , delivery_hours: {}
+    , delivery_service_zips: []
+    , delivery_times: {}
+    , delivery_zips: []
+    , display_phone: '1234567890'
+    , gb_fee: 0.1234
+    , lead_times: []
+    , logo_url: ''
+    , meal_types: []
+    , menu_url: ''
+    , minimum_order: '12'
+    , name: 'TEST'
+    , pickup_lead_times: []
+    , price: '2'
+    , services: 'delivery'
+    , state: 'TX'
+    , street: '123 four st'
+    , tags: []
+    , websites: '{}'
+    , zip: '78758'
+    }
+  }
+};

f4ed37842dd6a1a979efcf180808149418b9eebb
John Fawcett <jrf0110@gmail.com>
2015-07-10 10:56:14 -0500

    finish up view logic for audid-views

--- public/js/app/views/admin/audit-view.js
+++ public/js/app/views/admin/audit-view.js
@@ -10,19 +10,53 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 
 define( function( require, exports, module ){
   var utils = require('utils');
+  var FormView = require('../form-view-2');
 
-  return utils.View.extend({
+  return FormView.extend({
     events: {
-      'click [role="create"]': 'onCreateClick'
-    , 'click [role="remove"]': 'onRemoveClick'
+      'submit [role="create-form"]': 'onCreateSubmit'
+    , 'click [role="remove"]':      'onRemoveClick'
     }
 
-  , onCreateClick: function( e ){
+  , initialize: function( options ){
+      if ( !options.collection ){
+        throw new Error('Must provide an instance of Collection');
+      }
 
+      if ( !options.template ){
+        throw new Error('Must provide a template');
+      }
+
+      this.collection = options.collection;
+      this.template   = options.template;
+
+      this.collection.on( 'sync', this.render.bind( this ) );
+      this.collection.on( 'destroy', this.render.bind( this ) );
+    }
+
+  , render: function(){
+      var html = this.template({
+        items: this.collection.sort().toJSON()
+      });
+
+      this.$el.html( html );
+
+      return this;
+    }
+
+  , onCreateSubmit: function( e ){
+      e.preventDefault();
+
+      var model = this.collection.create( this.getModelData() );
+
+      if ( model.validationError ){
+        return this.displayErrors( model.validationError );
+      }
     }
 
   , onRemoveClick: function( e ){
-      
+      var model = this.collection.get( +$(e.currentTarget).data('id') );
+      model.destroy();
     }
   });
 });

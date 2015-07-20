2c5b752a91140925c0b4a33bddd14fabf63fda9e
John Fawcett <jrf0110@gmail.com>
2015-07-20 11:21:53 -0500

    Dropoff api creds/config

---
+++ configs/dropoff.js
@@ -0,0 +1,10 @@
+module.exports = {
+  privateKey: '740e66a9e5f0c18f5439e72497801b659b77820a69a3900ed16aff4adc68a52f'
+, publicKey:  'user::87cb7e7863c4e7c04026ff2b066462bc89d8d27cc41b443791d652dbab3d7727'
+, apiUrl:     'https://qa-brawndo.dropoff.com/v1'
+, host:       'qa-brawndo.dropoff.com'
+};
+
+if ( process.env.GB_ENV !== 'production' ){
+  // TODO: Use production keys
+}

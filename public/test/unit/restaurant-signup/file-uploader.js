9efa88028e72e23af63a924889d4b63a3e4b0b8a
paulserraino <pserraino99@gmail.com>
2015-07-27 15:18:22 -0500

    file uploader tests

--- public/test/unit/restaurant-signup/file-uploader.js
+++ public/test/unit/restaurant-signup/file-uploader.js
@@ -4,13 +4,42 @@ define(function(require, exports, module) {
 
   describe('File Uploader Component', function () {
 
-    it('should exists', function () {
-      var uploader = React.createElement(FileUploader, null);
+    it('.val() should return correct value', function () {
+      var props = { value: 'foo' };
+      var uploader = React.createElement(FileUploader, props);
       var component = React.render(uploader, document.createElement('div'));
 
-      console.log(component)
-      expect(component).not.toBeNull();
+      expect(component.val()).toEqual(props.value);
     });
+
+    it('should set errorMessage', function () {
+      var props = {
+        errorMessage: 'please enter foobar'
+      };
+
+      var uploader = React.createElement(FileUploader, props);
+      var component = React.render(uploader, document.createElement('div'));
+      var errorText = component.getDOMNode()
+        .querySelectorAll('.error')[0].innerText;
+
+      expect(errorText).toEqual(props.errorMessage);
+    });
+
+    it('should set label', function () {
+      var props = {
+        label: 'foobar'
+      };
+
+      var uploader = React.createElement(FileUploader, props);
+      var component = React.render(uploader, document.createElement('div'));
+
+      // grab the first label from the render method
+      var renderedLabel = component.getDOMNode()
+        .querySelectorAll('label')[0];
+
+      expect(renderedLabel.innerText).toEqual(props.label);
+    });
+
 
   });
 });

bb0f91903f2e228f1c8e95d1f551171c9a858aed
John Fawcett <jrf0110@gmail.com>
2015-07-30 13:52:04 -0500

    fix typo, add column for getting notification id

--- db/deltas/1.2.99.sql
+++ db/deltas/1.2.99.sql
@@ -8,5 +8,9 @@ begin
   -- Update version
   execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
 
-  perform add_column( 'delivery_servies', 'order_submitted_notification_id', 'text' );
+  perform add_column( 'delivery_services', 'order_submitted_notification_id', 'text' );
+
+  update delivery_services
+    set order_submitted_notification_id = 'dropoff-order-submitted'
+    where name = 'Dropoff';
 end$$;

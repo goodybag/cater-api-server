62592b9c13cba8ac01eb3fd81c761cebd70f1cf4
paulserraino <pserraino99@gmail.com>
2015-07-31 16:28:15 -0500

    order item price synchronizer

--- workers/orders/synchronizer/item-price.js
+++ workers/orders/synchronizer/item-price.js
@@ -1,50 +1,74 @@
-var db = require('../../../db');
+/*
+* item-price.js
+* syncs order_item and item prices
+*
+*/
 
-var query = {
-  type: 'select'
-, table: 'orders_items'
-, columns: [
-  '*'
-  , { name: 'items.price', alias: 'item_price' }
-  ]
-, with: {
-    orders_items: {
-      type: 'select'
-    , table: 'order_items'
-    , columns: [
-        { name: 'orders.id', alias: 'order_id' }
-      , { name: 'order_items.id', alias: 'order_item_id' }
-      , { name: 'order_items.item_id', alias: 'item_id' }
-      , { name: 'order_items.price', alias: 'price' }
-      , { name: 'order_items.name', alias: 'name' }
-      ]
-    , joins: [
-        {
-          type: 'inner'
-        , target: 'orders'
-        , on: { id: '$order_items.order_id$' }
+var db = require('../../../db');
+var logger  = require('../../../lib/logger').create('Synchronize-Item-Price');
+var utils = require('../../../utils');
+
+var getQuery = function () {
+  var query = {
+    type: 'select'
+  , table: 'orders_items'
+  , columns: [
+    '*'
+    , { name: 'items.price', alias: 'item_price' }
+    ]
+  , with: {
+      orders_items: {
+        type: 'select'
+      , table: 'order_items'
+      , columns: [
+          { name: 'orders.id', alias: 'order_id' }
+        , { name: 'order_items.id', alias: 'order_item_id' }
+        , { name: 'order_items.item_id', alias: 'item_id' }
+        , { name: 'order_items.price', alias: 'price' }
+        , { name: 'order_items.name', alias: 'name' }
+        ]
+      , joins: [
+          {
+            type: 'inner'
+          , target: 'orders'
+          , on: { id: '$order_items.order_id$' }
+          }
+        ]
+      , where: {
+          'orders.status': 'pending'
+        , $gte: { 'orders.datetime': 'now()' }
         }
-      ]
-    , where: {
-        'orders.status': 'pending'
-      , $gte: { 'orders.datetime': 'now()' }
       }
     }
-  }
-
-, joins: [
-    {
-      type: 'left'
-    , target: 'items'
-    , on: { id: '$orders_items.item_id$' }
+  , joins: [
+      {
+        type: 'left'
+      , target: 'items'
+      , on: { id: '$orders_items.item_id$' }
+      }
+    ]
+  , where: {
+      $ne: { price: '$items.price$' }
     }
-  ]
-, where: {
-    $ne: { price: '$items.price$' }
-  }
+  };
+
+  return query;
 };
 
-db.query2(query, function (error, items) {
-  if (error) return console.log(error);
-  console.log(items);
-});
+module.exports = function (callback) {
+  return db.query2(getQuery(), function (error, items) {
+    if (error) {
+      logger.error('Error fetching item prices', {
+        error: error
+      });
+      return process.exit(1);
+    }
+
+    logger.info('Processing', items.length, 'items');
+    var onItem = function (item, next) {
+      db.order_items.update({ id: item.order_item_id }, { price: item.item_price}, next);
+    };
+
+    return utils.async.each(items, onItem, callback);
+  });
+};

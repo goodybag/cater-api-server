26af6de45af155819da1f976ba5689e02cf72934
John Fawcett <jrf0110@gmail.com>
2015-08-05 11:06:53 -0500

    if mongo logging transport disabled, do not attempt to run server

--- workers/logs/index.js
+++ workers/logs/index.js
@@ -1,6 +1,11 @@
 var server = require('loglog-server');
 var config = require('../../config');
 
+if ( !config.logging.transports.mongo ){
+  console.warn('MongoDB disabled as trasnport. Exiting.');
+  process.exit(0);
+}
+
 server.set( 'source', server.sources.mongodb({
   connection: config.logging.mongoConnStr
 , collection: config.logging.mongoCollection

ac41fafb142c2f455324187459739a0e85a22559
paulserraino <pserraino99@gmail.com>
2015-08-06 15:15:06 -0500

    add pickup lead times to delivery-info component

--- jsx/restaurant-signup/steps/delivery-pickup.jsx
+++ jsx/restaurant-signup/steps/delivery-pickup.jsx
@@ -2,19 +2,28 @@ define(function(require, exports, module) {
   var React = require('react');
   var Input = require('../components/input');
   var LeadTimes = require('../components/lead-times')
+  var requestMixin = require('../mixins/requests');
 
   module.exports = React.createClass({
+    mixins: [requestMixin],
+
     getFields: function () {
-      return []; // TODO: add form fields
+      return ['delivery_fee', 'delivery_times', 'pickup_lead_times'];
     },
 
     render: function () {
       return (
         <div>
           <h2>Delivery Info</h2>
-          <Input label= "Delivery Free" />
-          <h3>Delivery Lead Times</h3>
-          <LeadTimes />
+          <Input label= "Delivery Fee" ref="delivery_fee" />
+          <div>
+            <h3>Delivery Lead Times</h3>
+            <LeadTimes ref="delivery_times" />
+          </div>
+          <div>
+            <h3>Pickup Lead Times</h3>
+            <LeadTimes ref="pickup_lead_times" />
+          </div>
         </div>
       );
     }

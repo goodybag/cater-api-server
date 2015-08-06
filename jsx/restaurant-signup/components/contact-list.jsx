e05905c01be42b4ef494be3dd65b7bfa48ae0313
paulserraino <pserraino99@gmail.com>
2015-08-06 12:44:59 -0500

    wip contact list component

--- jsx/restaurant-signup/components/contact-list.jsx
+++ jsx/restaurant-signup/components/contact-list.jsx
@@ -1,28 +1,35 @@
 define(function(require, exports, module) {
   var React = require('react');
   var Contact = require('./contact');
+  var isValid = require('../mixins/is-valid');
 
   module.exports = React.createClass({
+    mixins: [isValid],
+
     propTypes: {
       contacts: React.PropTypes.array
     },
 
     getInitialState: function () {
-      return { contacts: this.props.contacts || [null] };
+      return { contacts: this.props.contacts || [{}] };
+    },
+
+    val: function () {
+      return this.state.contacts;
     },
 
     addContact: function (e) {
       e.preventDefault();
 
       this.setState({
-        contacts: this.state.contacts.concat(null).reverse()
+        contacts: this.state.contacts.concat({})
       });
     },
 
     render: function () {
-      var contactList = this.state.contacts.map(function (contact) {
-        return <Contact contact={contact} />;
-      });
+      var contactList = this.state.contacts.map(function (contact, i) {
+        return <Contact ref={"contact-"+i} key={i} contact={contact} />;
+      }.bind(this));
 
       return (
         <div>

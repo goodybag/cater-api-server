00becb74c46369c309af0c55e2db47edc82eba9e
paulserraino <pserraino99@gmail.com>
2015-08-06 16:01:35 -0500

    toggle lead times

--- jsx/restaurant-signup/steps/delivery-pickup.jsx
+++ jsx/restaurant-signup/steps/delivery-pickup.jsx
@@ -11,7 +11,36 @@ define(function(require, exports, module) {
       return ['delivery_fee', 'delivery_times', 'pickup_lead_times'];
     },
 
+    toggleLeadTimes: function (ref, e) {
+      var el = this.refs[ref].getDOMNode();
+
+      // TODO: replace classList with a more IE friendly method
+      if (e.target.value === "custom") {
+        return el.classList.remove('hide');
+      }
+      return el.classList.add('hide');
+    },
+
     render: function () {
+      var leadTimesToggler = function (ref) {
+        return (
+          <div>
+            <div>
+             <input type="radio" onChange={this.toggleLeadTimes.bind(this, ref)} id={"default-"+ref} name={"name-"+ref} value="default" />
+             <label htmlFor={"default-"+ref}>
+              Use default lead times
+             </label>
+            </div>
+            <div>
+              <input type="radio" onChange={this.toggleLeadTimes.bind(this, ref)} id={"custom-"+ref} name={"name-"+ref} value="custom" />
+              <label htmlFor={"custom-"+ref}>
+              Customize my lead times
+              </label>
+            </div>
+          </div>
+          );
+      }.bind(this);
+
       return (
         <div>
           <h2>Delivery Info</h2>
@@ -23,8 +52,13 @@ define(function(require, exports, module) {
             restaurants. The smaller the lead time, the more orders you can
             receive. <strong>Cancel Time</strong> is the amount of notice
             needed if someone wants to cancel their order with you.</p>
-            <LeadTimes ref="delivery_times" />
+
+            {leadTimesToggler("delivery_times_container")}
+            <div ref="delivery_times_container" className="hide">
+              <LeadTimes ref="delivery_times" />
+            </div>
           </div>
+
           <div>
             <h3>Pickup Lead Times</h3>
             <p>The time you need to prepare an order that is being picked
@@ -32,7 +66,11 @@ define(function(require, exports, module) {
             for our restaurants. The smaller the lead time, the more orders
             you can receive. <strong>Cancel Time</strong> is the amount of
             notice needed if someone wants to cancel their order with you.</p>
-            <LeadTimes ref="pickup_lead_times" />
+
+            {leadTimesToggler("pickup_lead_times_container")}
+            <div ref="pickup_lead_times_container" className="hide">
+              <LeadTimes ref="pickup_lead_times" />
+            </div>
           </div>
         </div>
       );

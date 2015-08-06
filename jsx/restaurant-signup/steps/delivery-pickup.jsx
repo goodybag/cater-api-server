cd7966dea541bbec40cf2d557d8d96c97a8e8ddc
paulserraino <pserraino99@gmail.com>
2015-08-06 15:24:34 -0500

    lead times descriptions

--- jsx/restaurant-signup/steps/delivery-pickup.jsx
+++ jsx/restaurant-signup/steps/delivery-pickup.jsx
@@ -18,10 +18,20 @@ define(function(require, exports, module) {
           <Input label= "Delivery Fee" ref="delivery_fee" />
           <div>
             <h3>Delivery Lead Times</h3>
+            <p>The time you need to prepare and deliver an order for a given
+            number of guests. Below are the lead times we recommend for our
+            restaurants. The smaller the lead time, the more orders you can
+            receive. <strong>Cancel Time</strong> is the amount of notice
+            needed if someone wants to cancel their order with you.</p>
             <LeadTimes ref="delivery_times" />
           </div>
           <div>
             <h3>Pickup Lead Times</h3>
+            <p>The time you need to prepare an order that is being picked
+            up by an outside party. Below are the lead times we recommend
+            for our restaurants. The smaller the lead time, the more orders
+            you can receive. <strong>Cancel Time</strong> is the amount of
+            notice needed if someone wants to cancel their order with you.</p>
             <LeadTimes ref="pickup_lead_times" />
           </div>
         </div>

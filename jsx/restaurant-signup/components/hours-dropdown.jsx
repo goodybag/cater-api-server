3133c89a46c44cef642746b5414f3c06906edc1c
paulserraino <pserraino99@gmail.com>
2015-08-07 11:51:55 -0500

    wip hours dropdown component

--- jsx/restaurant-signup/components/hours-dropdown.jsx
+++ jsx/restaurant-signup/components/hours-dropdown.jsx
@@ -2,6 +2,7 @@ define(function(require, exports, module) {
   var React = require('react');
   var utils = require('utils');
   var pickatime = require('pickatime');
+  var isValid = require('../mixins/is-valid');
 
   var daysOfWeek = [
     'Sunday'
@@ -14,28 +15,49 @@ define(function(require, exports, module) {
   ];
 
   module.exports = React.createClass({
-    mixins: [requestMixin],
+    mixins: [isValid],
 
-    componentDidMount: function () {
-      $(this.refs.hoursFrom.getDOMNode()).pickatime();
-      $(this.refs.hoursTo.getDOMNode()).pickatime();
+    getInitialState: function () {
+      return {
+        day: null
+      , from : null
+      , to: null
+      };
     },
 
-    getFields: function () {
-      return []; // TODO: add fields
+    componentDidMount: function () {
+      $(this.refs.hoursFrom.getDOMNode()).pickatime({ onSet: this.setHours.bind(this, 'from')});
+      $(this.refs.hoursTo.getDOMNode()).pickatime({ onSet: this.setHours.bind(this, 'to')});
+    },
+
+    val: function () {
+      return this.state;
+    },
+
+    setDay: function (e) {
+      this.setState({
+        day: e.target.value
+      });
+    },
+
+    setHours: function (name, ctx) {
+      this.setState(function (state) {
+        state[name] = ctx.select;
+      });
     },
 
     render: function () {
+
       var selectDays = (function () {
         return (
-            <select ref="day">
+            <select ref="day" name="day" onChange={this.setDay} >
               <option key={-1} value=''>Choose Day</option>
               {daysOfWeek.map(function (day, i) {
                 return (<option key={i} value={i}>{day}</option>);
-              })}
+              }.bind(this))}
             </select>
           );
-      })();
+      }.bind(this))();
 
       return (
         <div>
@@ -43,10 +65,10 @@ define(function(require, exports, module) {
             {selectDays}
           </div>
           <div>
-            <input type="text"ref="hoursFrom" />
+            <input type="text"ref="hoursFrom" name="form" />
           </div>
           <div>
-            <input type="text" ref="hoursTo" />
+            <input type="text" ref="hoursTo" name="to" />
           </div>
 
         </div>

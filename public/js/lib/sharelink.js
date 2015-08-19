2edde7b430b8e8b24ea4647c82de6fa5d782c242
paulserraino <pserraino99@gmail.com>
2015-08-19 16:14:45 -0500

    load script on dom ready

--- public/js/lib/sharelink.js
+++ public/js/lib/sharelink.js
@@ -63,10 +63,15 @@
     }
   };
 
-  var init = function () {
-    embeddedOrderIcon();
+  var DOMReady = function(a,b,c){
+    b=document,
+    c='addEventListener'
+    ;
+    b[c] ? b[c] ('DOMContentLoaded',a) : window.attachEvent('onload',a)
   };
 
-  init();
+  DOMReady(function () {
+    embeddedOrderIcon();
+  });
 
 })(window, document);

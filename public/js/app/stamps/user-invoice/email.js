ef49528146dd42b7830ad2cecd9cb4aa2e2b727c
m-amaya <marissa.amaya.311@gmail.com>
2015-08-24 11:22:30 -0500

    Changed sendEmail to include the ability to send invoice to custom email

--- public/js/app/stamps/user-invoice/email.js
+++ public/js/app/stamps/user-invoice/email.js
@@ -8,6 +8,7 @@ module.exports = require('stampit')()
   })
   .methods({
     sendEmail: function( callback ){
+
       // No user record, go ahead and run a fetch
       if ( !this.user ){
         return this.fetch( function( error ){
@@ -17,11 +18,14 @@ module.exports = require('stampit')()
         }.bind( this ));
       }
 
+      // Set email to passed in email (if exists) or default email on user
+      var email = this.email || this.user.email;
+
       pdfs.invoice.get( { id: this.id }, function( error, res ){
         if ( error ) return callback( error );
 
         utils.sendMail2({
-          to:       this.user.email
+          to:       email
         , from:     config.invoice.fromMail
         , subject:  'Goodybag Invoice #' + this.id
 

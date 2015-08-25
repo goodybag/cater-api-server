78ac6ffc15cb8578ffc4a94d1c740a0c5760abee
John Fawcett <jrf0110@gmail.com>
2015-08-25 15:07:35 -0500

    seperate saving into async functions

--- public/js/app/views/order-address-view.js
+++ public/js/app/views/order-address-view.js
@@ -59,6 +59,7 @@ define(function(require, exports, module) {
     saveAddress: function(e) {
       var data = new FormView2({ el: this.$el }).getModelData();
 
+      // Do not send blank values
       data = Object
         .keys( data )
         .reduce( function( obj, key ){
@@ -67,24 +68,55 @@ define(function(require, exports, module) {
           return obj;
         }, {} );
 
-      var sent = this.order.address.save( data, {
-        success: function(){
-          var sent = this.options.orderView.model.save(data, {
-            success: _.bind(this.render, this),
-            error: function(jqXHR, textstatus, errorThrown) {
-              return this.options.orderView.model.trigger('change:invalid_address', textstatus, errorThrown);
-            }.bind(this)
-          });
+      var saveAddress = function( done ){
+        var sent = this.order.address.save( data, {
+          success: function(){
+            return done();
+          }
 
-          if (!sent) this.options.orderView.displayErrors();
-        }.bind( this )
+        , error: function( jqXHR, textstatus, errorThrown ){
+            return done( errorThrown );
+          }
+        });
 
-      , error: function( jqXHR, textstatus, errorThrown ){
-          return this.options.orderView.model.trigger('change:invalid_address', textstatus, errorThrown);
-        }.bind(this)
-      });
+        if ( !sent ){
+          this.order.validationError = this.order.address.validationError;
+          return this.options.orderView.displayErrors();
+        }
+      }.bind( this );
 
-      if (!sent) this.options.orderView.displayErrors();
+      var saveOrder = function( done ){
+        var orderData = utils.clone( data );
+        orderData.address_name = orderData.name;
+        delete orderData.name;
+
+        var sent = this.order.save( data, {
+          success: function(){
+            return done();
+          }
+
+        , error: function( jqXHR, textstatus, errorThrown ){
+            return done( errorThrown );
+          }
+        });
+
+        if (!sent) this.options.orderView.displayErrors();
+      }.bind( this );
+
+      utils.async.series([
+        // This state is the worst, but since we do not (for some reason)
+        // track user_address_id on the orders, we cannot reliably figure out
+        // which address object to save, unless we're adding a new one.
+        // If they clicked `edit`, ONLY save the order
+        this.previousModel ? saveAddress : function( done ){ return done(); }
+      , saveOrder
+      ], function( error ){
+        if ( error ){
+          return this.options.orderView.model.trigger('change:invalid_address', error);
+        }
+
+        this.render();
+      }.bind( this ));
     }
 
   , onCancelClick: function( e ){

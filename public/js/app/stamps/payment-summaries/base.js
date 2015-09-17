6e16106fd5fd29897294fc9a9856e200e3e8bd14
John Fawcett <jrf0110@gmail.com>
2015-09-17 09:30:57 -0500

    Get total payout for summary

--- public/js/app/stamps/payment-summaries/base.js
+++ public/js/app/stamps/payment-summaries/base.js
@@ -1,1 +1,25 @@
-module.exports = require('stampit')();
+if ( typeof module === 'object' && module && typeof module.exports === 'object' ){
+  var isNode = true, define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+define( function( require, exports, module ){
+  var PMSItem = require('../orders/payment-summary-item');
+  var utils   = require('utils');
+
+  return module.exports = require('stampit')()
+    .state({
+      orders: []
+    })
+    .methods({
+      getTotalPayout: function(){
+        return this.orders
+          .map( PMSItem )
+          .map( function( order ){
+            return order.getRestaurantCut();
+          })
+          .reduce( utils.add, 0 );
+      }
+    });
+});

e6199db6b07a8b6271f5a1ab5e39f8d9952eb8e0
John Fawcett <jrf0110@gmail.com>
2015-09-21 17:23:05 -0500

    WIP make email stamp specific to pms

--- public/js/app/stamps/payment-summaries/email.js
+++ public/js/app/stamps/payment-summaries/email.js
@@ -1,3 +1,4 @@
+var db      = require('db');
 var utils   = require('utils');
 var config  = require('config');
 var pdfs    = require('../../../../../lib/pdfs');
@@ -9,58 +10,49 @@ module.exports = require('stampit')()
   })
   .methods({
     sendEmail: function( callback ){
-
-      // No user record, go ahead and run a fetch
-      if ( !this.user ){
-        return this.fetch( function( error ){
-          if ( error ) return callback( error );
-
-          this.sendEmail( callback );
-        }.bind( this ));
-      }
-
-      // Set email to passed in email (if exists) or default email on user
-      var email = this.email || this.user.email;
-
-      pdfs.invoice.get( { id: this.id }, function( error, res ){
+      this.fetchRecipients( function( error, recipients ){
         if ( error ) return callback( error );
 
-        utils.sendMail2({
-          to:       email
-        , from:     config.invoice.fromMail
-        , subject:  'Goodybag Invoice #' + this.id
-
-        , body: [ 'Hi'
-                , '\n\n'
-                , 'Attached is invoice #:id for billing period :billing_period '
-                , 'for orders placed through Goodybag.com.'
-                , '\n\n'
-                , 'Checks should be made out to: :legal_name\n'
-                , 'Mailed to: :legal_address'
-                , '\n\n'
-                , 'This is an automated message, but feel free to reply to this '
-                , 'email if you have any questions.'
-                , '\n\n'
-                , 'Thanks a bunch!'
-                , '\n\n'
-                , '-Goodybot'
-                ].join('')
-                .replace( ':id', this.id )
-                .replace( ':billing_period', this.getBillingPeriodFormatted() )
-                .replace( ':legal_name', config.legal.name )
-                .replace( ':legal_address', Address( config.legal.mailingAddress ).toString() )
-
-        , attachment: {
-            fileName:     config.invoice.fileFormat.replace( ':id', this.id )
-          , streamSource: res
-          }
-        }, function( error ){
+        pdfs.pms.get( { id: this.id }, function( error, res ){
           if ( error ) return callback( error );
 
-          this.status = 'emailed';
-          this.email_sent_date = new Date();
+          utils.sendMail2({
+            to:       recipients
+          , from:     config.paymentSummaries.fromMail
+          , subject:  'Goodybag Payment Summary #' + this.id
 
-          this.save( callback );
+          , body: [ 'Hi'
+                  , '\n\n'
+                  , 'Attached is invoice #:id for billing period :billing_period '
+                  , 'for orders placed through Goodybag.com.'
+                  , '\n\n'
+                  , 'Checks should be made out to: :legal_name\n'
+                  , 'Mailed to: :legal_address'
+                  , '\n\n'
+                  , 'This is an automated message, but feel free to reply to this '
+                  , 'email if you have any questions.'
+                  , '\n\n'
+                  , 'Thanks a bunch!'
+                  , '\n\n'
+                  , '-Goodybot'
+                  ].join('')
+                  .replace( ':id', this.id )
+                  .replace( ':billing_period', this.getBillingPeriodFormatted() )
+                  .replace( ':legal_name', config.legal.name )
+                  .replace( ':legal_address', Address( config.legal.mailingAddress ).toString() )
+
+          , attachment: {
+              fileName:     config.paymentSummaries.fileName.replace( ':psdid', this.id )
+            , streamSource: res
+            }
+          }, function( error ){
+            if ( error ) return callback( error );
+
+            this.status = 'emailed';
+            this.email_sent_date = new Date();
+
+            this.save( callback );
+          }.bind( this ));
         }.bind( this ));
       }.bind( this ));
 

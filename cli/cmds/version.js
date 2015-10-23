eda9b30f3f87528a0dfeaebc1a51457881ca1358
m-amaya <marissa.amaya.311@gmail.com>
2015-10-22 21:30:56 -0500

    Updated version cmd

--- cli/cmds/version.js
+++ cli/cmds/version.js
@@ -1,9 +1,22 @@
 var format = require('../utils').format;
+var strUtil = require('../utils').strings;
+var name = require('../config').name;
+var description = require('../config').description;
 var version = require('../config').version;
+
+var chalk = require('chalk');
 
 module.exports = function(context) {
   // Command: version
   // Print out version of app
-  console.log(format.consolify("v" + version));
+  var output =
+    strUtil.concat([
+      "v",
+      version
+    ]);
+
+  output = format.wrapify(output);
+
+  console.log(format.consolify(output));
   context.end();
 };

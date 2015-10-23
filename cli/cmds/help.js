07477430100ece692ffdd404f2c0db3d7a7658bc
m-amaya <marissa.amaya.311@gmail.com>
2015-10-22 20:24:58 -0500

    Updated 'help' functionality

--- cli/cmds/help.js
+++ cli/cmds/help.js
@@ -1,18 +1,102 @@
 var utils = require('../utils');
-var config = require('../config');
+var config = require('../config.json');
+var cmdObj = config.commands;
 
-var cmdList = config.commands;
 var format = utils.format;
-var obj = utils.objects;
+var strUtil = utils.strings;
+var cmdUtil = utils.commands;
+
+var chalk = require('chalk');
+var wrap = require('word-wrap');
 
 module.exports = function(context) {
-  var keys = obj.getKeys(cmdList[0]);
+  var args = context.args;
 
-  var tabled = format.tablify(cmdList, {
-    cols: [ 'Command', 'Alias', 'Description' ],
-    keys: keys
-  });
+  // Command: help
+  // Print full 'help' table if no 'args' have been passed.
+  if(!args.length) {
+    var helpTable = getHelpTable();
+    console.log(format.consolify(helpTable));
 
-  console.log(format.consolify(tabled));
+  // Command: help <[COMMAND] ...>
+  // Print help information specific to the args passed
+  } else {
+    args.forEach(function(arg) {                            // for each arg
+
+      if(cmdUtil.isValidCommand(arg)) {                   // check if valid
+        var usageInfo = getUsageInfo(arg);
+        console.log(format.consolify(usageInfo));       // print usage info
+      }
+    });
+
+  }
   context.end();
 };
+
+function getUsageInfo(arg) {
+  var command = cmdUtil.getCommandOf(arg) || arg;
+
+  var label =
+    strUtil.concat([
+      chalk.blue("Command: "),
+      chalk.green(cmdUtil.getValueOf(command, "usage"))
+    ]);
+
+  var description =
+    strUtil.concat([
+      cmdUtil.getValueOf(command, "help")
+    ]);
+
+  var wrapDescription = format.wrapify(description);
+
+  var output =
+    strUtil.concat([
+      label,
+      "\n\n",
+      wrapDescription
+    ]);
+
+  var hasFlags = cmdUtil.hasFlags(command);
+  if(hasFlags) {
+    var flagObj = cmdUtil.getValueOf(command, "flags");
+    var flags = chalk.inverse("Flags:");
+    var cols = format.columnify(flagObj, [" ", "  "]);
+    var wrapCols = format.wrapify(cols);
+
+    output +=
+      strUtil.concat([
+        "\n\n",
+        flags,
+        "\n",
+        cols
+      ]);
+  }
+
+  return output;
+}
+
+function getHelpTable() {
+  var tabled =                                         // get table string
+  format.tablify(cmdObj, {
+    colHeaders: ['Command', 'Alias', 'Description'],
+    keysToShow: ['alias', 'description']
+  });
+
+  var footer =                              // get formatted footer string
+    strUtil.concat([
+      "Type '",
+      chalk.green(cmdObj["help"]["usage"]),
+      "' to view more info about a particular command, or set of commands."
+    ]);
+
+  var wrappedFooter = format.wrapify(footer);                // wrap footer
+
+  var output =                                        // consolidate output
+    strUtil.concat([
+      tabled,
+      "\n",
+      wrappedFooter
+    ]);
+
+  return output;
+}
--- cli/cmds/index.js
+++ cli/cmds/index.js
@@ -3,9 +3,15 @@ var shell = require('minish');
 // Shell command functions
 var help = require('./help');
 var version = require('./version');
+var start = require('./start');
+var stop = require('./stop');
+var restart = require('./restart');
 
 // Add commands to shell
-shell.command(["help", "h"], "List of available commands.", help);
+shell.command(["help", "h"], "Table of available commands.", help);
 shell.command(["version", "v"], "Current version.", version);
+shell.command(["start", "st"], "Starts the server.", start);
+shell.command(["stop", "stp"], "Stops the server.", stop);
+shell.command(["restart", "rst"], "Restarts the server, if already running.", restart);
 
 module.exports = shell;

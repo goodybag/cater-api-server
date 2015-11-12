e762c2e4ec64990be4ed79f7b7d604c847e7cab0
John Fawcett <jrf0110@gmail.com>
2015-11-12 15:42:07 -0600

    Route handlers for retrieving restaurant bank account and updating

--- controllers/api/restaurants.js
+++ controllers/api/restaurants.js
@@ -5,6 +5,7 @@
 var stampit = require('stampit');
 var db      = require('../../db');
 var utils   = require('../../utils');
+var errors  = require('../../errors');
 var yelp    = require('yelp');
 var m       = require('../../middleware');
 
@@ -86,14 +87,22 @@ module.exports.autoPopulate = function( req, res ){
   });
 };
 
-module.exports.getBankAccounts = [
+module.exports.getBankAccount = [
   m.getRestaurant({
     param:  'id'
   , stripe: true
   })
 
 , function( req, res ){
-    res.json( req.restaurant.stripe_account.bank_accounts.data[0] || {} );
+    var accounts = req.restaurant.stripe_account.bank_accounts.data;
+
+    if ( accounts.length === 0 ){
+      req.logger.warn('No bank account information found');
+
+      return next( errors.internal.NOT_FOUND );
+    }
+
+    res.json( accounts );
   }
 ];
 
@@ -103,7 +112,68 @@ module.exports.updateBankAccount = [
   , stripe: true
   })
 
+  //
+  // TODO:
+  // Abstract this out into models
+  //
 , function( req, res ){
-    
+    var stripeId = req.restaurant.stripe_id;
+    var accounts = req.restaurant.stripe_account.bank_accounts.data;
+
+    var newAccount = utils.extend({
+      object:   'bank_account'
+    , country:  'US'
+    , currency: 'usd'
+    }, utils.pick( req.body, [
+      'account_number', 'routing_number'
+    ]));
+
+    if ( !newAccount.account_number ){
+      return res.error( errors.input.VALIDATION_FAILED, {
+        message: 'Missing required field `account_number`'
+      });
+    }
+
+    if ( !newAccount.routing_number ){
+      return res.error( errors.input.VALIDATION_FAILED, {
+        message: 'Missing required field `routing_number`'
+      });
+    }
+
+    utils.async.series([
+      // Create the new Default account
+      function( next ){
+        utils.stripe.accounts.createExternalAccount( stripeId, {
+          external_account: newAccount
+        , default_for_currency: true
+        , metadata: req.body.metadata
+        }, next );
+      }
+
+      // Delete the existing accounts
+    , function deleteAccounts( next ){
+        if ( accounts.length === 0 ) return next();
+
+        var account = accounts.pop();
+
+        utils.stripe.accounts.deleteExternalAccount(
+          stripeId
+        , account.id
+        , function( error ){
+            if ( error ){
+              return next( error );
+            }
+
+            return deleteAccounts( next );
+          }
+        );
+      }
+    ], function( error ){
+      if ( error ){
+        return res.error( error );
+      }
+
+      res.send(204);
+    });
   }
 ];

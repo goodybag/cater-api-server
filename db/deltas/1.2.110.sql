abe3ba6e81545e3d1291db1a60316d45c1a2d6fa
John Fawcett <jrf0110@gmail.com>
2015-11-23 13:38:02 -0600

    DB changes to add courier_tracking_id

---
+++ db/deltas/1.2.110.sql
@@ -0,0 +1,12 @@
+-- Delta
+
+DO $$
+  declare version       text := '1.2.110';
+begin
+  raise notice '## Running Delta v% ##', version;
+
+  -- Update version
+  execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
+
+  perform add_column( 'orders', 'courier_tracking_id', 'text' );
+end$$;

ee55d512164c6793febf50a7da875774855d588f
m-amaya <marissa.amaya.311@gmail.com>
2015-12-01 12:27:13 -0600

    Delta to drop NOT NULL constraint from name and copy over initial user id, email, name info into user_invoice_recipients db

---
+++ db/deltas/1.2.110.sql
@@ -0,0 +1,23 @@
+-- Delta to create "user_invoice_recipients" table
+
+DO $$
+  declare version       text := '1.2.110';
+  declare u             users;
+
+begin
+  raise notice '## Running Delta v% ##', version;
+
+  -- Update version
+  execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
+
+  -- Drop NOT NULL constraint from user_invoice_recipients.name
+  alter table user_invoice_recipients alter name drop not null;
+
+  -- Init copy user (id, name, email) to user_invoice_recipients
+  for u in ( select * from users )
+  loop
+    insert into user_invoice_recipients
+      ( user_id, name, email ) values ( u.id, u.name, u.email );
+  end loop;
+
+end$$;

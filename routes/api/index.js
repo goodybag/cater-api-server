4b14d1433a5b97f6df07546f8675a0e1d2a5a76f
m-amaya <marissa.amaya.311@gmail.com>
2015-12-02 14:01:45 -0600

    API doc for /api/delivery_services

--- routes/api/index.js
+++ routes/api/index.js
@@ -26,6 +26,41 @@ route.post('/users/:uid/rewards'
  * Delivery Services
  */
 
+/**
+ * @api {get} /delivery_services Returns a list of all delivery services.
+ * @apiName GetDeliveryServices
+ * @apiGroup Delivery Services
+
+ * @apiSuccess   {Object[]}   delivery_service                 List of delivery services.
+ * @apiSuccess   {Number}     delivery_service.id              Delivery service id.
+ * @apiSuccess   {Number}     delivery_service.region_id       Delivery service region id.
+ * @apiSuccess   {String}     delivery_service.name            Delivery service name.
+ * @apiSuccess   {Number}     delivery_service.rate            Delivery service rating.
+ * @apiSuccess   {String}     delivery_service.created_at      Timestamp when delivery service was created.
+ * @apiSuccess   {String}     delivery_service.order_email     Delivery service order email.
+ * @apiSuccess   {String}     delivery_service.order_phone     Delivery service order phone.
+ * @apiSuccess   {Number}     delivery_service.region_order_distribution           Delivery service region order distribution.
+ * @apiSuccess   {String}     delivery_service.order_submitted_notification_id     Delivery serice order submitted notification id.
+
+ * @apiSuccess   {Object[]}   delivery_service.zips                List of available zipcodes on delivery service.
+ * @apiSuccess   {Number}     delivery_service.zips.id             Unique id for zipcode.
+ * @apiSuccess   {Number}     delivery_service.zips.delivery_service_id     Delivery service id for zipcode.
+ * @apiSuccess   {String}     delivery_service.zips.from           Used to calculate gb delivery fee.
+ * @apiSuccess   {String}     delivery_service.zips.to             Used to calculate gb delivery fee.
+ * @apiSuccess   {Number}     delivery_service.zips.price          Used to calculate gb delivery fee.
+ * @apiSuccess   {String}     delivery_service.zips.created_at     Timestamp when zip was added.
+
+ * @apiSuccess   {Object}     delivery_service.region               Region covered by delivery service.
+ * @apiSuccess   {Number}     delivery_service.region.id            Region id.
+ * @apiSuccess   {String}     delivery_service.region.name          Region name.
+ * @apiSuccess   {String}     delivery_service.region.state         Region state.
+ * @apiSuccess   {String[]}   delivery_service.region.cities        List of cities in region.
+ * @apiSuccess   {String}     delivery_service.region.timezone      Region timezone.
+ * @apiSuccess   {Number}     delivery_service.region.sales_tax     Region salex tax.
+ * @apiSuccess   {String}     delivery_service.region.lead_time_modifier     Region lead time modifier.
+ * @apiSuccess   {Boolean}    delivery_service.region.is_hidden     "True" if region is hidden.
+ * @apiSuccess   {Boolean}    delivery_service.region.sorts_by_no_contract     "True" if sorted by no contract.
+ */
 route.get('/delivery-services'
 , m.restrict(['admin'])
 , m.sort('-id')

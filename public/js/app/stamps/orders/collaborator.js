ba4eabf163a929526e28a90083655a33d7b80673
John Fawcett <jrf0110@gmail.com>
2016-01-05 10:14:24 -0600

    initial order collaborator model

---
+++ public/js/app/stamps/orders/collaborator.js
@@ -0,0 +1,16 @@
+/**
+ * Order Collaborator
+ */
+
+var db = require('db');
+var utils = require('utils');
+
+module.exports = require('stampit')()
+  .state({
+
+  })
+  .methods({
+    save: function( callback ){
+
+    }
+  });

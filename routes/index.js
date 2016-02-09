fa191d4bf55d400f5b87408cabd46d3170f30f93
m-amaya <marissa.amaya.311@gmail.com>
2016-02-09 12:12:56 -0600

    Whoops, didn't mean to push that

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ module.exports.register = function(app) {
   if (config.enableCaterWeb) {
     app.use(require('@goodybag/cater-web').makeHandler({
       env: process.env.NODE_ENV,
-      serverRendering: false
+      serverRendering: config.caterWebServerRendering
     }));
   }
 

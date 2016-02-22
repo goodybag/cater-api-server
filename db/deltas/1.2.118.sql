ebc3cba2c85f641098cf3134a7d4efef630c1d74
John Fawcett <jrf0110@gmail.com>
2016-02-22 15:10:30 -0600

    Don't update austin to 4% just yet

--- db/deltas/1.2.118.sql
+++ db/deltas/1.2.118.sql
@@ -9,6 +9,4 @@ begin
   execute 'insert into deltas (version, date) values ($1, $2)' using version, now();
 
   perform add_column( 'regions', 'default_price_hike', 'numeric(5, 5) not null default 0' );
-
-  update regions set default_price_hike = 0.04 where id = 1;
 end$$;

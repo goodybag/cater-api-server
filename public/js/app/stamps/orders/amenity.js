e18f4ac6d4391ead57ed3e54184711584277cb61
John Fawcett <jrf0110@gmail.com>
2016-02-23 16:33:30 -0600

    calculate price hike into amenities, make sure we're adding up priority account cost properly

--- public/js/app/stamps/orders/amenity.js
+++ public/js/app/stamps/orders/amenity.js
@@ -17,6 +17,7 @@ define( function( require, exports, module ){
     , price:        0
     , scale:        'flat'
     , enabled:      false
+    , priority_account_price_hike_percentage: 0
     })
     .enclose( function(){
 
@@ -27,6 +28,12 @@ define( function( require, exports, module ){
         else if (this.scale === 'flat') return this.price;
         else if (this.scale === 'multiply') return this.price * this.guests;
         throw new Error('Invalid amenity scale type');
+      }
+
+    , getPriorityAccountCost: function(){
+        return utils.nearestNickel(
+          this.getTotal() * this.priority_account_price_hike_percentage
+        );
       }
     });
 });

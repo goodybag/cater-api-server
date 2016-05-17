fd6364db8a8893a631ed651c85453e770fab7dde
m-amaya <marissa.amaya.311@gmail.com>
2016-05-17 10:57:06 -0500

    cleaned up validation controller

--- controllers/orders/promos.js
+++ controllers/orders/promos.js
@@ -5,26 +5,29 @@ module.exports.validate = function(req, res) {
   const promo_code = req.body.promo_code;
   const order_id = req.params.oid;
 
-  var where = { promo_code };
+  var where = { promo_code, 'promos_applied.order_id': order_id };
 
-  db.promos.findOne( where, (err, result) => {
+  var options = {
+    joins: [{
+        type: 'left'
+      , target: 'promos_applied'
+      , on: { promo_id: '$promos.id$' }
+    }]
+  };
+
+  db.promos.findOne( where, options, (err, result) => {
 
     if( !result ) {
       res.error( errors.promos.DOES_NOT_EXIST );
       return;
     }
 
-    var where = { promo_id: promo_code, order_id };
+    if( result.order_id ) {
+      res.error( errors.promos.ALREADY_APPLIED );
+      return;
+    }
 
-    db.promos_applied.findOne( where, (err, result) => {
-
-      if( result ) {
-        res.error( errors.promos.ALREADY_APPLIED );
-        return;
-      }
-
-      res.sendStatus(200);
-    });
+    res.sendStatus(204);
 
   });
 };

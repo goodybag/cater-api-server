6c9088ee81863406528807d05f26efed585133ab
Tenor Biel <tenorbiel@gmail.com>
2016-07-05 11:59:51 -0500

    Add post-independence day promo

--- config.js
+++ config.js
@@ -131,6 +131,7 @@ config.defaults = {
   , { start: '2015-05-26 00:00:00', end: '2015-05-27 00:00:00', rate: '2.0', description: 'Post-Memorial Day' }
   , { start: '2015-11-24 00:00:00', end: '2015-11-26 00:00:00', rate: '2.0', description: 'Thanksgiving week' }
   , { start: '2016-02-16 00:00:00', end: '2016-02-17 00:00:00', rate: '2.0', description: 'Post-President\'s Day' }
+  , { start: '2016-07-05 00:00:00', end: '2016-07-06 00:00:00', rate: '2.0', description: 'Post-Independence Day' }
   ]
 
 , welcome: {
